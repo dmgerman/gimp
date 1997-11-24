9e6863242da787e74d1425058b6e186811d0ffdc
Elliot Lee <sopwith@src.gnome.org>
1997-11-24 22:05:25 +0000

    Let's try this import one last time.
    Former-commit-id: b79b45400e9dfd14b3adb36c368a53e64e32d90d

--- plug-ins/sgi.h
+++ plug-ins/sgi.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * "$Id$"  *  *   SGI image file format library definitions.  *  *   Copyright 1997 Michael Sweet (mike@easysw.com)  *  *   This program is free software; you can redistribute it and/or modify it  *   under the terms of the GNU General Public License as published by the Free  *   Software Foundation; either version 2 of the License, or (at your option)  *   any later version.  *  *   This program is distributed in the hope that it will be useful, but  *   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY  *   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License  *   for more details.  *  *   You should have received a copy of the GNU General Public License  *   along with this program; if not, write to the Free Software  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *  * Revision History:  *  *   $Log$  *   Revision 1.1  1997/11/24 22:03:36  sopwith  *   Initial revision  *  *   Revision 1.3  1997/09/30 09:16:02  nobody  *   work I men it...  *  *   Revision 1.2  1997/06/18  00:55:28  mike  *   Updated to hold length table when writing.  *   Updated to hold current length when doing ARLE.  *  *   Revision 1.1  1997/06/15  03:37:19  mike  *   Initial revision  */
+comment|/*  * "$Id$"  *  *   SGI image file format library definitions.  *  *   Copyright 1997 Michael Sweet (mike@easysw.com)  *  *   This program is free software; you can redistribute it and/or modify it  *   under the terms of the GNU General Public License as published by the Free  *   Software Foundation; either version 2 of the License, or (at your option)  *   any later version.  *  *   This program is distributed in the hope that it will be useful, but  *   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY  *   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License  *   for more details.  *  *   You should have received a copy of the GNU General Public License  *   along with this program; if not, write to the Free Software  *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  *  * Revision History:  *  *   $Log$  *   Revision 1.1.1.1  1997/11/24 22:03:36  sopwith  *   Let's try this import one last time.  *  *   Revision 1.3  1997/09/30 09:16:02  nobody  *   work I men it...  *  *   Revision 1.2  1997/06/18  00:55:28  mike  *   Updated to hold length table when writing.  *   Updated to hold current length when doing ARLE.  *  *   Revision 1.1  1997/06/15  03:37:19  mike  *   Initial revision  */
 end_comment
 
 begin_ifndef
@@ -80,7 +80,7 @@ comment|/* Agressive run-length encoding */
 comment|/*  * Image structure...  */
 typedef|typedef
 struct|struct
-DECL|struct|__anon2adde53a0108
+DECL|struct|__anon298b94020108
 block|{
 DECL|member|file
 name|FILE
