dae78ac99ccd634ce412209c6ac1b9b186ef6ac8
Elliot Lee <sopwith@src.gnome.org>
1998-01-20 04:51:56 +0000

    misc minor performance improvements
    Former-commit-id: 03752a9264e9616585936cdcdab3b93a08f65df0

--- app/linked.h
+++ app/linked.h
@@ -3,6 +3,10 @@ begin_comment
 comment|/* The GIMP -- an image manipulation program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * This program is free software; you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 2 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program; if not, write to the Free Software  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
 end_comment
 
+begin_comment
+comment|/* This is compatibility stuff only... */
+end_comment
+
 begin_ifndef
 ifndef|#
 directive|ifndef
@@ -16,135 +20,129 @@ directive|define
 name|__LINKED_H__
 end_define
 
+begin_include
+include|#
+directive|include
+file|<glib.h>
+end_include
+
 begin_typedef
-DECL|struct|_link
-typedef|typedef
-struct|struct
-name|_link
-block|{
-DECL|member|data
-name|void
-modifier|*
-name|data
-decl_stmt|;
-DECL|member|next
-name|struct
-name|_link
-modifier|*
-name|next
-decl_stmt|;
 DECL|typedef|link_ptr
-block|}
-typedef|*
+typedef|typedef
+name|GSList
+modifier|*
 name|link_ptr
 typedef|;
 end_typedef
 
-begin_function_decl
-specifier|extern
-name|link_ptr
+begin_define
+DECL|macro|alloc_list ()
+define|#
+directive|define
 name|alloc_list
-parameter_list|(
-name|void
-parameter_list|)
-function_decl|;
-end_function_decl
+parameter_list|()
+value|g_slist_alloc()
+end_define
 
-begin_function_decl
-specifier|extern
-name|link_ptr
+begin_define
+DECL|macro|free_list (x)
+define|#
+directive|define
 name|free_list
 parameter_list|(
-name|link_ptr
+name|x
 parameter_list|)
-function_decl|;
-end_function_decl
+value|g_slist_free((x))
+end_define
 
-begin_function_decl
-specifier|extern
-name|link_ptr
+begin_define
+DECL|macro|add_to_list (x,y)
+define|#
+directive|define
 name|add_to_list
 parameter_list|(
-name|link_ptr
+name|x
 parameter_list|,
-name|void
-modifier|*
+name|y
 parameter_list|)
-function_decl|;
-end_function_decl
+value|g_slist_prepend((x), (y))
+end_define
 
-begin_function_decl
-specifier|extern
-name|link_ptr
+begin_define
+DECL|macro|append_to_list (x,y)
+define|#
+directive|define
 name|append_to_list
 parameter_list|(
-name|link_ptr
+name|x
 parameter_list|,
-name|void
-modifier|*
+name|y
 parameter_list|)
-function_decl|;
-end_function_decl
+value|g_slist_append((x), (y))
+end_define
 
-begin_function_decl
-specifier|extern
-name|link_ptr
+begin_define
+DECL|macro|insert_in_list (x,y,z)
+define|#
+directive|define
 name|insert_in_list
 parameter_list|(
-name|link_ptr
+name|x
 parameter_list|,
-name|void
-modifier|*
+name|y
 parameter_list|,
-name|int
+name|z
 parameter_list|)
-function_decl|;
-end_function_decl
+value|g_slist_insert((x), (y), (z))
+end_define
 
-begin_function_decl
-specifier|extern
-name|link_ptr
+begin_define
+DECL|macro|remove_from_list (x,y)
+define|#
+directive|define
 name|remove_from_list
 parameter_list|(
-name|link_ptr
+name|x
 parameter_list|,
-name|void
-modifier|*
+name|y
 parameter_list|)
-function_decl|;
-end_function_decl
+value|g_slist_remove((x), (y))
+end_define
 
-begin_function_decl
-specifier|extern
-name|link_ptr
+begin_define
+DECL|macro|next_item (x)
+define|#
+directive|define
 name|next_item
 parameter_list|(
-name|link_ptr
+name|x
 parameter_list|)
-function_decl|;
-end_function_decl
+value|(x)?g_slist_next((x)):NULL
+end_define
 
-begin_function_decl
-specifier|extern
-name|link_ptr
+begin_define
+DECL|macro|nth_item (x,y)
+define|#
+directive|define
 name|nth_item
 parameter_list|(
-name|link_ptr
+name|x
 parameter_list|,
-name|int
+name|y
 parameter_list|)
-function_decl|;
-end_function_decl
+value|g_slist_nth((x), (y))
+end_define
 
-begin_function_decl
-specifier|extern
-name|int
+begin_define
+DECL|macro|list_length (x)
+define|#
+directive|define
 name|list_length
 parameter_list|(
-name|link_ptr
+name|x
 parameter_list|)
-function_decl|;
-end_function_decl
+value|g_slist_length((x))
+end_define
 
 begin_endif
 endif|#
