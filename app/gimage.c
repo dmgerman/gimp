2e6d044b1d1a4a83a1059ed2aed0f24c7ebef354
Elliot Lee <sopwith@src.gnome.org>
1998-01-21 21:36:47 +0000

    Sort of revert linked.h changes, seeing as it does not work well :(
    Former-commit-id: 264c895b7d345a54880619014201012d85b00684

--- app/gimage.c
+++ app/gimage.c
@@ -6400,6 +6400,16 @@ name|index
 init|=
 literal|0
 decl_stmt|;
+name|g_return_val_if_fail
+argument_list|(
+name|gimage
+operator|!=
+name|NULL
+argument_list|,
+operator|-
+literal|1
+argument_list|)
+expr_stmt|;
 while|while
 condition|(
 name|layers
