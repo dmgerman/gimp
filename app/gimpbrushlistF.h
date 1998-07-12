f2b5af94d55fdd5ddbf64145c7595763b8704a7c
jaycox <jaycox@src.gnome.org>
1998-07-12 11:40:43 +0000

    ----------------------------------------------------------------------
    ----------------------------------------------------------------------
    Modified Files:
    	ChangeLog app/Makefile.am app/brush_select.c
    	app/gimpbrushlist.c app/gimpbrushlist.h app/gimpsetF.h
    	added sorting on the brush_list, fixed some encapulation issues.
    
    	app/paint_funcs.c
    	minor speed tweak to border_region
    Added Files:
    	app/gimpbrushlistF.h app/gimpbrushlistP.h app/gimplist.c
    	app/gimplist.h app/gimplistF.h app/gimplistP.h
            Split gimpbrushlist.h into 3 files.  New class "GimpList"
    ----------------------------------------------------------------------
    
    
    Former-commit-id: ed2e3ee66f6f63783a4bc0bee67c0b0335711c33

--- app/gimpbrushlistF.h
+++ app/gimpbrushlistF.h
@@ -2,22 +2,22 @@ begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|__GIMPSETF_H__
+name|__GIMPBRUSHLISTF_H__
 end_ifndef
 
 begin_define
-DECL|macro|__GIMPSETF_H__
+DECL|macro|__GIMPBRUSHLISTF_H__
 define|#
 directive|define
-name|__GIMPSETF_H__
+name|__GIMPBRUSHLISTF_H__
 end_define
 
 begin_typedef
-DECL|typedef|GimpSet
+DECL|typedef|GimpBrushList
 typedef|typedef
 name|struct
-name|_GimpSet
-name|GimpSet
+name|_GimpBrushList
+name|GimpBrushList
 typedef|;
 end_typedef
 
@@ -27,7 +27,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* __GIMPSETF_H__ */
+comment|/* __GIMPBRUSHLISTF_H__ */
 end_comment
 
 end_unit
--- app/gimplistF.h
+++ app/gimplistF.h
@@ -2,22 +2,22 @@ begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|__GIMPSETF_H__
+name|__GIMPLISTF_H__
 end_ifndef
 
 begin_define
-DECL|macro|__GIMPSETF_H__
+DECL|macro|__GIMPLISTF_H__
 define|#
 directive|define
-name|__GIMPSETF_H__
+name|__GIMPLISTF_H__
 end_define
 
 begin_typedef
-DECL|typedef|GimpSet
+DECL|typedef|GimpList
 typedef|typedef
 name|struct
-name|_GimpSet
-name|GimpSet
+name|_GimpList
+name|GimpList
 typedef|;
 end_typedef
 
@@ -27,7 +27,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* __GIMPSETF_H__ */
+comment|/* __GIMPLISTF_H__ */
 end_comment
 
 end_unit
