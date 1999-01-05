8a166be73b1aaac9ec0d449d4783e5dc8a86d59d
Lauri Alanko <nether@src.gnome.org>
1999-01-05 20:20:43 +0000

    s/_i/\. :P
    Former-commit-id: ba4354351d1eed15c50167fb808d4f093d24d994

--- tools/gcg/ex/strtest.c
+++ tools/gcg/ex/strtest.c
@@ -2,7 +2,7 @@ begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_include
 include|#
 directive|include
-file|<ex/file_ostream_i.h>
+file|<ex/file_ostream.i.h>
 end_include
 
 begin_function
