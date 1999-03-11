905ad4c39ba33ec1898db4dae914d0b3bf804006
Marc Lehmann <pcg@src.gnome.org>
1999-03-11 20:08:58 +0000

    see plug-ins/perl/Changes
    Former-commit-id: 12cd1a5c0e87ed7ca6ac3a785721c7b7ca0ffa3a

---
+++ plug-ins/perl/etc/acconfig.h
@@ -0,0 +1,73 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* Define if you have the ANSI C header files.  */
+end_comment
+
+begin_undef
+undef|#
+directive|undef
+name|STDC_HEADERS
+end_undef
+
+begin_comment
+comment|/* Define if you have the vsnprintf function.  */
+end_comment
+
+begin_undef
+undef|#
+directive|undef
+name|HAVE_VSNPRINTF
+end_undef
+
+begin_comment
+comment|/* Define if you have the<libgimp/gimp.h> header file.  */
+end_comment
+
+begin_undef
+undef|#
+directive|undef
+name|HAVE_LIBGIMP_GIMP_H
+end_undef
+
+begin_comment
+comment|/* Define if you have the glib library (-lglib).  */
+end_comment
+
+begin_undef
+undef|#
+directive|undef
+name|HAVE_LIBGLIB
+end_undef
+
+begin_comment
+comment|/* Define if you don't have gimp_get_data_size.  */
+end_comment
+
+begin_undef
+undef|#
+directive|undef
+name|HAVE_GET_DATA_SIZE
+end_undef
+
+begin_comment
+comment|/* Define if we have DIVIDE_MODE.  */
+end_comment
+
+begin_undef
+undef|#
+directive|undef
+name|HAVE_DIVIDE_MODE
+end_undef
+
+begin_comment
+comment|/* Define if we have _exit(2).  */
+end_comment
+
+begin_undef
+undef|#
+directive|undef
+name|HAVE__EXIT
+end_undef
+
+end_unit
+
