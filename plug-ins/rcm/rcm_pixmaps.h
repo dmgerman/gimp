ac679f539ea435fbddbb00ec8ea6867f6cc1794c
Sven Neumann <neo@src.gnome.org>
1999-04-05 20:31:20 +0000

    Added the rcm plugin.
    --Sven
    
    
    Former-commit-id: 4c3308411d681e73e7d070ecb11b0cec8e3e37b9

---
+++ plug-ins/rcm/rcm_pixmaps.h
@@ -0,0 +1,377 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* XPM */
+end_comment
+
+begin_decl_stmt
+DECL|variable|rcm_360
+specifier|static
+name|char
+modifier|*
+name|rcm_360
+index|[]
+init|=
+block|{
+literal|"24 24 15 1"
+block|,
+literal|" 	c None"
+block|,
+literal|".	c #E50466"
+block|,
+literal|"+	c #E50365"
+block|,
+literal|"@	c #F20233"
+block|,
+literal|"#	c #FF0000"
+block|,
+literal|"$	c #FE0000"
+block|,
+literal|"%	c #FB000C"
+block|,
+literal|"&	c #F50126"
+block|,
+literal|"*	c #F80119"
+block|,
+literal|"=	c #F20132"
+block|,
+literal|"-	c #EB034C"
+block|,
+literal|";	c #EB024C"
+block|,
+literal|">	c #F80019"
+block|,
+literal|",	c #E20472"
+block|,
+literal|"'	c #E80359"
+block|,
+literal|"                        "
+block|,
+literal|"                        "
+block|,
+literal|"                        "
+block|,
+literal|"                        "
+block|,
+literal|"         ...+++         "
+block|,
+literal|"        @###$$$%        "
+block|,
+literal|"       $&      @*$      "
+block|,
+literal|"      $=         *      "
+block|,
+literal|"     %&          @%     "
+block|,
+literal|"    -$            $;    "
+block|,
+literal|">#$$$$#         -$    "
+block|,
+literal|"                   $    "
+block|,
+literal|"                   $    "
+block|,
+literal|"    $-             $    "
+block|,
+literal|"    $,%@           $    "
+block|,
+literal|"   %>$ =%         $>    "
+block|,
+literal|"   @>            $+    "
+block|,
+literal|"  @  @%          @&     "
+block|,
+literal|"  '&$        $&      "
+block|,
+literal|"       =#......$=       "
+block|,
+literal|"         ###$$$         "
+block|,
+literal|"                        "
+block|,
+literal|"                        "
+block|,
+literal|"                        "
+block|}
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* XPM */
+end_comment
+
+begin_decl_stmt
+DECL|variable|rcm_a_b
+specifier|static
+name|char
+modifier|*
+name|rcm_a_b
+index|[]
+init|=
+block|{
+literal|"24 24 14 1"
+block|,
+literal|" 	c None"
+block|,
+literal|".	c #FE0000"
+block|,
+literal|"+	c #E50466"
+block|,
+literal|"@	c #FF0000"
+block|,
+literal|"#	c #F20233"
+block|,
+literal|"$	c #F50126"
+block|,
+literal|"%	c #F80019"
+block|,
+literal|"&	c #FB000C"
+block|,
+literal|"*	c #F20132"
+block|,
+literal|"=	c #EB024C"
+block|,
+literal|"-	c #E50365"
+block|,
+literal|";	c #EB034C"
+block|,
+literal|">	c #E80359"
+block|,
+literal|",	c #F80119"
+block|,
+literal|"                        "
+block|,
+literal|" .    +                 "
+block|,
+literal|" @   #$                 "
+block|,
+literal|" @ @%                   "
+block|,
+literal|" @@@#+                  "
+block|,
+literal|" .@$ $&#                "
+block|,
+literal|" @$$    *$              "
+block|,
+literal|" . =#    -*;            "
+block|,
+literal|" .  =      $>           "
+block|,
+literal|" @   *      $=          "
+block|,
+literal|" .           #=         "
+block|,
+literal|" @            $>        "
+block|,
+literal|" @             .-       "
+block|,
+literal|" .             ;*       "
+block|,
+literal|" @              ;       "
+block|,
+literal|" @              +.      "
+block|,
+literal|" @               ,      "
+block|,
+literal|" .               #>>  "
+block|,
+literal|" .>$>* #>  "
+block|,
+literal|" @             =# * ,   "
+block|,
+literal|" @              =$&,    "
+block|,
+literal|" .&.%    "
+block|,
+literal|" .;++++++++++++++,.#+++ "
+block|,
+literal|" .@@....@@@@...........$"
+block|}
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* XPM */
+end_comment
+
+begin_decl_stmt
+DECL|variable|rcm_ccw
+specifier|static
+name|char
+modifier|*
+name|rcm_ccw
+index|[]
+init|=
+block|{
+literal|"24 24 13 1"
+block|,
+literal|" 	c None"
+block|,
+literal|".	c #F20233"
+block|,
+literal|"+	c #EB034C"
+block|,
+literal|"@	c #FF0000"
+block|,
+literal|"#	c #F20132"
+block|,
+literal|"$	c #FB000C"
+block|,
+literal|"%	c #F50126"
+block|,
+literal|"&	c #FE0000"
+block|,
+literal|"*	c #E50365"
+block|,
+literal|"=	c #EB024C"
+block|,
+literal|"-	c #E50466"
+block|,
+literal|";	c #F80019"
+block|,
+literal|">	c #F80119"
+block|,
+literal|"                        "
+block|,
+literal|"                        "
+block|,
+literal|"                        "
+block|,
+literal|"                        "
+block|,
+literal|"                        "
+block|,
+literal|"         .....          "
+block|,
+literal|"       +@#####$%        "
+block|,
+literal|"&#*&#       "
+block|,
+literal|"  .  .%         +%      "
+block|,
+literal|"  . +&           $#     "
+block|,
+literal|"   @&+=#-        .#     "
+block|,
+literal|"   @&%%           $     "
+block|,
+literal|"    #                   "
+block|,
+literal|"                        "
+block|,
+literal|"    .           .$$     "
+block|,
+literal|"    ;          $#$&=    "
+block|,
+literal|"    -&>=&    "
+block|,
+literal|"     %$>& +    "
+block|,
+literal|"      %&&;       "
+block|,
+literal|"       *;@.-+&&*        "
+block|,
+literal|"          =@%           "
+block|,
+literal|"                        "
+block|,
+literal|"                        "
+block|,
+literal|"                        "
+block|}
+decl_stmt|;
+end_decl_stmt
+
+begin_comment
+comment|/* XPM */
+end_comment
+
+begin_decl_stmt
+DECL|variable|rcm_cw
+specifier|static
+name|char
+modifier|*
+name|rcm_cw
+index|[]
+init|=
+block|{
+literal|"24 24 13 1"
+block|,
+literal|" 	c None"
+block|,
+literal|".	c #F20233"
+block|,
+literal|"+	c #F50126"
+block|,
+literal|"@	c #FB000C"
+block|,
+literal|"#	c #F20132"
+block|,
+literal|"$	c #FE0000"
+block|,
+literal|"%	c #EB024C"
+block|,
+literal|"&	c #FF0000"
+block|,
+literal|"*	c #E50365"
+block|,
+literal|"=	c #E50466"
+block|,
+literal|"-	c #EB034C"
+block|,
+literal|";	c #F80019"
+block|,
+literal|">	c #F80119"
+block|,
+literal|"                        "
+block|,
+literal|"                        "
+block|,
+literal|"                        "
+block|,
+literal|"                        "
+block|,
+literal|"                        "
+block|,
+literal|"          .....         "
+block|,
+literal|"        +@#####$%       "
+block|,
+literal|"       .&      *.$      "
+block|,
+literal|"      +%         +#  .  "
+block|,
+literal|"     .@           $% #  "
+block|,
+literal|"     .#        =.%-$$   "
+block|,
+literal|"     @           ++$$   "
+block|,
+literal|"                   #    "
+block|,
+literal|"                        "
+block|,
+literal|"     @@.           .    "
+block|,
+literal|"    -$@#@          ;    "
+block|,
+literal|"    $%>           $*    "
+block|,
+literal|"    % $>         @+     "
+block|,
+literal|"       ;$       $+      "
+block|,
+literal|"        *&&-=.$;*       "
+block|,
+literal|"           +$%          "
+block|,
+literal|"                        "
+block|,
+literal|"                        "
+block|,
+literal|"                        "
+block|}
+decl_stmt|;
+end_decl_stmt
+
+end_unit
+
