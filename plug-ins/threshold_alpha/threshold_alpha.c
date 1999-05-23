fb258e8226ed0fe9ad89180a526190f358dfaf44
BST 1999  Adam D. Moss <adam@gimp.org>
1999-05-23 13:21:49 +0000

    Added support for greyscale drawables (why not?)
    Sun May 23 14:13:03 BST 1999  Adam D. Moss  <adam@gimp.org>
    
    	* plug-ins/threshold_alpha/threshold_alpha.c: Added
    	support for greyscale drawables (why not?)
    
    
    Former-commit-id: 0c627c00bedec26d35a9bf353db37df291107713

--- plug-ins/threshold_alpha/threshold_alpha.c
+++ plug-ins/threshold_alpha/threshold_alpha.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* threshold_alpha.c -- This is a plug-in for the GIMP (1.0's API)  * Author: Shuji Narazaki<narazaki@InetQ.or.jp>  * Time-stamp:<1997/06/08 22:34:26 narazaki@InetQ.or.jp>  * Version: 0.13  *  * Copyright (C) 1997 Shuji Narazaki<narazaki@InetQ.or.jp>  *  * This program is free software; you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 2 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program; if not, write to the Free Software  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+comment|/* threshold_alpha.c -- This is a plug-in for the GIMP (1.0's API)  * Author: Shuji Narazaki<narazaki@InetQ.or.jp>  * Time-stamp:<1997/06/08 22:34:26 narazaki@InetQ.or.jp>  * Version: 0.13A (the 'A' is for Adam who hacked in greyscale  *                 support - don't know if there's a more recent official  *                 version)  *  * Copyright (C) 1997 Shuji Narazaki<narazaki@InetQ.or.jp>  *  * This program is free software; you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 2 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program; if not, write to the Free Software  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 end_comment
 
 begin_include
@@ -491,7 +491,7 @@ end_decl_stmt
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2c90c9110108
+DECL|struct|__anon278bfcc00108
 block|{
 DECL|member|threshold
 name|gint
@@ -518,7 +518,7 @@ end_decl_stmt
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2c90c9110208
+DECL|struct|__anon278bfcc00208
 block|{
 DECL|member|run
 name|gint
@@ -785,13 +785,19 @@ name|gimp_drawable_color
 argument_list|(
 name|drawable_id
 argument_list|)
+operator|&&
+operator|!
+name|gimp_drawable_gray
+argument_list|(
+name|drawable_id
+argument_list|)
 condition|)
 block|{
 name|ERROR_DIALOG
 argument_list|(
 literal|1
 argument_list|,
-literal|"RGB drawable is not selected."
+literal|"RGBA/GRAYA drawable is not selected."
 argument_list|)
 expr_stmt|;
 return|return;
@@ -997,9 +1003,21 @@ condition|)
 return|return
 name|STATUS_EXECUTION_ERROR
 return|;
+if|if
+condition|(
+name|gimp_drawable_color
+argument_list|(
+name|drawable_id
+argument_list|)
+condition|)
 name|gap
 operator|=
 literal|3
+expr_stmt|;
+else|else
+name|gap
+operator|=
+literal|1
 expr_stmt|;
 name|gimp_drawable_mask_bounds
 argument_list|(
