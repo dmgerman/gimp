987f4d6d2783637cd8337ad99914c486fb8f26c8
Asbjørn Pettersen <asbjoer@src.gnome.org>
1999-05-24 19:18:03 +0000

    Add include <sys/types.h> for OS/2
    Former-commit-id: 942728ab90addc0e0d1bdfa2dbabb2ad493252fc

--- plug-ins/faxg3/faxg3.c
+++ plug-ins/faxg3/faxg3.c
@@ -26,6 +26,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/types.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<unistd.h>
 end_include
 
--- plug-ins/faxg3/g3.c
+++ plug-ins/faxg3/g3.c
@@ -18,6 +18,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<sys/types.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<unistd.h>
 end_include
 
