86a6eba1f59459686f9da9a45ba5e6d11a5f8e59
Marc Lehmann <pcg@src.gnome.org>
1999-10-24 21:27:29 +0000

    *** empty log message ***
    Former-commit-id: 1a2becb0b7119cd1d5c49a2795116180b5e434ef

--- libgimp/gimp.h
+++ libgimp/gimp.h
@@ -64,6 +64,16 @@ directive|include
 file|"libgimp/gimpunit.h"
 end_include
 
+begin_include
+include|#
+directive|include
+file|"libgimp/gimpcompat.h"
+end_include
+
+begin_comment
+comment|/* to be removed in 1.3 */
+end_comment
+
 begin_ifdef
 ifdef|#
 directive|ifdef
