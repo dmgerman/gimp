74fd1f977d1d4dfa1668f16892a67b93ea6b6bd2
Sven Neumann <neo@src.gnome.org>
2000-02-06 23:16:24 +0000

    doumented gimpenv
    --Sven
    
    
    Former-commit-id: 2debda2f0f2d1a9b4fc0fd59bcac49fea0fae823

--- libgimpbase/gimpenv.c
+++ libgimpbase/gimpenv.c
@@ -80,25 +80,31 @@ endif|#
 directive|endif
 end_endif
 
+begin_comment
+comment|/**  * gimp_directory:  *  * Returns the user-specific GIMP settings directory. If the environment   * variable GIMP_DIRECTORY exists, it is used. If it is an absolute path,   * it is used as is.  If it is a relative path, it is taken to be a   * subdirectory of the home directory. If it is relative path, and no home   * directory can be determined, it is taken to be a subdirectory of  * gimp_data_directory().  *  * The usual case is that no GIMP_DIRECTORY environment variable exists,   * and then we use the GIMPDIR subdirectory of the home directory. If no   * home directory exists, we use a per-user subdirectory of  * gimp_data_directory().  * In any case, we always return some non-empty string, whether it  * corresponds to an existing directory or not.  *  * The returned string is allocated just once, and should *NOT* be  * freed with g_free().  *  * Returns: The user-specific GIMP settings directory.  */
+end_comment
+
 begin_function
-name|char
+name|gchar
 modifier|*
-DECL|function|gimp_directory ()
+DECL|function|gimp_directory (void)
 name|gimp_directory
-parameter_list|()
+parameter_list|(
+name|void
+parameter_list|)
 block|{
 specifier|static
-name|char
+name|gchar
 modifier|*
 name|gimp_dir
 init|=
 name|NULL
 decl_stmt|;
-name|char
+name|gchar
 modifier|*
 name|env_gimp_dir
 decl_stmt|;
-name|char
+name|gchar
 modifier|*
 name|home_dir
 decl_stmt|;
@@ -266,13 +272,17 @@ return|;
 block|}
 end_function
 
+begin_comment
+comment|/**  * gimp_personal_rc_file:  * @basename: The basename of a rc_file.  *  * Returns the name of a file in the user-specific GIMP settings directory.  *  * The returned string is allocated dynamically and *SHOULD* be freed  * with g_free() after use.  *  * Returns: The name of a file in the user-specific GIMP settings directory.  */
+end_comment
+
 begin_function
-name|char
+name|gchar
 modifier|*
-DECL|function|gimp_personal_rc_file (char * basename)
+DECL|function|gimp_personal_rc_file (gchar * basename)
 name|gimp_personal_rc_file
 parameter_list|(
-name|char
+name|gchar
 modifier|*
 name|basename
 parameter_list|)
@@ -293,21 +303,27 @@ return|;
 block|}
 end_function
 
+begin_comment
+comment|/**  * gimp_data_directory:  *  * Returns the top directory for GIMP data. If the environment variable   * GIMP_DATADIR exists, that is used.  It should be an absolute pathname.  * Otherwise, on Unix the compile-time defined directory is used.  On  * Win32, the installation directory as deduced from the executable's  * name is used.  *  * The returned string is allocated just once, and should *NOT* be  * freed with g_free().  *  * Returns: The top directory for GIMP data.  */
+end_comment
+
 begin_function
-name|char
+name|gchar
 modifier|*
-DECL|function|gimp_data_directory ()
+DECL|function|gimp_data_directory (void)
 name|gimp_data_directory
-parameter_list|()
+parameter_list|(
+name|void
+parameter_list|)
 block|{
 specifier|static
-name|char
+name|gchar
 modifier|*
 name|gimp_data_dir
 init|=
 name|NULL
 decl_stmt|;
-name|char
+name|gchar
 modifier|*
 name|env_gimp_data_dir
 init|=
@@ -403,13 +419,13 @@ directive|endif
 else|#
 directive|else
 comment|/* Figure it out from the executable name */
-name|char
+name|gchar
 name|filename
 index|[
 name|MAX_PATH
 index|]
 decl_stmt|;
-name|char
+name|gchar
 modifier|*
 name|sep1
 decl_stmt|,
@@ -515,18 +531,20 @@ block|}
 end_function
 
 begin_comment
-comment|/* gimp_gtkrc returns the name of the GIMP's application-specific  * gtkrc file.  *  * The returned string is allocated just once, and should *NOT* be  * freed with g_free().  */
+comment|/**  * gimp_gtkrc:  *  * Returns the name of the GIMP's application-specific gtkrc file.  *  * The returned string is allocated just once, and should *NOT* be  * freed with g_free().  *  * Returns: The name of the GIMP's application-specific gtkrc file.  */
 end_comment
 
 begin_function
-name|char
+name|gchar
 modifier|*
-DECL|function|gimp_gtkrc ()
+DECL|function|gimp_gtkrc (void)
 name|gimp_gtkrc
-parameter_list|()
+parameter_list|(
+name|void
+parameter_list|)
 block|{
 specifier|static
-name|char
+name|gchar
 modifier|*
 name|gimp_gtkrc_filename
 init|=
