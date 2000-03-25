643ee1680b3721b04612b1c7cc8196f3b926c2bc
Stanislav Brabec <utx@src.gnome.org>
2000-03-25 13:12:01 +0000

    i18n fix
    Former-commit-id: b08f5860df7fa8c06ee9ddbad1d7fc7a6d3eda7a

--- app/tools/gimphuesaturationtool.c
+++ app/tools/gimphuesaturationtool.c
@@ -1904,10 +1904,13 @@ name|gtk_radio_button_new_with_label
 argument_list|(
 name|group
 argument_list|,
+name|gettext
+argument_list|(
 name|hue_partition_names
 index|[
 name|i
 index|]
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|group
