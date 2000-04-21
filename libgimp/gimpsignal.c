dfe6bef1a81eb6e88947b9c6a3bb63b4977d0982
Sven Neumann <neo@src.gnome.org>
2000-04-21 18:37:25 +0000

    cleaned up the messy spanish translation someone added
    fixed compiler warnings, changed some gints to gbooleans
    
    cleaned up namespace and documentation
    
    updated libgimp documentation
    
    
    
    --Sven
    
    
    Former-commit-id: e47e118dd79469993e0f7ca983c994a1d08528c1

--- libgimp/gimpsignal.c
+++ libgimp/gimpsignal.c
@@ -10,15 +10,15 @@ file|"gimpsignal.h"
 end_include
 
 begin_comment
-comment|/**   * gimp_signal:   * @signum:   selects signal to be handled see man 5 signal  * @handler:  handler that maps to signum. Invoked by O/S.   *            handler gets signal that caused invocation.   * @sa_flags: preferences. OR'ed SA_<xxx>. See signal.h   *  * This function furnishes a workalike for signal(2) but  * which internally invokes sigaction(2) after certain  * sa_flags are set; these primarily to ensure restarting  * of interrupted system calls. See sigaction(2)  It is a   * aid to transition and not new development: that effort   * should employ sigaction directly. [<gosgood@idt.net> 18.04.2000]   *  * Cause handler to be run when signum is delivered.  We  * use sigaction(2) rather than signal(2) so that we can control the  * signal hander's environment completely via sa_flags: some signal(2)  * implementations differ in their sematics, so we need to nail down  * exactly what we want. [<austin@gimp.org> 06.04.2000]  *  * Returns: RetSigType (a reference to a signal handling function)    *  */
-end_comment
-
-begin_comment
 comment|/* Courtesy of Austin Donnelly 06-04-2000 to address bug #2742 */
 end_comment
 
+begin_comment
+comment|/**   * gimp_signal_private:   * @signum: selects signal to be handled see man 5 signal  * @gimp_sighandler:  handler that maps to signum. Invoked by O/S.   *                    handler gets signal that caused invocation.   * @sa_flags: preferences. OR'ed SA_<xxx>. See signal.h   *  * This function furnishes a workalike for signal(2) but  * which internally invokes sigaction(2) after certain  * sa_flags are set; these primarily to ensure restarting  * of interrupted system calls. See sigaction(2)  It is a   * aid to transition and not new development: that effort   * should employ sigaction directly. [gosgood 18.04.2000]   *  * Cause handler to be run when signum is delivered.  We  * use sigaction(2) rather than signal(2) so that we can control the  * signal hander's environment completely via sa_flags: some signal(2)  * implementations differ in their sematics, so we need to nail down  * exactly what we want. [austin 06.04.2000]  *  * Returns: a reference to a signal handling function  */
+end_comment
+
 begin_function
-name|RetSigType
+name|GimpRetSigType
 DECL|function|gimp_signal_private (gint signum,void (* gimp_sighandler)(int),gint sa_flags)
 name|gimp_signal_private
 parameter_list|(
