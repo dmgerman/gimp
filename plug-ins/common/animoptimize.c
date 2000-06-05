b6424e3de40b0f3f1e8dd0097d0a500af744a6b4
BST 2000  Adam D. Moss <adam@gimp.org>
2000-06-05 12:02:31 +0000

    Fix old bug which could cause errors in evaluating the final pixel of each
    Mon Jun  5 12:48:10 BST 2000  Adam D. Moss <adam@gimp.org>
    
    	* plug-ins/common/animoptimize.c: Fix old bug which could
    	cause errors in evaluating the final pixel of each composed layer.
    
    	* plug-ins/common/animationplay.c: Same bug, different plugin.
    
    
    Former-commit-id: 17c60b169c415919ae9a783bc6dcc27cc0c5c5e4

--- plug-ins/common/animoptimize.c
+++ plug-ins/common/animoptimize.c
@@ -1,10 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * Animation Optimizer plug-in version 1.0.2  *  * (c) Adam D. Moss, 1997-2000  *     adam@gimp.org  *     adam@foxbox.org  *  * This is part of the GIMP package and falls under the GPL.  */
+comment|/*  * Animation Optimizer plug-in version 1.0.3  *  * (c) Adam D. Moss, 1997-2000  *     adam@gimp.org  *     adam@foxbox.org  *  * This is part of the GIMP package and falls under the GPL.  */
 end_comment
 
 begin_comment
-comment|/*  * REVISION HISTORY:  *  * 2000-01-13 : version 1.0.2  *              Collapse timing of completely optimized-away frames  *              onto previous surviving frame.  Also be looser with  *              (XXXXX) tag parsing.  *  * 2000-01-07 : version 1.0.1  *              PDB interface submitted by Andreas Jaekel  *<jaekel@cablecats.de>  *  * 98.05.17 : version 1.0.0  *            Finally preserves frame timings / layer names.  Has  *            a progress bar now.  No longer beta, I suppose.  *  * 98.04.19 : version 0.70.0  *            Plug-in doubles up as Animation UnOptimize too!  (This  *            is somewhat more useful than it sounds.)  *  * 98.03.16 : version 0.61.0  *            Support more rare opaque/transparent combinations.  *  * 97.12.09 : version 0.60.0  *            Added support for INDEXED* and GRAY* images.  *  * 97.12.09 : version 0.52.0  *            Fixed some bugs.  *  * 97.12.08 : version 0.51.0  *            Relaxed bounding box on optimized layers marked  *            'replace'.  *  * 97.12.07 : version 0.50.0  *            Initial release.  */
+comment|/*  * REVISION HISTORY:  *  * 2000-06-05 : version 1.0.3  *              Fix old bug which could cause errors in evaluating the  *              final pixel of each composed layer.  *  * 2000-01-13 : version 1.0.2  *              Collapse timing of completely optimized-away frames  *              onto previous surviving frame.  Also be looser with  *              (XXXXX) tag parsing.  *  * 2000-01-07 : version 1.0.1  *              PDB interface submitted by Andreas Jaekel  *<jaekel@cablecats.de>  *  * 98.05.17 : version 1.0.0  *            Finally preserves frame timings / layer names.  Has  *            a progress bar now.  No longer beta, I suppose.  *  * 98.04.19 : version 0.70.0  *            Plug-in doubles up as Animation UnOptimize too!  (This  *            is somewhat more useful than it sounds.)  *  * 98.03.16 : version 0.61.0  *            Support more rare opaque/transparent combinations.  *  * 97.12.09 : version 0.60.0  *            Added support for INDEXED* and GRAY* images.  *  * 97.12.09 : version 0.52.0  *            Fixed some bugs.  *  * 97.12.08 : version 0.51.0  *            Relaxed bounding box on optimized layers marked  *            'replace'.  *  * 97.12.07 : version 0.50.0  *            Initial release.  */
 end_comment
 
 begin_comment
@@ -60,7 +60,7 @@ end_include
 begin_typedef
 typedef|typedef
 enum|enum
-DECL|enum|__anon27db05d70103
+DECL|enum|__anon275f988b0103
 block|{
 DECL|enumerator|DISPOSE_UNDEFINED
 name|DISPOSE_UNDEFINED
@@ -1429,8 +1429,8 @@ name|rawheight
 expr_stmt|;
 while|while
 condition|(
-operator|--
 name|i
+operator|--
 condition|)
 block|{
 if|if
@@ -1528,8 +1528,8 @@ name|rawheight
 expr_stmt|;
 while|while
 condition|(
-operator|--
 name|i
+operator|--
 condition|)
 block|{
 operator|*
@@ -1584,7 +1584,7 @@ block|{
 comment|/* --- These are suboptimal catch-all cases for when  --- */
 comment|/* --- this frame is bigger/smaller than the preview  --- */
 comment|/* --- buffer, and/or offset within it.               --- */
-comment|/* FIXME: FINISH ME! */
+comment|/* FIXME: FINISH ME! [done?] */
 if|if
 condition|(
 name|gimp_drawable_has_alpha
@@ -1980,8 +1980,8 @@ name|rawheight
 expr_stmt|;
 while|while
 condition|(
-operator|--
 name|i
+operator|--
 condition|)
 block|{
 if|if
@@ -2051,8 +2051,8 @@ name|rawheight
 expr_stmt|;
 while|while
 condition|(
-operator|--
 name|i
+operator|--
 condition|)
 block|{
 operator|*
