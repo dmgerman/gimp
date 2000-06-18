9a298a1575cd1992f839301897b17d238680cfaa
Sven Neumann <sven@gimp.org>
2000-06-18 20:32:45 +0000

    applied gimp-cornwell-000613-0 which brings the list of authors upto date
    2000-06-18  Sven Neumann  <sven@gimp.org>
    
    	* AUTHORS: applied gimp-cornwell-000613-0 which brings the
    	list of authors upto date including all translators and
    	plug-in authors. Please check that you are listed if you have
    	contributed!
    
    	* gimp.1.in
    	* app/about_dialog.c: synced the authors lists
    
    	* INSTALL: we need gtk+-1.2.8 or newer
    
    
    Former-commit-id: 37d2c07ffaa7f2b477aef789b61cabb3f0c578e3

--- app/about_dialog.c
+++ app/about_dialog.c
@@ -345,6 +345,10 @@ literal|"Lauri Alanko"
 block|,
 literal|"Shawn Amundson"
 block|,
+literal|"Sven Anders"
+block|,
+literal|"Karl-Johan Andersson"
+block|,
 literal|"John Beale"
 block|,
 literal|"Zach Beane"
@@ -355,25 +359,51 @@ literal|"Marc Bless"
 block|,
 literal|"Edward Blevins"
 block|,
+literal|"Reagan Blundell"
+block|,
+literal|"Andreas Bogk"
+block|,
+literal|"Xavier Bouchoux"
+block|,
 literal|"Roberto Boyd"
 block|,
 literal|"Stanislav Brabec"
 block|,
+literal|"Robert Brady"
+block|,
+literal|"Hans Breuer"
+block|,
 literal|"Simon Budig"
+block|,
+literal|"Carey Bunks"
 block|,
 literal|"Seth Burgess"
 block|,
 literal|"Brent Burton"
 block|,
-literal|"Carey Bunks"
-block|,
 literal|"Francisco Bustamante"
+block|,
+literal|"Albert Cahalan"
+block|,
+literal|"Sean Cier"
+block|,
+literal|"Winston Chang"
+block|,
+literal|"Kenneth Christiansen"
+block|,
+literal|"Zbigniew Chyla"
 block|,
 literal|"Ed Connel"
 block|,
+literal|"Piers Cornwell"
+block|,
+literal|"Daniel Cotting"
+block|,
 literal|"Jay Cox"
 block|,
-literal|"Kenneth Christiansen"
+literal|"Brian Degenhardt"
+block|,
+literal|"Gert Dewit"
 block|,
 literal|"Andreas Dilger"
 block|,
@@ -381,9 +411,13 @@ literal|"Austin Donnelly"
 block|,
 literal|"Scott Draves"
 block|,
+literal|"Daniel Dunbar"
+block|,
 literal|"Misha Dynin"
 block|,
 literal|"Daniel Egger"
+block|,
+literal|"Morton Eriksen"
 block|,
 literal|"Larry Ewing"
 block|,
@@ -393,15 +427,31 @@ literal|"Valek Filippov"
 block|,
 literal|"David Forsyth"
 block|,
+literal|"Raphael Francois"
+block|,
+literal|"Jochen Friedrich"
+block|,
+literal|"Sami Gerdt"
+block|,
 literal|"Jim Geuther"
+block|,
+literal|"Graeme Gill"
 block|,
 literal|"Scott Goehring"
 block|,
 literal|"Heiko Goller"
 block|,
+literal|"Marcelo de Gomensoro Malheiros"
+block|,
+literal|"Pavel Grinfeld"
+block|,
 literal|"Michael Hammel"
 block|,
+literal|"Henrik Hansen"
+block|,
 literal|"James Henstridge"
+block|,
+literal|"Eric Hernes"
 block|,
 literal|"Christoph Hoegl"
 block|,
@@ -409,23 +459,41 @@ literal|"Wolfgang Hofer"
 block|,
 literal|"Jan Hubicka"
 block|,
+literal|"Andreas Hyden"
+block|,
+literal|"Ben Jackson"
+block|,
+literal|"Krzysztof Jakubowski"
+block|,
 literal|"Simon Janes"
 block|,
 literal|"Tim Janik"
 block|,
-literal|"Tuomas Kuosmanen"
+literal|"Fellmann Joaquim"
+block|,
+literal|"Andrew Kieschnick"
 block|,
 literal|"Peter Kirchgessner"
+block|,
+literal|"Philipp Klaus"
+block|,
+literal|"David Koblas"
+block|,
+literal|"Vincent Kolwitz"
+block|,
+literal|"Tuomas Kuosmanen"
 block|,
 literal|"Karin Kylander"
 block|,
 literal|"Olof S Kylander"
 block|,
-literal|"Nick Lamb"
-block|,
-literal|"Karl LaRocca"
+literal|"Karl La Rocca"
 block|,
 literal|"Chris Lahey"
+block|,
+literal|"Nick Lamb"
+block|,
+literal|"Marco Lamberto"
 block|,
 literal|"Jens Lautenbacher"
 block|,
@@ -434,6 +502,8 @@ block|,
 literal|"Elliot Lee"
 block|,
 literal|"Marc Lehmann"
+block|,
+literal|"Ray Lahtiniemi"
 block|,
 literal|"Raph Levien"
 block|,
@@ -449,8 +519,6 @@ literal|"Josh MacDonald"
 block|,
 literal|"Ed Mackey"
 block|,
-literal|"Daniele Medri"
-block|,
 literal|"Vidar Madsen"
 block|,
 literal|"Marcelo Malheiros"
@@ -463,15 +531,31 @@ literal|"Kelly Martin"
 block|,
 literal|"Torsten Martinsen"
 block|,
-literal|"Federico Mena"
+literal|"Gordon Matzigkeit"
+block|,
+literal|"Gregory McLean"
+block|,
+literal|"Daniele Medri"
+block|,
+literal|"Federico Mena Quintero"
+block|,
+literal|"James Mitchell"
+block|,
+literal|"Hirotsuna Mizuno"
 block|,
 literal|"David Monniaux"
 block|,
-literal|"Adam D. Moss"
+literal|"Adam D Moss"
+block|,
+literal|"Balazs Nagy"
+block|,
+literal|"Yukihiro Nakai"
 block|,
 literal|"Sung-Hyun Nam"
 block|,
 literal|"Shuji Narazaki"
+block|,
+literal|"Felix Natter"
 block|,
 literal|"Michael Natterer"
 block|,
@@ -479,41 +563,75 @@ literal|"Sven Neumann"
 block|,
 literal|"Stephen Robert Norris"
 block|,
-literal|"Erik Nygren"
+literal|"Tim Newsome"
 block|,
-literal|"Balazs Nagy"
+literal|"Erik Nygren"
 block|,
 literal|"Miles O'Neal"
 block|,
-literal|"Garry R. Osgood"
+literal|"Thom van Os"
+block|,
+literal|"Gary Osgood"
+block|,
+literal|"Alan Paeth"
 block|,
 literal|"Jay Painter"
+block|,
+literal|"Sergey Panov"
 block|,
 literal|"Asbjorn Pettersen"
 block|,
 literal|"Mike Phillips"
 block|,
+literal|"Artur Polaczyaski"
+block|,
 literal|"Raphael Quinet"
 block|,
 literal|"Vincent Renardias"
 block|,
+literal|"Jens Restemeier"
+block|,
+literal|"Maurits Rijk"
+block|,
+literal|"Daniel Risacher"
+block|,
 literal|"James Robinson"
+block|,
+literal|"Tim Rowley"
+block|,
+literal|"Pablo Saratxaga"
 block|,
 literal|"Mike Schaeffer"
 block|,
+literal|"John Schlag"
+block|,
+literal|"Norbert Schmitz"
+block|,
+literal|"Thorsten Schnier"
+block|,
+literal|"Alexander Schulz"
+block|,
 literal|"Tracy Scott"
+block|,
+literal|"Craig Setera"
 block|,
 literal|"Aaron Sherman"
 block|,
 literal|"Manish Singh"
 block|,
+literal|"Daniel Skarda"
+block|,
 literal|"Nathan Summers"
 block|,
 literal|"Mike Sweet"
 block|,
+literal|"Yuri Syrota"
+block|,
 literal|"Eiichi Takamori"
 block|,
 literal|"Tristan Tarrant"
+block|,
+literal|"Michael Taylor"
 block|,
 literal|"Owen Taylor"
 block|,
@@ -521,9 +639,17 @@ literal|"Ian Tester"
 block|,
 literal|"Andy Thomas"
 block|,
+literal|"Kevin Turner"
+block|,
+literal|"Martin Weber"
+block|,
 literal|"James Wang"
 block|,
 literal|"Kris Wehner"
+block|,
+literal|"Nigel Wetten"
+block|,
+literal|"Calvin Williamson"
 block|,
 literal|"Matthew Wilson"
 block|,
