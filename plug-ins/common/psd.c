ef54476244bc236b82f2ae2b832d4b91f8cad48e
BST 2000  Adam D. Moss <adam@gimp.org>
2000-08-23 19:13:41 +0000

    removed some more debugging output. Zzz.
    Wed Aug 23 20:15:21 BST 2000  Adam D. Moss <adam@gimp.org>
    
    	* plug-ins/common/psd.c: removed some more debugging output.  Zzz.
    
    
    Former-commit-id: 722f600e0f7be96c1182e77191accdf508f50748

--- plug-ins/common/psd.c
+++ plug-ins/common/psd.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/*  * PSD Plugin version 2.0.5  * This GIMP plug-in is designed to load Adobe Photoshop(tm) files (.PSD)  *  * Adam D. Moss<adam@gimp.org><adam@foxbox.org>  *  *     If this plug-in fails to load a file which you think it should,  *     please tell me what seemed to go wrong, and anything you know  *     about the image you tried to load.  Please don't send big PSD  *     files to me without asking first.  *  *          Copyright (C) 1997-99 Adam D. Moss  *          Copyright (C) 1996    Torsten Martinsen  *  * This program is free software; you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 2 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program; if not, write to the Free Software  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+comment|/*  * PSD Plugin version 2.0.6  * This GIMP plug-in is designed to load Adobe Photoshop(tm) files (.PSD)  *  * Adam D. Moss<adam@gimp.org><adam@foxbox.org>  *  *     If this plug-in fails to load a file which you think it should,  *     please tell me what seemed to go wrong, and anything you know  *     about the image you tried to load.  Please don't send big PSD  *     files to me without asking first.  *  *          Copyright (C) 1997-2000 Adam D. Moss  *          Copyright (C) 1996      Torsten Martinsen  *  * This program is free software; you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 2 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program; if not, write to the Free Software  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
 end_comment
 
 begin_comment
@@ -8,11 +8,11 @@ comment|/*  * Adobe and Adobe Photoshop are trademarks of Adobe Systems  * Incor
 end_comment
 
 begin_comment
-comment|/*  * Revision history:  *  *  1999.11.14 / v2.0.5 / Adam D. Moss  *       Applied patch by Andy Hefner to load 1-bit images.  *  *  1999.08.13 / v2.0.4 / Adam D. Moss  *       Allowed NULL layer names again, whee.  *  *  1999.08.20 / v2.0.3 / Adam D. Moss  *       Ensure that NULL name does not get passed to gimp_layer_new(),  *       or it will fail to create the layer and cause problems down  *       the line (only since April 1999).  *  *  1999.01.18 / v2.0.2 / Adam D. Moss  *       Better guess at how PSD files store Guide position precision.  *  *  1999.01.10 / v2.0.1 / Adam D. Moss  *       Greatly reduced memory requirements for layered image loading -  *       we now do just-in-time channel unpacking.  Some little  *       cleanups too.  *  *  1998.09.04 / v2.0.0 / Adam D. Moss  *       Now recognises and loads the new Guides extensions written  *       by Photoshop 4 and 5.  *  *  1998.07.31 / v1.9.9.9f / Adam D. Moss  *       Use GIMP_OVERLAY_MODE if available.  *  *  1998.07.31 / v1.9.9.9e / Adam D. Moss  *       Worked around some buggy PSD savers (suspect PS4 on Mac) - ugh.  *       Fixed a bug when loading layer masks of certain dimensions.  *  *  1998.05.04 / v1.9.9.9b / Adam D. Moss  *       Changed the Pascal-style string-reading stuff.  That fixed  *       some file-padding problems.  Made all debugging output  *       compile-time optional (please leave it enabled for now).  *       Reduced memory requirements; still much room for improvement.  *  *  1998.04.28 / v1.9.9.9 / Adam D. Moss  *       Fixed the correct channel interlacing of 'raw' flat images.  *       Thanks to Christian Kirsch and Jay Cox for spotting this.  *       Changed some of the I/O routines.  *  *  1998.04.26 / v1.9.9.8 / Adam D. Moss  *       Implemented Aux-channels for layered files.  Got rid  *       of<endian.h> nonsense.  Improved Layer Mask padding.  *       Enforced num_layers/num_channels limit checks.  *  *  1998.04.23 / v1.9.9.5 / Adam D. Moss  *       Got Layer Masks working, got Aux-channels working  *       for unlayered files, fixed 'raw' channel loading, fixed  *       some other mini-bugs, slightly better progress meters.  *       Thanks to everyone who is helping with the testing!  *  *  1998.04.21 / v1.9.9.1 / Adam D. Moss  *       A little cleanup.  Implemented Layer Masks but disabled  *       them again - PS masks can be a different size to their  *       owning layer, unlike those in GIMP.  *  *  1998.04.19 / v1.9.9.0 / Adam D. Moss  *       Much happier now.  *  *  1997.03.13 / v1.9.0 / Adam D. Moss  *       Layers, channels and masks, oh my.  *       + Bugfixes& rearchitecturing.  *  *  1997.01.30 / v1.0.12 / Torsten Martinsen  *       Flat PSD image loading.  */
+comment|/*  * Revision history:  *  *  2000.08.23 / v2.0.6 / Adam D. Moss  *       Eliminate more debugging output (don't people have more  *       substantial problems to report?  I'm poised at my keyboard).  *  *  1999.11.14 / v2.0.5 / Adam D. Moss  *       Applied patch by Andy Hefner to load 1-bit images.  *  *  1999.08.13 / v2.0.4 / Adam D. Moss  *       Allowed NULL layer names again, whee.  (Also fixed the time machine.)  *  *  1999.08.20 / v2.0.3 / Adam D. Moss  *       Ensure that NULL name does not get passed to gimp_layer_new(),  *       or it will fail to create the layer and cause problems down  *       the line (only since April 1999).  *  *  1999.01.18 / v2.0.2 / Adam D. Moss  *       Better guess at how PSD files store Guide position precision.  *  *  1999.01.10 / v2.0.1 / Adam D. Moss  *       Greatly reduced memory requirements for layered image loading -  *       we now do just-in-time channel unpacking.  Some little  *       cleanups too.  *  *  1998.09.04 / v2.0.0 / Adam D. Moss  *       Now recognises and loads the new Guides extensions written  *       by Photoshop 4 and 5.  *  *  1998.07.31 / v1.9.9.9f / Adam D. Moss  *       Use GIMP_OVERLAY_MODE if available.  *  *  1998.07.31 / v1.9.9.9e / Adam D. Moss  *       Worked around some buggy PSD savers (suspect PS4 on Mac) - ugh.  *       Fixed a bug when loading layer masks of certain dimensions.  *  *  1998.05.04 / v1.9.9.9b / Adam D. Moss  *       Changed the Pascal-style string-reading stuff.  That fixed  *       some file-padding problems.  Made all debugging output  *       compile-time optional (please leave it enabled for now).  *       Reduced memory requirements; still much room for improvement.  *  *  1998.04.28 / v1.9.9.9 / Adam D. Moss  *       Fixed the correct channel interlacing of 'raw' flat images.  *       Thanks to Christian Kirsch and Jay Cox for spotting this.  *       Changed some of the I/O routines.  *  *  1998.04.26 / v1.9.9.8 / Adam D. Moss  *       Implemented Aux-channels for layered files.  Got rid  *       of<endian.h> nonsense.  Improved Layer Mask padding.  *       Enforced num_layers/num_channels limit checks.  *  *  1998.04.23 / v1.9.9.5 / Adam D. Moss  *       Got Layer Masks working, got Aux-channels working  *       for unlayered files, fixed 'raw' channel loading, fixed  *       some other mini-bugs, slightly better progress meters.  *       Thanks to everyone who is helping with the testing!  *  *  1998.04.21 / v1.9.9.1 / Adam D. Moss  *       A little cleanup.  Implemented Layer Masks but disabled  *       them again - PS masks can be a different size to their  *       owning layer, unlike those in GIMP.  *  *  1998.04.19 / v1.9.9.0 / Adam D. Moss  *       Much happier now.  *  *  1997.03.13 / v1.9.0 / Adam D. Moss  *       Layers, channels and masks, oh my.  *       + Bugfixes& rearchitecturing.  *  *  1997.01.30 / v1.0.12 / Torsten Martinsen  *       Flat PSD image loading.  */
 end_comment
 
 begin_comment
-comment|/*  * TODO:  *  *      Crush 16bpp channels *  *	CMYK -> RGB *  *      * I don't think these should be done lossily -- wait for  *        GIMP to be able to support them natively.  *  *      Read in the paths.  *  *      File saving (accepting sponsors!)  */
+comment|/*  * TODO:  *  *      Crush 16bpp channels *  *	CMYK -> RGB *  *      * I don't think these should be done lossily -- wait for  *        GIMP to be able to support them natively.  *  *      Read in the paths.  *  *      File saving (someone has an alpha plugin for this)  */
 end_comment
 
 begin_comment
@@ -149,7 +149,7 @@ end_comment
 begin_typedef
 typedef|typedef
 enum|enum
-DECL|enum|__anon274f169d0103
+DECL|enum|__anon2b4444b00103
 block|{
 DECL|enumerator|PSD_UNKNOWN_IMAGE
 name|PSD_UNKNOWN_IMAGE
@@ -339,7 +339,7 @@ end_typedef
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon274f169d0208
+DECL|struct|__anon2b4444b00208
 block|{
 DECL|member|hRes
 name|Fixed
@@ -589,7 +589,7 @@ end_decl_stmt
 begin_struct
 specifier|static
 struct|struct
-DECL|struct|__anon274f169d0308
+DECL|struct|__anon2b4444b00308
 block|{
 DECL|member|signature
 name|gchar
@@ -2516,7 +2516,18 @@ condition|(
 name|remaining
 condition|)
 block|{
+name|IFDBG
 name|dumpchunk
+argument_list|(
+name|remaining
+argument_list|,
+name|fd
+argument_list|,
+literal|"alphaname padding 0 throw"
+argument_list|)
+decl_stmt|;
+else|else
+name|throwchunk
 argument_list|(
 name|remaining
 argument_list|,
@@ -3439,11 +3450,12 @@ name|remaining
 condition|)
 block|{
 name|IFDBG
+block|{
 name|printf
 argument_list|(
 literal|"** GUIDE INFORMATION DROSS: "
 argument_list|)
-decl_stmt|;
+expr_stmt|;
 name|dumpchunk
 argument_list|(
 name|remaining
@@ -3453,6 +3465,19 @@ argument_list|,
 literal|"dispatch_res"
 argument_list|)
 expr_stmt|;
+block|}
+else|else
+block|{
+name|throwchunk
+argument_list|(
+name|remaining
+argument_list|,
+name|fd
+argument_list|,
+literal|"dispatch_res"
+argument_list|)
+expr_stmt|;
+block|}
 operator|(
 operator|*
 name|offset
@@ -3701,12 +3726,12 @@ expr_stmt|;
 break|break;
 default|default:
 name|IFDBG
+block|{
 name|printf
 argument_list|(
 literal|"\t\t<Undocumented field.>\n"
 argument_list|)
-decl_stmt|;
-name|IFDBG
+expr_stmt|;
 name|dumpchunk
 argument_list|(
 name|Size
@@ -3715,7 +3740,8 @@ name|fd
 argument_list|,
 literal|"dispatch_res"
 argument_list|)
-decl_stmt|;
+expr_stmt|;
+block|}
 else|else
 name|throwchunk
 argument_list|(
