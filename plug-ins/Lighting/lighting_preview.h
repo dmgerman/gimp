875290e51eb0e52c7cb70bca1d5b32b166fee5d6
Piers Cornwell <piers.cornwell@bigfoot.com>
2000-08-29 14:27:40 +0000

    plug-ins/Lighting/lighting_ui.c Make Env and Bumpmap tabs show all the
    2000-08-29  Piers Cornwell  <piers.cornwell@bigfoot.com>
    
        * plug-ins/Lighting/lighting_ui.c
        * plug-ins/Lighting/lighting_preview.h: Make Env and Bumpmap
        tabs show all the time and other minor changes.
    
    
    Former-commit-id: 8db327b5548b953bd3a723939e89f483639ae282

--- plug-ins/Lighting/lighting_preview.h
+++ plug-ins/Lighting/lighting_preview.h
@@ -1,4 +1,8 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* Lighting Effects - A plug-in for GIMP  *  * This program is free software; you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 2 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program; if not, write to the Free Software  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+end_comment
+
 begin_ifndef
 ifndef|#
 directive|ifndef
@@ -17,7 +21,7 @@ DECL|macro|PREVIEW_WIDTH
 define|#
 directive|define
 name|PREVIEW_WIDTH
-value|300
+value|200
 end_define
 
 begin_define
@@ -25,13 +29,13 @@ DECL|macro|PREVIEW_HEIGHT
 define|#
 directive|define
 name|PREVIEW_HEIGHT
-value|300
+value|200
 end_define
 
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2910a8820108
+DECL|struct|__anon28fd85760108
 block|{
 DECL|member|x
 DECL|member|y
@@ -59,10 +63,6 @@ end_typedef
 
 begin_comment
 comment|/* Externally visible variables */
-end_comment
-
-begin_comment
-comment|/* ============================ */
 end_comment
 
 begin_decl_stmt
@@ -94,10 +94,6 @@ end_decl_stmt
 
 begin_comment
 comment|/* Externally visible functions */
-end_comment
-
-begin_comment
-comment|/* ============================ */
 end_comment
 
 begin_function_decl
