d28200c01d0a4c59de18107a1ac458639368d2d5
Vidar Madsen/gimpressionist <vidar@src.gnome.org>
2000-12-13 09:52:15 +0000

    Small algo improvements.
    * plug-ins/common/spheredesigner.c: Small algo improvements.
    
    
    Former-commit-id: b2b5e03d57d1c96abb8a0ddd929d2fea2be8a9d3

--- plug-ins/common/spheredesigner.c
+++ plug-ins/common/spheredesigner.c
@@ -285,7 +285,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_enum
-DECL|enum|__anon29f4dc9b0103
+DECL|enum|__anon2b433e390103
 enum|enum
 block|{
 DECL|enumerator|TRIANGLE
@@ -310,7 +310,7 @@ enum|;
 end_enum
 
 begin_enum
-DECL|enum|__anon29f4dc9b0203
+DECL|enum|__anon2b433e390203
 enum|enum
 block|{
 DECL|enumerator|SOLID
@@ -359,7 +359,7 @@ enum|;
 end_enum
 
 begin_enum
-DECL|enum|__anon29f4dc9b0303
+DECL|enum|__anon2b433e390303
 enum|enum
 block|{
 DECL|enumerator|PERSPECTIVE
@@ -375,7 +375,7 @@ enum|;
 end_enum
 
 begin_enum
-DECL|enum|__anon29f4dc9b0403
+DECL|enum|__anon2b433e390403
 enum|enum
 block|{
 DECL|enumerator|FOG
@@ -421,7 +421,7 @@ value|0x00000001
 end_define
 
 begin_typedef
-DECL|struct|__anon29f4dc9b0508
+DECL|struct|__anon2b433e390508
 typedef|typedef
 struct|struct
 block|{
@@ -445,7 +445,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|struct|__anon29f4dc9b0608
+DECL|struct|__anon2b433e390608
 typedef|typedef
 struct|struct
 block|{
@@ -469,7 +469,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|struct|__anon29f4dc9b0708
+DECL|struct|__anon2b433e390708
 typedef|typedef
 struct|struct
 block|{
@@ -498,7 +498,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|struct|__anon29f4dc9b0808
+DECL|struct|__anon2b433e390808
 typedef|typedef
 struct|struct
 block|{
@@ -594,7 +594,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|struct|__anon29f4dc9b0908
+DECL|struct|__anon2b433e390908
 typedef|typedef
 struct|struct
 block|{
@@ -621,7 +621,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|struct|__anon29f4dc9b0a08
+DECL|struct|__anon2b433e390a08
 typedef|typedef
 struct|struct
 block|{
@@ -663,7 +663,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|struct|__anon29f4dc9b0b08
+DECL|struct|__anon2b433e390b08
 typedef|typedef
 struct|struct
 block|{
@@ -688,7 +688,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|struct|__anon29f4dc9b0c08
+DECL|struct|__anon2b433e390c08
 typedef|typedef
 struct|struct
 block|{
@@ -714,7 +714,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|struct|__anon29f4dc9b0d08
+DECL|struct|__anon2b433e390d08
 typedef|typedef
 struct|struct
 block|{
@@ -737,7 +737,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|struct|__anon29f4dc9b0e08
+DECL|struct|__anon2b433e390e08
 typedef|typedef
 struct|struct
 block|{
@@ -762,7 +762,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|struct|__anon29f4dc9b0f08
+DECL|struct|__anon2b433e390f08
 typedef|typedef
 struct|struct
 block|{
@@ -785,7 +785,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|struct|__anon29f4dc9b1008
+DECL|struct|__anon2b433e391008
 typedef|typedef
 struct|struct
 block|{
@@ -808,7 +808,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|struct|__anon29f4dc9b1108
+DECL|struct|__anon2b433e391108
 typedef|typedef
 struct|struct
 block|{
@@ -834,7 +834,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|union|__anon29f4dc9b120a
+DECL|union|__anon2b433e39120a
 typedef|typedef
 union|union
 block|{
@@ -1170,7 +1170,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_struct
-DECL|struct|__anon29f4dc9b1308
+DECL|struct|__anon2b433e391308
 struct|struct
 block|{
 DECL|member|solid
@@ -7879,61 +7879,6 @@ operator|.
 name|v2
 argument_list|)
 expr_stmt|;
-if|if
-condition|(
-name|world
-operator|.
-name|quality
-operator|>=
-literal|3
-condition|)
-block|{
-name|o
-operator|=
-literal|0
-expr_stmt|;
-if|if
-condition|(
-operator|!
-operator|(
-name|world
-operator|.
-name|light
-index|[
-name|i
-index|]
-operator|.
-name|com
-operator|.
-name|flags
-operator|&
-name|NOSHADOW
-operator|)
-condition|)
-name|o
-operator|=
-name|traceray
-argument_list|(
-operator|&
-name|r
-argument_list|,
-name|NULL
-argument_list|,
-operator|-
-literal|1
-argument_list|,
-literal|1.0
-argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|o
-condition|)
-block|{
-continue|continue;
-block|}
-block|}
-comment|/* OK, light is visible */
 name|vsub
 argument_list|(
 operator|&
@@ -7970,13 +7915,13 @@ operator|&
 name|norm
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
 name|b
-operator|=
-name|fabs
-argument_list|(
-name|b
-argument_list|)
-expr_stmt|;
+operator|<
+literal|0.0
+condition|)
+continue|continue;
 for|for
 control|(
 name|j
