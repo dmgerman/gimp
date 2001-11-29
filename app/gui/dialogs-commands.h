1002b0c47f9b2a6a4c686ac11ae1d28cd8b4637d
Michael Natterer <mitch@gimp.org>
2001-11-29 18:11:31 +0000

    put the last_opened menu entries in a submenu called "Open Recent", added
    2001-11-29  Michael Natterer  <mitch@gimp.org>
    
    	* app/gui/menus.c: put the last_opened menu entries in a
    	submenu called "Open Recent", added items to create a dock
    	with some useful tabs in it ("Brushes, Patterns & Stuff..."),
    
    	* app/gui/dialogs-commands.[ch]: added a callback for the new
    	"Stuff" menu entry.
    
    	* app/gui/dialogs-constructors.h: removed dialogs_lc_get()
    	declaration because it doesn't exist.
    
    
    Former-commit-id: b017fa15389222f37d70519ccbef7578d8013359

--- app/gui/dialogs-commands.h
+++ app/gui/dialogs-commands.h
@@ -169,6 +169,23 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|void
+name|dialogs_create_stuff_cmd_callback
+parameter_list|(
+name|GtkWidget
+modifier|*
+name|widget
+parameter_list|,
+name|gpointer
+name|data
+parameter_list|,
+name|guint
+name|action
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
