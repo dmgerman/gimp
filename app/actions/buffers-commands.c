26e2620256059b2f22ec5ec02741e12aeb3f2438
Michael Natterer <mitch@gimp.org>
2002-01-30 17:39:56 +0000

    derive from GtkDrawingArea instead of deprecated GtkPreview.
    2002-01-30  Michael Natterer  <mitch@gimp.org>
    
    	* app/widgets/gimppreview.[ch]: derive from GtkDrawingArea
    	instead of deprecated GtkPreview.
    
    	* app/gui/buffers-commands.c
    	* app/gui/device-status-dialog.c
    	* app/gui/dialogs-constructors.c
    	* app/gui/indicator-area.c
    	* app/gui/info-window.c
    	* app/gui/palette-import-dialog.c
    	* app/gui/palettes-commands.c
    	* app/gui/test-commands.c
    	* app/gui/tool-options-dialog.c
    	* app/gui/toolbox.c
    	* app/widgets/gimpdnd.c
    	* app/widgets/gimpimagedock.c
    	* app/widgets/gimpmenuitem.c: removed #undef GTK_DISABLE_DEPRECATED.
    
    
    Former-commit-id: 074b1cda76fbd3d95c915da88566d1c80f0d426f

--- app/actions/buffers-commands.c
+++ app/actions/buffers-commands.c
@@ -9,29 +9,6 @@ directive|include
 file|"config.h"
 end_include
 
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|__GNUC__
-end_ifdef
-
-begin_warning
-warning|#
-directive|warning
-warning|GTK_DISABLE_DEPRECATED
-end_warning
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_undef
-undef|#
-directive|undef
-name|GTK_DISABLE_DEPRECATED
-end_undef
-
 begin_include
 include|#
 directive|include
@@ -90,12 +67,6 @@ begin_include
 include|#
 directive|include
 file|"widgets/gimplistitem.h"
-end_include
-
-begin_include
-include|#
-directive|include
-file|"widgets/gimppreview.h"
 end_include
 
 begin_include
