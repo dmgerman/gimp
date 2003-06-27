cb3f5eb86324b97575a6c91a09397c4554c3ac39
Michael Natterer <mitch@gimp.org>
2003-06-27 14:18:54 +0000

    #include "config/gimpconfig.h"
    2003-06-27  Michael Natterer  <mitch@gimp.org>
    
    	* app/gui/grid-dialog.c: #include "config/gimpconfig.h"
    
    
    Former-commit-id: b264fa1f4c481f9e05e5e72fda222c2c590aa966

--- app/gui/grid-dialog.c
+++ app/gui/grid-dialog.c
@@ -36,6 +36,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|"config/gimpconfig.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|"config/gimpconfig-types.h"
 end_include
 
