3f5c5a8a7f1ced1f762b6e7557113a37a86ed678
Sven Neumann <sven@gimp.org>
2003-07-15 00:29:34 +0000

    include <libgimpcolor/gimpcmyk.h>.
    2003-07-15  Sven Neumann  <sven@gimp.org>
    
    	* libgimpcolor/gimpcolor.h: include <libgimpcolor/gimpcmyk.h>.
    
    	* modules/Makefile.am
    	* modules/colorsel_cmyk.c: added a simple CMYK color selector.
    
    
    Former-commit-id: 2a72abfd9352f9254ffe03779b8e5e592389ca76

--- libgimpcolor/gimpcolor.h
+++ libgimpcolor/gimpcolor.h
@@ -43,6 +43,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<libgimpcolor/gimpcmyk.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<libgimpcolor/gimphsv.h>
 end_include
 
