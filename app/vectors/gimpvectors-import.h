10ec88b34a6325d15730b5fdfcb1230e56fc33b4
Sven Neumann <sven@gimp.org>
2003-09-10 22:11:02 +0000

    app/vectors/gimpvectors-import.[ch] allow to optionally create a
    2003-09-11  Sven Neumann  <sven@gimp.org>
    
    	* app/vectors/gimpvectors-import.[ch]
    	* app/gui/vectors-commands.c: allow to optionally create a
    	GimpVectors object per <path> element. Made this the default until
    	there's a GUI for this.
    
    
    Former-commit-id: 0037f5a7ea8a88a1ec5024630c032dfc4b8ce66d

--- app/vectors/gimpvectors-import.h
+++ app/vectors/gimpvectors-import.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* The GIMP -- an image manipulation program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * This program is free software; you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 2 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program; if not, write to the Free Software  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+comment|/* The GIMP -- an image manipulation program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * GimpVectors Import  * Copyright (C) 2003  Sven Neumann<sven@gimp.org>  *  * This program is free software; you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 2 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program; if not, write to the Free Software  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 end_comment
 
 begin_ifndef
@@ -17,8 +17,7 @@ name|__GIMP_VECTORS_IMPORT_H__
 end_define
 
 begin_function_decl
-name|GimpVectors
-modifier|*
+name|gboolean
 name|gimp_vectors_import
 parameter_list|(
 name|GimpImage
@@ -29,6 +28,9 @@ specifier|const
 name|gchar
 modifier|*
 name|filename
+parameter_list|,
+name|gboolean
+name|merge
 parameter_list|,
 name|GError
 modifier|*
