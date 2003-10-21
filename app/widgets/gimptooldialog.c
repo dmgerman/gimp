1ef1e987a13cd95c1bf7755b8e2d5fe38535aca6
Sven Neumann <sven@gimp.org>
2003-10-21 11:31:49 +0000

    documented gimp_tool_dialog_new().
    2003-10-21  Sven Neumann  <sven@gimp.org>
    
    	* app/widgets/gimptooldialog.c: documented gimp_tool_dialog_new().
    
    
    Former-commit-id: 55fd373096927e9e897bcd09911d971007db2ae8

--- app/widgets/gimptooldialog.c
+++ app/widgets/gimptooldialog.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* The GIMP -- an image manipulation program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * gimpviewabledialog.c  * Copyright (C) 2000 Michael Natterer<mitch@gimp.org>  *  * This program is free software; you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 2 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program; if not, write to the Free Software  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+comment|/* The GIMP -- an image manipulation program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * gimptooldialog.c  * Copyright (C) 2003  Sven Neumann<sven@gimp.org>  *  * This program is free software; you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 2 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program; if not, write to the Free Software  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 end_comment
 
 begin_include
@@ -56,6 +56,10 @@ include|#
 directive|include
 file|"gimpviewabledialog.h"
 end_include
+
+begin_comment
+comment|/**  * gimp_tool_dialog_new:  * @tool_info: a #GimpToolInfo  * @desc: a string to use in the dialog header or %NULL to use the help  *        field from #GimpToolInfo  *  * This function conveniently creates a #GimpViewableDialog using the  * information stored in @tool_info. It also registers the tool with  * the "toplevel" dialog factory.  *  * Return value: a new #GimpViewableDialog  **/
+end_comment
 
 begin_function
 name|GtkWidget
