775c3887bd1f3b032646269178b32caa1b3f7861
Sven Neumann <sven@gimp.org>
2003-11-22 00:01:49 +0000

    let the templates editor expand.
    2003-11-22  Sven Neumann  <sven@gimp.org>
    
    	* app/gui/preferences-dialog.c: let the templates editor expand.
    
    
    Former-commit-id: fafc492a0e80c940053f164f71b7702afed0fd7a

--- app/dialogs/preferences-dialog.c
+++ app/dialogs/preferences-dialog.c
@@ -4901,9 +4901,9 @@ argument_list|)
 argument_list|,
 name|editor
 argument_list|,
-name|FALSE
+name|TRUE
 argument_list|,
-name|FALSE
+name|TRUE
 argument_list|,
 literal|0
 argument_list|)
@@ -6798,7 +6798,7 @@ argument_list|)
 block|}
 decl_stmt|;
 struct|struct
-DECL|struct|__anon2bb7f4e50108
+DECL|struct|__anon2b1d88b50108
 block|{
 DECL|member|current_setting
 name|gchar
@@ -9239,7 +9239,7 @@ block|{
 specifier|static
 specifier|const
 struct|struct
-DECL|struct|__anon2bb7f4e50208
+DECL|struct|__anon2b1d88b50208
 block|{
 DECL|member|label
 specifier|const
@@ -9398,7 +9398,7 @@ block|{
 specifier|static
 specifier|const
 struct|struct
-DECL|struct|__anon2bb7f4e50308
+DECL|struct|__anon2b1d88b50308
 block|{
 DECL|member|tree_label
 specifier|const
