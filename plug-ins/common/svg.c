9850a275ede9c64f12d095ab34350c873faec4cf
Sven Neumann <sven@gimp.org>
2003-11-26 20:22:18 +0000

    fixed SVG import preview for portrait SVG images.
    2003-11-26  Sven Neumann  <sven@gimp.org>
    
    	* plug-ins/common/svg.c (load_set_size_callback): fixed SVG import
    	preview for portrait SVG images.
    
    
    Former-commit-id: f659de8b745518aaff5737c909fe63f62ea086b0

--- plug-ins/common/svg.c
+++ plug-ins/common/svg.c
@@ -90,7 +90,7 @@ end_define
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2c7e2a830108
+DECL|struct|__anon28fbe72d0108
 block|{
 DECL|member|resolution
 name|gdouble
@@ -1329,7 +1329,7 @@ name|vals
 operator|->
 name|width
 argument_list|)
-operator|/
+operator|*
 operator|(
 name|w
 operator|/
