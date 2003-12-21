4918750625666e64c883f360140e8e5a66e884b3
Sven Neumann <sven@gimp.org>
2003-12-21 11:52:20 +0000

    removed the GError parameter from gimp_vectors_export_string() and
    2003-12-21  Sven Neumann  <sven@gimp.org>
    
    	* app/vectors/gimpvectors-export.[ch]: removed the GError parameter
    	from gimp_vectors_export_string() and implemented this function.
    
    
    Former-commit-id: 9630d408340731bc804546d25a2c94224ac03bad

--- app/vectors/gimpvectors-export.h
+++ app/vectors/gimpvectors-export.h
@@ -57,11 +57,6 @@ specifier|const
 name|GimpVectors
 modifier|*
 name|vectors
-parameter_list|,
-name|GError
-modifier|*
-modifier|*
-name|error
 parameter_list|)
 function_decl|;
 end_function_decl
