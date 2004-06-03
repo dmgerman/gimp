8599f8e12681c5c71bdc6a0acca5117f492c29c5
Michael Natterer <mitch@gimp.org>
2004-06-03 09:47:05 +0000

    added alternative actions "vectors-selection-from-vectors" and
    2004-06-03  Michael Natterer  <mitch@gimp.org>
    
    	* app/actions/vectors-actions.c: added alternative actions
    	"vectors-selection-from-vectors" and
    	"vectors-selection-to-vectors-short" with different labels suited
    	for the "Select" menu.
    
    	* app/actions/select-actions.c: removed "select-from-vectors"
    	and "select-to-vectors" (to vectors was crashing anyway).
    
    	* app/actions/select-commands.[ch]: removed
    	select_from_vectors_cmd_callback(). Fixes code dupliction.
    
    	* menus/image-menu.xml.in
    	* menus/selection-editor-menu.xml: changed accordingly.
    
    
    Former-commit-id: 421b2a0d0e6e7bd11fab946adf22d82a3498e588

--- app/actions/select-commands.h
+++ app/actions/select-commands.h
@@ -60,20 +60,6 @@ end_function_decl
 
 begin_function_decl
 name|void
-name|select_from_vectors_cmd_callback
-parameter_list|(
-name|GtkAction
-modifier|*
-name|action
-parameter_list|,
-name|gpointer
-name|data
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|void
 name|select_float_cmd_callback
 parameter_list|(
 name|GtkAction
