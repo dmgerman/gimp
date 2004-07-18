8d77c80cb65b9524deb9cb9b15514525268086df
Sven Neumann <sven@gimp.org>
2004-07-18 18:24:32 +0000

    don't start a comment with /** unless it's meant to be parsed by gtk-doc.
    2004-07-18  Sven Neumann  <sven@gimp.org>
    
    	* app/composite/gimp-composite-mmx.h: don't start a comment with
    	/** unless it's meant to be parsed by gtk-doc.
    
    	* app/actions/Makefile.am:
    	* app/actions/file-dialog-commands.[ch]: removed, not used any
    	longer.
    
    
    Former-commit-id: 55c03a25b56ed6a65ee7426ce7037e1683503d68

--- app/composite/gimp-composite-mmx.h
+++ app/composite/gimp-composite-mmx.h
@@ -37,7 +37,7 @@ function_decl|;
 end_function_decl
 
 begin_comment
-comment|/**  * Rummage through the compile-time environment to ensure this file  * can actually be compiled like we expect it to.  If all is okay,  * define the manifest constant COMPILE_IS_OKAY.  */
+comment|/*  * Rummage through the compile-time environment to ensure this file  * can actually be compiled like we expect it to.  If all is okay,  * define the manifest constant COMPILE_IS_OKAY.  */
 end_comment
 
 begin_if
