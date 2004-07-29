fdd0653640499459bc3a095d34506d9160de33c6
Sven Neumann <sven@gimp.org>
2004-07-29 17:10:28 +0000

    libgimpwidgets/Makefile.am libgimpwidgets/gimpwidgets.def
    2004-07-29  Sven Neumann  <sven@gimp.org>
    
    	* libgimpwidgets/Makefile.am
    	* libgimpwidgets/gimpwidgets.def
    	* libgimpwidgets/gimpwidgets.h
    	* libgimpwidgets/gimpwidgetstypes.h
    	* libgimpwidgets/gimppreviewarea.[ch]: added GimpPreviewArea, a
    	replacement for GtkPreview, loosely based on patches from Geert
    	Jordaens and David Odin. Fixes bug #144759.
    
    	* plug-ins/common/sharpen.c: use the new widget instead of a
    	GtkPreview; saves about 100 lines of rather complex code :)
    
    
    Former-commit-id: c307429a2440fa9cf0f98129b973e2024c8ebf56

--- libgimpwidgets/gimpwidgetstypes.h
+++ libgimpwidgets/gimpwidgetstypes.h
@@ -33,7 +33,7 @@ end_comment
 begin_typedef
 typedef|typedef
 enum|enum
-DECL|enum|__anon2b6826c70103
+DECL|enum|__anon2be376ed0103
 block|{
 DECL|enumerator|GIMP_COLOR_AREA_FLAT
 name|GIMP_COLOR_AREA_FLAT
@@ -54,7 +54,7 @@ end_typedef
 begin_typedef
 typedef|typedef
 enum|enum
-DECL|enum|__anon2b6826c70203
+DECL|enum|__anon2be376ed0203
 block|{
 DECL|enumerator|GIMP_COLOR_SELECTOR_HUE
 name|GIMP_COLOR_SELECTOR_HUE
@@ -85,7 +85,7 @@ end_typedef
 begin_typedef
 typedef|typedef
 enum|enum
-DECL|enum|__anon2b6826c70303
+DECL|enum|__anon2be376ed0303
 block|{
 DECL|enumerator|GIMP_SIZE_ENTRY_UPDATE_NONE
 name|GIMP_SIZE_ENTRY_UPDATE_NONE
@@ -329,6 +329,15 @@ typedef|typedef
 name|struct
 name|_GimpPickButton
 name|GimpPickButton
+typedef|;
+end_typedef
+
+begin_typedef
+DECL|typedef|GimpPreviewArea
+typedef|typedef
+name|struct
+name|_GimpPreviewArea
+name|GimpPreviewArea
 typedef|;
 end_typedef
 
