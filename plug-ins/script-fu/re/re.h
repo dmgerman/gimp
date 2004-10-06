536151e6e39c58a4f8134f275d6ada0108da9eca
Kevin Cozens <kcozens@src.gnome.org>
2004-10-06 21:47:36 +0000

    Initial revision
    Former-commit-id: 5a420188add2999204ad17b95a80d5fc7f6a6c88

---
+++ plug-ins/script-fu/re/re.h
@@ -0,0 +1,18 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* This function gets called when TinyScheme is initializing the extension */
+end_comment
+
+begin_function_decl
+name|void
+name|init_re
+parameter_list|(
+name|scheme
+modifier|*
+name|sc
+parameter_list|)
+function_decl|;
+end_function_decl
+
+end_unit
+
