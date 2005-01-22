4d0f631d71874cdf91ab3d82b1d0b490138baba9
Manish Singh <yosh@gimp.org>
2005-01-22 02:33:29 +0000

    #include gimpbase.h for declaration of gimp_param_spec_unit().
    2005-01-21  Manish Singh  <yosh@gimp.org>
    
            * app/display/gimpdisplayshell.c: #include gimpbase.h for declaration
            of gimp_param_spec_unit().
    
    
    Former-commit-id: 1960d78cced11657c3b9a072eb505ea8ff6bb7c9

--- app/display/gimpdisplayshell.c
+++ app/display/gimpdisplayshell.c
@@ -24,6 +24,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|"libgimpbase/gimpbase.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|"libgimpwidgets/gimpwidgets.h"
 end_include
 
@@ -280,7 +286,7 @@ end_include
 
 begin_enum
 enum|enum
-DECL|enum|__anon27d6f4100103
+DECL|enum|__anon2988d5670103
 block|{
 DECL|enumerator|PROP_0
 name|PROP_0
@@ -296,7 +302,7 @@ end_enum
 
 begin_enum
 enum|enum
-DECL|enum|__anon27d6f4100203
+DECL|enum|__anon2988d5670203
 block|{
 DECL|enumerator|SCALED
 name|SCALED
