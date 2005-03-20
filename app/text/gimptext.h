1e6362333da4d73612bccecf11c5a68686f5d7ca
Sven Neumann <sven@gimp.org>
2005-03-20 01:56:58 +0000

    app/text/gimptext.[ch] some infrastructure for text transforms.
    2005-03-20  Sven Neumann  <sven@gimp.org>
    
    	* app/text/gimptext.[ch]
    	* app/text/gimptextlayer-transform.[ch]: some infrastructure for
    	text transforms.
    
    
    Former-commit-id: 00338c9ef4d0b82ea398d3770f05583490b30f7f

--- app/text/gimptext.h
+++ app/text/gimptext.h
@@ -229,6 +229,21 @@ name|G_GNUC_CONST
 decl_stmt|;
 end_decl_stmt
 
+begin_function_decl
+name|void
+name|gimp_text_get_transformation
+parameter_list|(
+name|GimpText
+modifier|*
+name|text
+parameter_list|,
+name|GimpMatrix3
+modifier|*
+name|matrix
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
