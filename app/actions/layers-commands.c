f3674ab81f7ccf0d5c2d706789f9031170422688
Sven Neumann <sven@gimp.org>
2005-03-24 10:34:41 +0000

    app/actions/layers-commands.c app/core/core-enums.[ch]
    2005-03-24  Sven Neumann  <sven@gimp.org>
    
    	* app/actions/layers-commands.c
    	* app/core/core-enums.[ch]
    	* app/core/gimpimage-undo-push.[ch]
    	* app/pdb/pdb_glue.h: reverted Bill's changes to fix the build.
    
    
    Former-commit-id: 4314dca031d0316c628bce99ca9d00c614d8a481

--- app/actions/layers-commands.c
+++ app/actions/layers-commands.c
@@ -90,12 +90,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|"core/gimpimage-undo-push.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"core/gimpitemundo.h"
 end_include
 
@@ -2375,8 +2369,6 @@ argument_list|(
 name|mask
 argument_list|,
 name|active
-argument_list|,
-name|TRUE
 argument_list|)
 expr_stmt|;
 name|gimp_image_flush
@@ -2452,8 +2444,6 @@ argument_list|(
 name|mask
 argument_list|,
 name|active
-argument_list|,
-name|TRUE
 argument_list|)
 expr_stmt|;
 name|gimp_image_flush
@@ -2524,24 +2514,12 @@ name|action
 argument_list|)
 argument_list|)
 expr_stmt|;
-if|if
-condition|(
-name|active
-operator|==
-name|gimp_layer_mask_get_apply
-argument_list|(
-name|mask
-argument_list|)
-condition|)
-block|{
 name|gimp_layer_mask_set_apply
 argument_list|(
 name|mask
 argument_list|,
 operator|!
 name|active
-argument_list|,
-name|TRUE
 argument_list|)
 expr_stmt|;
 name|gimp_image_flush
@@ -2549,7 +2527,6 @@ argument_list|(
 name|gimage
 argument_list|)
 expr_stmt|;
-block|}
 block|}
 block|}
 end_function
