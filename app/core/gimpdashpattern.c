83ad4585e045f8547809747df5c53fec7d3e4ba6
Michael Natterer <mitch@gimp.org>
2005-05-20 15:30:57 +0000

    don't include "libgimpbase/gimpbase.h"
    2005-05-20  Michael Natterer  <mitch@gimp.org>
    
    	* app/core/gimpdashpattern.c: don't include "libgimpbase/gimpbase.h"
    
    
    Former-commit-id: 05b0266911bb9cf592c090760af2a1ba6cf7053c

--- app/core/gimpdashpattern.c
+++ app/core/gimpdashpattern.c
@@ -18,12 +18,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|"libgimpbase/gimpbase.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"core-types.h"
 end_include
 
