08a403f280e29363a3b8699b9da932827964b4fa
Michael Natterer <mitch@gimp.org>
2005-09-10 22:50:57 +0000

    take the container's border_width into account.
    2005-09-11  Michael Natterer  <mitch@gimp.org>
    
    	* app/widgets/gimpcontainerbox.c
    	(gimp_container_box_set_size_request): take the container's
    	border_width into account.
    
    	* app/widgets/gimpcontainergridview.c
    	(gimp_container_grid_view_init): make sure GTK_SHADOW_IN is set on
    	the scrolled window, not on the viewport, so we get the same
    	results for list and grid views when using
    	gimp_container_box_set_size_request().
    
    	* app/widgets/gimpcontainerpopup.[ch]: added setters and getters
    	for view_type and preview_size, don't allow the preview to grow
    	larger than the popup.
    
    	* app/widgets/gimpviewablebutton.[ch]: added "popup-view-type" and
    	"popup-preview-size" properties and setters/getters.
    
    
    Former-commit-id: 1db678df125dcbdfed543adf56b9c095c4d4815b

--- app/widgets/gimpviewablebutton.h
+++ app/widgets/gimpviewablebutton.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* The GIMP -- an image manipulation program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * gimpviewablebutton.h  * Copyright (C) 2003 Michael Natterer<mitch@gimp.org>  *  * This program is free software; you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 2 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program; if not, write to the Free Software  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+comment|/* The GIMP -- an image manipulation program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * gimpviewablebutton.h  * Copyright (C) 2003-2005 Michael Natterer<mitch@gimp.org>  *  * This program is free software; you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 2 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program; if not, write to the Free Software  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 end_comment
 
 begin_ifndef
@@ -227,6 +227,17 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
+name|GimpViewType
+name|gimp_viewable_button_get_view_type
+parameter_list|(
+name|GimpViewableButton
+modifier|*
+name|button
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
 name|void
 name|gimp_viewable_button_set_view_type
 parameter_list|(
@@ -236,6 +247,31 @@ name|button
 parameter_list|,
 name|GimpViewType
 name|view_type
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|gint
+name|gimp_viewable_button_get_preview_size
+parameter_list|(
+name|GimpViewableButton
+modifier|*
+name|button
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|void
+name|gimp_viewable_button_set_preview_size
+parameter_list|(
+name|GimpViewableButton
+modifier|*
+name|button
+parameter_list|,
+name|gint
+name|preview_size
 parameter_list|)
 function_decl|;
 end_function_decl
