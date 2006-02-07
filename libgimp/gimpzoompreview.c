c4db211f2ffd243a1171d9221db7544f13d774d9
Sven Neumann <neo@src.gnome.org>
2006-02-07 15:54:10 +0000

    removed unused import
    Former-commit-id: ef1c9dc84092ec5a944fc16815cfc100b74fc06a

--- libgimp/gimpzoompreview.c
+++ libgimp/gimpzoompreview.c
@@ -36,12 +36,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|"libgimp-intl.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"gimpdrawablepreview.h"
 end_include
 
