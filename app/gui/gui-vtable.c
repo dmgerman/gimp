92e0f2df4ec206ce55516632dcf6fe63b22d791d
Michael Natterer <mitch@gimp.org>
2006-05-09 11:05:44 +0000

    don't #include "pdb/gimppluginprocedure.h".
    2006-05-09  Michael Natterer  <mitch@gimp.org>
    
    	* app/gui/gui-vtable.c: don't #include "pdb/gimppluginprocedure.h".
    
    
    Former-commit-id: b436bf2e722d82c3bca5ec9128ae08c3b5fdf2b6

--- app/gui/gui-vtable.c
+++ app/gui/gui-vtable.c
@@ -108,12 +108,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|"pdb/gimppluginprocedure.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"widgets/gimpactiongroup.h"
 end_include
 
