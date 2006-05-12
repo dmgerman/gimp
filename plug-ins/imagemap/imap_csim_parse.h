d364daf1c001bf922f9344caf8fdf9000e0e1193
Michael Natterer <mitch@gimp.org>
2006-05-12 15:32:23 +0000

    fix warning about unused return value of g_list_append().
    2006-05-12  Michael Natterer  <mitch@gimp.org>
    
    	* plug-ins/imagemap/imap_csim.y: fix warning about unused return
    	value of g_list_append().
    
    	* plug-ins/imagemap/imap_csim_parse.[ch]: regenerated (using
    	bison 2.1 instead of 2.0, lots of things changed, please test).
    
    
    Former-commit-id: 67f9321bc728ba89f409358ea2ce9914b0ebb6c3

--- plug-ins/imagemap/imap_csim_parse.h
+++ plug-ins/imagemap/imap_csim_parse.h
@@ -1,10 +1,10 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* A Bison parser, made by GNU Bison 2.0.  */
+comment|/* A Bison parser, made by GNU Bison 2.1.  */
 end_comment
 
 begin_comment
-comment|/* Skeleton parser for Yacc-like parsing with Bison,    Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004 Free Software Foundation, Inc.     This program is free software; you can redistribute it and/or modify    it under the terms of the GNU General Public License as published by    the Free Software Foundation; either version 2, or (at your option)    any later version.     This program is distributed in the hope that it will be useful,    but WITHOUT ANY WARRANTY; without even the implied warranty of    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    GNU General Public License for more details.     You should have received a copy of the GNU General Public License    along with this program; if not, write to the Free Software    Foundation, Inc., 59 Temple Place - Suite 330,    Boston, MA 02111-1307, USA.  */
+comment|/* Skeleton parser for Yacc-like parsing with Bison,    Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005 Free Software Foundation, Inc.     This program is free software; you can redistribute it and/or modify    it under the terms of the GNU General Public License as published by    the Free Software Foundation; either version 2, or (at your option)    any later version.     This program is distributed in the hope that it will be useful,    but WITHOUT ANY WARRANTY; without even the implied warranty of    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    GNU General Public License for more details.     You should have received a copy of the GNU General Public License    along with this program; if not, write to the Free Software    Foundation, Inc., 51 Franklin Street, Fifth Floor,    Boston, MA 02110-1301, USA.  */
 end_comment
 
 begin_comment
@@ -174,6 +174,10 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_comment
+comment|/* Tokens.  */
+end_comment
 
 begin_define
 DECL|macro|IMG
@@ -435,13 +439,13 @@ typedef|;
 end_typedef
 
 begin_comment
-comment|/* Line 1318 of yacc.c.  */
+comment|/* Line 1447 of yacc.c.  */
 end_comment
 
 begin_line
 line|#
 directive|line
-number|99
+number|100
 file|"y.tab.h"
 end_line
 
