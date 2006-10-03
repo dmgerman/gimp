e3b57a6a3f7d194fe30211a3514767c2587a0a33
Michael Natterer <mitch@gimp.org>
2006-10-03 14:54:07 +0000

    added gimp_palette_load_detect_format().
    2006-10-03  Michael Natterer  <mitch@gimp.org>
    
    	* app/core/gimppalette-load.[ch]: added
    	gimp_palette_load_detect_format().
    
    	* app/core/gimppalette-import.c: removed it here. Also removed all
    	file I/O related includes.
    
    
    Former-commit-id: c6ae0a7918d9c77dd904ec6cae62960f323e3afa

--- app/core/gimppalette-load.h
+++ app/core/gimppalette-load.h
@@ -24,6 +24,35 @@ name|GIMP_PALETTE_FILE_EXTENSION
 value|".gpl"
 end_define
 
+begin_typedef
+typedef|typedef
+enum|enum
+DECL|enum|__anon275d20d70103
+block|{
+DECL|enumerator|GIMP_PALETTE_FILE_FORMAT_UNKNOWN
+name|GIMP_PALETTE_FILE_FORMAT_UNKNOWN
+block|,
+DECL|enumerator|GIMP_PALETTE_FILE_FORMAT_GPL
+name|GIMP_PALETTE_FILE_FORMAT_GPL
+block|,
+comment|/* GIMP palette                        */
+DECL|enumerator|GIMP_PALETTE_FILE_FORMAT_RIFF_PAL
+name|GIMP_PALETTE_FILE_FORMAT_RIFF_PAL
+block|,
+comment|/* RIFF palette                        */
+DECL|enumerator|GIMP_PALETTE_FILE_FORMAT_ACT
+name|GIMP_PALETTE_FILE_FORMAT_ACT
+block|,
+comment|/* Photoshop binary color palette      */
+DECL|enumerator|GIMP_PALETTE_FILE_FORMAT_PSP_PAL
+name|GIMP_PALETTE_FILE_FORMAT_PSP_PAL
+comment|/* JASC's Paint Shop Pro color palette */
+DECL|typedef|GimpPaletteFileFormat
+block|}
+name|GimpPaletteFileFormat
+typedef|;
+end_typedef
+
 begin_function_decl
 name|GList
 modifier|*
@@ -92,6 +121,18 @@ name|GError
 modifier|*
 modifier|*
 name|error
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|GimpPaletteFileFormat
+name|gimp_palette_load_detect_format
+parameter_list|(
+specifier|const
+name|gchar
+modifier|*
+name|filename
 parameter_list|)
 function_decl|;
 end_function_decl
