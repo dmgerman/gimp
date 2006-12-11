8dc9e167863af0b64385af67b58562e2e52bea14
Sven Neumann <sven@gimp.org>
2006-12-11 20:33:01 +0000

    app/actions/file-actions.c app/actions/file-commands.[ch] introduced new
    2006-12-11  Sven Neumann  <sven@gimp.org>
    
    	* app/actions/file-actions.c
    	* app/actions/file-commands.[ch]
    	* app/display/gimpdisplayshell-close.c: introduced new action
    	'file-save-and-close' and moved display deletion there.
    
    
    Former-commit-id: 7f6033fe721159cc13e6ef52d8f9d41a3fef8de0

--- app/actions/file-commands.h
+++ app/actions/file-commands.h
@@ -133,6 +133,20 @@ end_function_decl
 
 begin_function_decl
 name|void
+name|file_save_and_close_cmd_callback
+parameter_list|(
+name|GtkAction
+modifier|*
+name|action
+parameter_list|,
+name|gpointer
+name|data
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|void
 name|file_save_template_cmd_callback
 parameter_list|(
 name|GtkAction
