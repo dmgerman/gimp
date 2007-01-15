d75f36296f463831cc5bb3a2fabf50d4f2abc8af
Sven Neumann <sven@gimp.org>
2007-01-15 10:09:42 +0000

    plug-ins/imagemap/imap_csim_lex.c plug-ins/imagemap/imap_ncsa_parse.[ch]
    2007-01-15  Sven Neumann  <sven@gimp.org>
    
    	* plug-ins/imagemap/imap_csim_lex.c
    	* plug-ins/imagemap/imap_ncsa_parse.[ch]
    	* plug-ins/imagemap/imap_ncsa_lex.c
    	* plug-ins/imagemap/imap_cern_lex.c
    	* plug-ins/imagemap/imap_csim_parse.[ch]
    	* plug-ins/imagemap/imap_cern_parse.[ch]: regenerated using GNU
    	Bison 2.3.  Should fix bug #396261.
    
    
    svn path=/trunk/; revision=21710
    
    
    Former-commit-id: ac2ee350d3e5999c2ebb64b4c2b7885d423e5231

--- plug-ins/imagemap/imap_ncsa_parse.h
+++ plug-ins/imagemap/imap_ncsa_parse.h
@@ -1,14 +1,14 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* A Bison parser, made by GNU Bison 1.875d.  */
+comment|/* A Bison parser, made by GNU Bison 2.3.  */
 end_comment
 
 begin_comment
-comment|/* Skeleton parser for Yacc-like parsing with Bison,    Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004 Free Software Foundation, Inc.     This program is free software; you can redistribute it and/or modify    it under the terms of the GNU General Public License as published by    the Free Software Foundation; either version 2, or (at your option)    any later version.     This program is distributed in the hope that it will be useful,    but WITHOUT ANY WARRANTY; without even the implied warranty of    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    GNU General Public License for more details.     You should have received a copy of the GNU General Public License    along with this program; if not, write to the Free Software    Foundation, Inc., 59 Temple Place - Suite 330,    Boston, MA 02111-1307, USA.  */
+comment|/* Skeleton interface for Bison's Yacc-like parsers in C     Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006    Free Software Foundation, Inc.     This program is free software; you can redistribute it and/or modify    it under the terms of the GNU General Public License as published by    the Free Software Foundation; either version 2, or (at your option)    any later version.     This program is distributed in the hope that it will be useful,    but WITHOUT ANY WARRANTY; without even the implied warranty of    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    GNU General Public License for more details.     You should have received a copy of the GNU General Public License    along with this program; if not, write to the Free Software    Foundation, Inc., 51 Franklin Street, Fifth Floor,    Boston, MA 02110-1301, USA.  */
 end_comment
 
 begin_comment
-comment|/* As a special exception, when this file is copied by Bison into a    Bison output file, you may use that output file without restriction.    This special exception was added by the Free Software Foundation    in version 1.24 of Bison.  */
+comment|/* As a special exception, you may create a larger work that contains    part or all of the Bison parser skeleton and distribute that work    under terms of your choice, so long as that work isn't itself a    parser generator using the skeleton or a modified version thereof    as a parser skeleton.  Alternatively, if you modify or redistribute    the parser skeleton itself, you may (at your option) remove this    special exception, which will cause the skeleton and the resulting    Bison output files to be licensed under the GNU General Public    License without this special exception.     This special exception was added by the Free Software Foundation in    version 2.2 of Bison.  */
 end_comment
 
 begin_comment
@@ -99,6 +99,10 @@ begin_endif
 endif|#
 directive|endif
 end_endif
+
+begin_comment
+comment|/* Tokens.  */
+end_comment
 
 begin_define
 DECL|macro|RECTANGLE
@@ -193,29 +197,22 @@ if|#
 directive|if
 operator|!
 name|defined
-argument_list|(
 name|YYSTYPE
-argument_list|)
 operator|&&
 operator|!
 name|defined
-argument_list|(
 name|YYSTYPE_IS_DECLARED
-argument_list|)
 end_if
-
-begin_line
-line|#
-directive|line
-number|46
-file|"imap_ncsa.y"
-end_line
 
 begin_typedef
 DECL|union|YYSTYPE
 typedef|typedef
 union|union
 name|YYSTYPE
+line|#
+directive|line
+number|46
+file|"imap_ncsa.y"
 block|{
 DECL|member|val
 name|int
@@ -232,22 +229,16 @@ index|[
 literal|256
 index|]
 decl_stmt|;
-DECL|typedef|YYSTYPE
 block|}
+comment|/* Line 1489 of yacc.c.  */
+line|#
+directive|line
+number|81
+file|"y.tab.h"
+DECL|typedef|YYSTYPE
 name|YYSTYPE
 typedef|;
 end_typedef
-
-begin_comment
-comment|/* Line 1285 of yacc.c.  */
-end_comment
-
-begin_line
-line|#
-directive|line
-number|69
-file|"y.tab.h"
-end_line
 
 begin_define
 DECL|macro|yystype
