2104ee9c72d62698ef3946537a85807d734ce1ef
Michael Natterer <mitch@gimp.org>
2007-08-11 19:40:22 +0000

    formatting.
    2007-08-11  Michael Natterer  <mitch@gimp.org>
    
    	* app/vectors/gimpstroke-new.[ch]: formatting.
    
    
    svn path=/trunk/; revision=23218
    
    
    Former-commit-id: 66ce241acf57d460ff06bc21ff322e0a065d2901

--- app/vectors/gimpstroke-new.c
+++ app/vectors/gimpstroke-new.c
@@ -24,12 +24,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|"core/gimpcoords.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"gimpstroke-new.h"
 end_include
 
