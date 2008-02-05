004181244952243840280f08ea3134e7a385d646
Michael Natterer <mitch@gimp.org>
2008-02-05 21:26:09 +0000

    don't include <gegl-plugin.h> any longer.
    2008-02-05  Michael Natterer  <mitch@gimp.org>
    
    	* app/tools/gimpgegltool.c: don't include <gegl-plugin.h> any longer.
    
    
    svn path=/trunk/; revision=24816
    
    
    Former-commit-id: 3d079cfaeef17e48ed212e684eb3bdd5fdb937ae

--- app/tools/gimpgegltool.c
+++ app/tools/gimpgegltool.c
@@ -24,16 +24,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<gegl-plugin.h>
-end_include
-
-begin_comment
-comment|/* temp hack for color pspec */
-end_comment
-
-begin_include
-include|#
-directive|include
 file|<gtk/gtk.h>
 end_include
 
