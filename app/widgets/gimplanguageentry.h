d03ec75402d9fa51d7ac2450d3b39a642c333d7d
Sven Neumann <sven@gimp.org>
2008-02-11 20:19:03 +0000

    app/widgets/Makefile.am app/widgets/widgets-types.h
    2008-02-11  Sven Neumann  <sven@gimp.org>
    
    	* app/widgets/Makefile.am
    	* app/widgets/widgets-types.h
    	* app/widgets/gimplanguageentry.[ch]
    	* app/widgets/gimptexteditor.c: turned language entry into a 
    widget.
    
    
    svn path=/trunk/; revision=24866
    
    
    Former-commit-id: 384835d7e26766ed3fbda0872788d2952ca5d451

---
+++ app/widgets/gimplanguageentry.h
@@ -0,0 +1,147 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* GIMP - The GNU Image Manipulation Program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * gimplanguageentry.h  * Copyright (C) 2008  Sven Neumann<sven@gimp.org>  *  * This program is free software; you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 2 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program; if not, write to the Free Software  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+end_comment
+
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|__GIMP_LANGUAGE_ENTRY_H__
+end_ifndef
+
+begin_define
+DECL|macro|__GIMP_LANGUAGE_ENTRY_H__
+define|#
+directive|define
+name|__GIMP_LANGUAGE_ENTRY_H__
+end_define
+
+begin_define
+DECL|macro|GIMP_TYPE_LANGUAGE_ENTRY
+define|#
+directive|define
+name|GIMP_TYPE_LANGUAGE_ENTRY
+value|(gimp_language_entry_get_type ())
+end_define
+
+begin_define
+DECL|macro|GIMP_LANGUAGE_ENTRY (obj)
+define|#
+directive|define
+name|GIMP_LANGUAGE_ENTRY
+parameter_list|(
+name|obj
+parameter_list|)
+value|(G_TYPE_CHECK_INSTANCE_CAST ((obj), GIMP_TYPE_LANGUAGE_ENTRY, GimpLanguageEntry))
+end_define
+
+begin_define
+DECL|macro|GIMP_LANGUAGE_ENTRY_CLASS (klass)
+define|#
+directive|define
+name|GIMP_LANGUAGE_ENTRY_CLASS
+parameter_list|(
+name|klass
+parameter_list|)
+value|(G_TYPE_CHECK_CLASS_CAST ((klass), GIMP_TYPE_LANGUAGE_ENTRY, GimpLanguageEntryClass))
+end_define
+
+begin_define
+DECL|macro|GIMP_IS_LANGUAGE_ENTRY (obj)
+define|#
+directive|define
+name|GIMP_IS_LANGUAGE_ENTRY
+parameter_list|(
+name|obj
+parameter_list|)
+value|(G_TYPE_CHECK_INSTANCE_TYPE ((obj), GIMP_TYPE_LANGUAGE_ENTRY))
+end_define
+
+begin_define
+DECL|macro|GIMP_IS_LANGUAGE_ENTRY_CLASS (klass)
+define|#
+directive|define
+name|GIMP_IS_LANGUAGE_ENTRY_CLASS
+parameter_list|(
+name|klass
+parameter_list|)
+value|(G_TYPE_CHECK_CLASS_TYPE ((klass), GIMP_TYPE_LANGUAGE_ENTRY))
+end_define
+
+begin_define
+DECL|macro|GIMP_LANGUAGE_ENTRY_GET_CLASS (obj)
+define|#
+directive|define
+name|GIMP_LANGUAGE_ENTRY_GET_CLASS
+parameter_list|(
+name|obj
+parameter_list|)
+value|(G_TYPE_INSTANCE_GET_CLASS ((obj), GIMP_TYPE_LANGUAGE_ENTRY, GimpLanguageEntryClass))
+end_define
+
+begin_typedef
+DECL|typedef|GimpLanguageEntryClass
+typedef|typedef
+name|struct
+name|_GimpLanguageEntryClass
+name|GimpLanguageEntryClass
+typedef|;
+end_typedef
+
+begin_struct
+DECL|struct|_GimpLanguageEntryClass
+struct|struct
+name|_GimpLanguageEntryClass
+block|{
+DECL|member|parent_class
+name|GtkEntryClass
+name|parent_class
+decl_stmt|;
+block|}
+struct|;
+end_struct
+
+begin_struct
+DECL|struct|_GimpLanguageEntry
+struct|struct
+name|_GimpLanguageEntry
+block|{
+DECL|member|parent_instance
+name|GtkEntry
+name|parent_instance
+decl_stmt|;
+block|}
+struct|;
+end_struct
+
+begin_decl_stmt
+name|GType
+name|gimp_language_entry_get_type
+argument_list|(
+name|void
+argument_list|)
+name|G_GNUC_CONST
+decl_stmt|;
+end_decl_stmt
+
+begin_function_decl
+name|GtkWidget
+modifier|*
+name|gimp_language_entry_new
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* __GIMP_LANGUAGE_ENTRY_H__ */
+end_comment
+
+end_unit
+
