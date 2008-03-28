fd5e6aa1429faf1afedebeb1ce90669ce7e41f39
Sven Neumann <sven@gimp.org>
2008-03-28 16:56:43 +0000

    deprecate gimp_md5_get_digest() in favor of GCheckSum.
    2008-03-28  Sven Neumann  <sven@gimp.org>
    
    	* libgimpmath/gimpmd5.[ch]: deprecate gimp_md5_get_digest() in
    	favor of GCheckSum.
    
    	* libgimpmath/test-md5.c: undef GIMP_DISABLE_DEPRECATED.
    
    svn path=/trunk/; revision=25288
    
    
    Former-commit-id: b29a050c13747a27591d0aacdfb464ca798c823c

--- libgimpmath/gimpmd5.h
+++ libgimpmath/gimpmd5.h
@@ -23,6 +23,9 @@ end_define
 begin_function_decl
 name|G_BEGIN_DECLS
 comment|/* For information look into the C source or the html documentation */
+ifndef|#
+directive|ifndef
+name|GIMP_DISABLE_DEPRECATED
 name|void
 name|gimp_md5_get_digest
 parameter_list|(
@@ -42,6 +45,15 @@ index|]
 parameter_list|)
 function_decl|;
 end_function_decl
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* GIMP_DISABLE_DEPRECATED */
+end_comment
 
 begin_macro
 name|G_END_DECLS
--- libgimpmath/test-md5.c
+++ libgimpmath/test-md5.c
@@ -27,6 +27,12 @@ directive|include
 file|<glib.h>
 end_include
 
+begin_undef
+undef|#
+directive|undef
+name|GIMP_DISABLE_DEPRECATED
+end_undef
+
 begin_include
 include|#
 directive|include
