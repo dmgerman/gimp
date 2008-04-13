130ce92071c8f32915f006ba00c6fe1e78615db4
Sven Neumann <sven@gimp.org>
2008-04-13 17:01:19 +0000

    added "label-scale" style property.
    2008-04-13  Sven Neumann  <sven@gimp.org>
    
    	* app/widgets/gimpunitcombobox.c: added "label-scale" style 
    property.
    
    	* app/display/gimpscalecombobox.[ch]: ditto. Also removed the
    	support for extra action items.
    
    	* app/display/gimpstatusbar.c: changed accordingly.
    
    	* themes/Default/gtkrc
    	* themes/Small/gtkrc: use a smaller font for the combo-box 
    labels
    	in the statusbar.
    
    
    svn path=/trunk/; revision=25475
    
    
    Former-commit-id: d8ad4cfa7c689fad759c1b00552dae5e6edf32f8

--- app/display/gimpscalecombobox.h
+++ app/display/gimpscalecombobox.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* GIMP - The GNU Image Manipulation Program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * gimpscalecombobox.h  * Copyright (C) 2004  Sven Neumann<sven@gimp.org>  *  * This program is free software; you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 2 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program; if not, write to the Free Software  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+comment|/* GIMP - The GNU Image Manipulation Program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * gimpscalecombobox.h  * Copyright (C) 2004, 2008  Sven Neumann<sven@gimp.org>  *  * This program is free software; you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 2 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program; if not, write to the Free Software  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 end_comment
 
 begin_ifndef
@@ -116,10 +116,6 @@ DECL|member|parent_instance
 name|GtkComboBox
 name|parent_instance
 decl_stmt|;
-DECL|member|actions_added
-name|gboolean
-name|actions_added
-decl_stmt|;
 DECL|member|last_path
 name|GtkTreePath
 modifier|*
@@ -150,26 +146,6 @@ modifier|*
 name|gimp_scale_combo_box_new
 parameter_list|(
 name|void
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-name|void
-name|gimp_scale_combo_box_add_action
-parameter_list|(
-name|GimpScaleComboBox
-modifier|*
-name|combo_box
-parameter_list|,
-name|GtkAction
-modifier|*
-name|action
-parameter_list|,
-specifier|const
-name|gchar
-modifier|*
-name|label
 parameter_list|)
 function_decl|;
 end_function_decl
