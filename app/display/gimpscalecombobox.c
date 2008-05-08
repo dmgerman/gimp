8b1b6e8c302fafa6108308ae78d01949cb28a1cf
Sven Neumann <sven@gimp.org>
2008-05-08 12:47:21 +0000

    removed the entry's inner border to save some precious space in the
    2008-05-08  Sven Neumann  <sven@gimp.org>
    
    	* app/display/gimpscalecombobox.c (gimp_scale_combo_box_init):
    	removed the entry's inner border to save some precious space in
    	the status-bar.
    
    
    svn path=/trunk/; revision=25588
    
    
    Former-commit-id: d4ac3ad00e79c31fe986642496283731516d0bce

--- app/display/gimpscalecombobox.c
+++ app/display/gimpscalecombobox.c
@@ -67,7 +67,7 @@ end_define
 
 begin_enum
 enum|enum
-DECL|enum|__anon2a8b9bef0103
+DECL|enum|__anon275f08270103
 block|{
 DECL|enumerator|SCALE
 name|SCALE
@@ -86,7 +86,7 @@ end_enum
 
 begin_enum
 enum|enum
-DECL|enum|__anon2a8b9bef0203
+DECL|enum|__anon275f08270203
 block|{
 DECL|enumerator|ENTRY_ACTIVATED
 name|ENTRY_ACTIVATED
@@ -364,6 +364,19 @@ decl_stmt|;
 name|GtkTreeIter
 name|iter
 decl_stmt|;
+name|GtkBorder
+name|border
+init|=
+block|{
+literal|0
+block|,
+literal|0
+block|,
+literal|0
+block|,
+literal|0
+block|}
+decl_stmt|;
 name|gint
 name|i
 decl_stmt|;
@@ -448,6 +461,11 @@ argument_list|,
 literal|"truncate-multiline"
 argument_list|,
 name|TRUE
+argument_list|,
+literal|"inner-border"
+argument_list|,
+operator|&
+name|border
 argument_list|,
 name|NULL
 argument_list|)
