012b4a7aa816d142ed03da406e9372ca4b753de8
Simon Budig <simon@gimp.org>
2008-05-13 09:18:32 +0000

    add a comment explaining the hack.
    2008-05-13  Simon Budig  <simon@gimp.org>
    
    	* app/vectors/vectors-types.h: add a comment explaining the hack.
    
    
    svn path=/trunk/; revision=25648
    
    
    Former-commit-id: b449e38485f173100a255b2858419c0a18e9819a

--- app/vectors/vectors-types.h
+++ app/vectors/vectors-types.h
@@ -91,6 +91,10 @@ name|GimpBezierStroke
 typedef|;
 end_typedef
 
+begin_comment
+comment|/*  * The following hack is made so that we can reuse the definition  * the cairo definition of cairo_path_t without having to translate  * between our own version of a bezier description and cairos version.  *  * to avoid having to include<cairo/cairo.h> in each and every file  * including this file we only use the "real" definition when cairo.h  * already has been included and use something else.  *  * Note that if you really want to work with GimpBezierDesc (except just  * passing pointers to it around) you also need to include<cairo/cairo.h>.  */
+end_comment
+
 begin_ifdef
 ifdef|#
 directive|ifdef
