f51e9dbb043a53b97cc96f8bf587ad128542bd4e
Martin Nordholts <martinn@svn.gnome.org>
2008-07-11 19:31:45 +0000

    New function to replace any occurance of
    2008-07-11  Martin Nordholts  <martinn@svn.gnome.org>
    
    	* app/display/gimpdisplayshell-scroll.c
    	(gimp_display_shell_get_scaled_image_viewport_offset): New
    	function to replace any occurance of
    
    	  shell->disp_[xy]offset - shell->offset_[xy]
    
    	that is just an implementation specific way of saying the same
    	thing.
    
    	* app/display/gimpdisplayshell-draw.c
    	* app/display/gimpdisplayshell-transform.c: Make the code less
    	implementation dependant by using the new function.
    
    svn path=/trunk/; revision=26137
    
    
    Former-commit-id: 18c89550fa9b8325a22ad965cedda29b29e0868d

--- app/display/gimpdisplayshell-scroll.h
+++ app/display/gimpdisplayshell-scroll.h
@@ -98,6 +98,25 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|void
+name|gimp_display_shell_get_scaled_image_viewport_offset
+parameter_list|(
+name|GimpDisplayShell
+modifier|*
+name|shell
+parameter_list|,
+name|gint
+modifier|*
+name|x
+parameter_list|,
+name|gint
+modifier|*
+name|y
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
