09e2fc2ff73b7cbbb048f39ecaecb6da2fcdb09a
Martin Nordholts <martinn@src.gnome.org>
2008-11-30 21:25:02 +0000

    New files defining GIMP_WIDGETS_ERROR domain with currently only a
    * libgimpwidgets/gimpwidgets-error.[ch]: New files defining
    GIMP_WIDGETS_ERROR domain with currently only a
    GIMP_WIDGETS_PARSE_ERROR error code.
    
    * libgimpwidgets/Makefile.am: Add the new files here.
    
    svn path=/trunk/; revision=27741
    
    
    Former-commit-id: bb591631fe41ab7f27a8470773c474e52f60ca32

---
+++ libgimpwidgets/gimpwidgets-error.c
@@ -0,0 +1,46 @@
+begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
+begin_comment
+comment|/* LIBGIMP - The GIMP Library  * Copyright (C) 1995-1997 Peter Mattis and Spencer Kimball  *  * gimpwidgets-error.c  * Copyright (C) 2008 Martin Nordholts<martinn@svn.gnome.org>  *  * This library is free software; you can redistribute it and/or  * modify it under the terms of the GNU Lesser General Public  * License as published by the Free Software Foundation; either  * version 2 of the License, or (at your option) any later version.  *  * This library is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU  * Lesser General Public License for more details.  *  * You should have received a copy of the GNU Lesser General Public  * License along with this library; if not, write to the  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,  * Boston, MA 02111-1307, USA.  */
+end_comment
+
+begin_include
+include|#
+directive|include
+file|"config.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|<glib.h>
+end_include
+
+begin_include
+include|#
+directive|include
+file|"gimpwidgets-error.h"
+end_include
+
+begin_comment
+comment|/**  * gimp_widgets_error_quark:  *  * This function is never called directly. Use GIMP_WIDGETS_ERROR() instead.  *  * Return value: the #GQuark that defines the GIMP widgets error domain.  **/
+end_comment
+
+begin_function
+name|GQuark
+DECL|function|gimp_widgets_error_quark (void)
+name|gimp_widgets_error_quark
+parameter_list|(
+name|void
+parameter_list|)
+block|{
+return|return
+name|g_quark_from_static_string
+argument_list|(
+literal|"gimp-widgets-error-quark"
+argument_list|)
+return|;
+block|}
+end_function
+
+end_unit
+
