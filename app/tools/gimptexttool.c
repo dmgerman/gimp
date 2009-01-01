d47472b013cf8c980008c0c22237ab83001fbc52
Sven Neumann <sven@gimp.org>
2009-01-01 17:00:39 +0000

    removed unused include.
    2009-01-01  Sven Neumann  <sven@gimp.org>
    
    	* app/tools/gimptexttool.c: removed unused include.
    
    
    svn path=/trunk/; revision=27873
    
    
    Former-commit-id: 6ee74726a23d595289e180c0927294da4480a763

--- app/tools/gimptexttool.c
+++ app/tools/gimptexttool.c
@@ -168,12 +168,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|"display/gimpcanvas.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"display/gimpdisplay.h"
 end_include
 
