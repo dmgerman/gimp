8d4cc6ea0cf5d21c16fed1739e636073a6f66d94
Michael Natterer <mitch@gimp.org>
2009-06-17 20:55:05 +0200

    Add forgotten #include "libgimpbase/gimpbase.h"
    Former-commit-id: 8423757d3f180a051fa4ac97de5cf431da3b1877

--- app/paint/gimpdodgeburn.c
+++ app/paint/gimpdodgeburn.c
@@ -18,6 +18,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|"libgimpbase/gimpbase.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|"libgimpmath/gimpmath.h"
 end_include
 
