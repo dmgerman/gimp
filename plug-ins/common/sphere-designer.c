4473a067dcee4b1b6f17e210f0ffdaca8631c3bf
Martin Nordholts <martinn@src.gnome.org>
2009-07-21 18:52:12 +0200

    Bug 582821 – 'Sphere Designer' does not reset correctly...
    Make sure to reconstruct the list in the UI when using layers from a
    previous invocation.
    
    
    Former-commit-id: cabcc592f03ff10bcdbc9c969c53f5c03603483a

--- plug-ins/common/sphere-designer.c
+++ plug-ins/common/sphere-designer.c
@@ -214,7 +214,7 @@ end_decl_stmt
 
 begin_enum
 enum|enum
-DECL|enum|__anon2acbef0e0103
+DECL|enum|__anon29f817c80103
 block|{
 DECL|enumerator|TRIANGLE
 name|TRIANGLE
@@ -239,7 +239,7 @@ end_enum
 
 begin_enum
 enum|enum
-DECL|enum|__anon2acbef0e0203
+DECL|enum|__anon29f817c80203
 block|{
 DECL|enumerator|SOLID
 name|SOLID
@@ -288,7 +288,7 @@ end_enum
 
 begin_enum
 enum|enum
-DECL|enum|__anon2acbef0e0303
+DECL|enum|__anon29f817c80303
 block|{
 DECL|enumerator|PERSPECTIVE
 name|PERSPECTIVE
@@ -304,7 +304,7 @@ end_enum
 
 begin_enum
 enum|enum
-DECL|enum|__anon2acbef0e0403
+DECL|enum|__anon29f817c80403
 block|{
 DECL|enumerator|FOG
 name|FOG
@@ -314,7 +314,7 @@ end_enum
 
 begin_enum
 enum|enum
-DECL|enum|__anon2acbef0e0503
+DECL|enum|__anon29f817c80503
 block|{
 DECL|enumerator|TYPE
 name|TYPE
@@ -367,7 +367,7 @@ end_define
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2acbef0e0608
+DECL|struct|__anon29f817c80608
 block|{
 DECL|member|xsize
 DECL|member|ysize
@@ -390,7 +390,7 @@ end_typedef
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2acbef0e0708
+DECL|struct|__anon29f817c80708
 block|{
 DECL|member|numcol
 name|gshort
@@ -419,7 +419,7 @@ end_typedef
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2acbef0e0808
+DECL|struct|__anon29f817c80808
 block|{
 DECL|member|majtype
 name|gint
@@ -514,7 +514,7 @@ end_typedef
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2acbef0e0908
+DECL|struct|__anon29f817c80908
 block|{
 DECL|member|type
 name|gshort
@@ -541,7 +541,7 @@ end_typedef
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2acbef0e0a08
+DECL|struct|__anon29f817c80a08
 block|{
 DECL|member|type
 name|gshort
@@ -582,7 +582,7 @@ end_typedef
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2acbef0e0b08
+DECL|struct|__anon29f817c80b08
 block|{
 DECL|member|com
 name|common
@@ -607,7 +607,7 @@ end_typedef
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2acbef0e0c08
+DECL|struct|__anon29f817c80c08
 block|{
 DECL|member|com
 name|common
@@ -633,7 +633,7 @@ end_typedef
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2acbef0e0d08
+DECL|struct|__anon29f817c80d08
 block|{
 DECL|member|com
 name|common
@@ -656,7 +656,7 @@ end_typedef
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2acbef0e0e08
+DECL|struct|__anon29f817c80e08
 block|{
 DECL|member|com
 name|common
@@ -681,7 +681,7 @@ end_typedef
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2acbef0e0f08
+DECL|struct|__anon29f817c80f08
 block|{
 DECL|member|com
 name|common
@@ -704,7 +704,7 @@ end_typedef
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2acbef0e1008
+DECL|struct|__anon29f817c81008
 block|{
 DECL|member|com
 name|common
@@ -727,7 +727,7 @@ end_typedef
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2acbef0e1108
+DECL|struct|__anon29f817c81108
 block|{
 DECL|member|v1
 DECL|member|v2
@@ -753,7 +753,7 @@ end_typedef
 begin_typedef
 typedef|typedef
 union|union
-DECL|union|__anon2acbef0e120a
+DECL|union|__anon29f817c8120a
 block|{
 DECL|member|com
 name|common
@@ -17873,13 +17873,16 @@ argument_list|()
 expr_stmt|;
 if|if
 condition|(
-operator|!
 name|s
 operator|.
 name|com
 operator|.
 name|numtexture
+operator|==
+literal|0
 condition|)
+block|{
+comment|/* Setup and use default list */
 name|sphere_response
 argument_list|(
 name|NULL
@@ -17889,6 +17892,14 @@ argument_list|,
 name|NULL
 argument_list|)
 expr_stmt|;
+block|}
+else|else
+block|{
+comment|/* Reuse the list from a previous invocation */
+name|rebuildlist
+argument_list|()
+expr_stmt|;
+block|}
 name|gtk_main
 argument_list|()
 expr_stmt|;
