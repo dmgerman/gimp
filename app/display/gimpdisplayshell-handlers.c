458388f883e288fe22c70ac74cfcbf91d5c975b7
Michael Natterer <mitch@gimp.org>
2009-08-04 22:32:24 +0200

    Include "core/gimptreehandler.h"
    Former-commit-id: 6a64de7f48c3bbcc1ea3212f9cac6fbeef7b7a46

--- app/display/gimpdisplayshell-handlers.c
+++ app/display/gimpdisplayshell-handlers.c
@@ -78,6 +78,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|"core/gimptreehandler.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|"widgets/gimpwidgets-utils.h"
 end_include
 
