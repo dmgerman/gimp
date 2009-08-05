d84c7db7339f6b03ddcd6e8e445520e49828a7f4
Michael Natterer <mitch@gimp.org>
2009-08-05 14:17:07 +0200

    Add gimp_viewable_is_ancestor()
    The new functions figures if one viewable is another's parent or
    grandparent or grandgrand... Note that unlike gtk_widget_is_ancestor(),
    this function has its parameters in the right order.
    
    
    Former-commit-id: 8cda7dca56cb647f1fba4fa75b648bbbe4dc03ad

--- app/core/gimpviewable.h
+++ app/core/gimpviewable.h
@@ -815,6 +815,21 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|gboolean
+name|gimp_viewable_is_ancestor
+parameter_list|(
+name|GimpViewable
+modifier|*
+name|ancestor
+parameter_list|,
+name|GimpViewable
+modifier|*
+name|descendant
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
