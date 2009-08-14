065426c61d2330fa90a0f6384102a0c3f5a04f50
Martin Nordholts <martinn@src.gnome.org>
2009-08-14 19:18:56 +0200

    app: Add temp_buf_dump()
    Add temp_buf_dump() which can be used to dump TempBufs to disk for
    easier analysis, for example with GIMP.
    
    
    Former-commit-id: 1dbf732943497b4428862966b32911ef770debad

--- app/base/temp-buf.h
+++ app/base/temp-buf.h
@@ -268,6 +268,22 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|void
+name|temp_buf_dump
+parameter_list|(
+name|TempBuf
+modifier|*
+name|buf
+parameter_list|,
+specifier|const
+name|gchar
+modifier|*
+name|filename
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
