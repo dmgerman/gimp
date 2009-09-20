432e799d9687793f1cc28903176fdc30e7feae63
Martin Nordholts <martinn@src.gnome.org>
2009-09-20 19:24:04 +0200

    app: Add GimpUIManager property to GimpDock
    Add GimpUIManager property to GimpDock. We need it later when the
    GimpDock stops being a GimpDockWindow.
    
    
    Former-commit-id: 3ebad746ee7227f3e18259bb0355fa67cf2d6c5a

--- app/widgets/gimpdock.h
+++ app/widgets/gimpdock.h
@@ -398,6 +398,18 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
+name|GimpUIManager
+modifier|*
+name|gimp_dock_get_ui_manager
+parameter_list|(
+name|GimpDock
+modifier|*
+name|dock
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
 name|GList
 modifier|*
 name|gimp_dock_get_dockbooks
