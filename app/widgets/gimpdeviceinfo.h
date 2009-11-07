d154faf4d0e2d79b81a55a2971700539e3ef027f
Michael Natterer <mitch@gimp.org>
2009-11-07 17:39:02 +0100

    Store the active dynamics with the input device
    Also remove cruft #include <stdio.h>
    
    
    Former-commit-id: e4d8a360803211a3c4b904a0f054ac8d2cbcd5f9

--- app/widgets/gimpdeviceinfo.h
+++ app/widgets/gimpdeviceinfo.h
@@ -19,12 +19,6 @@ end_define
 begin_include
 include|#
 directive|include
-file|<stdio.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|"core/gimpcontext.h"
 end_include
 
@@ -37,7 +31,7 @@ DECL|macro|GIMP_DEVICE_INFO_CONTEXT_MASK
 define|#
 directive|define
 name|GIMP_DEVICE_INFO_CONTEXT_MASK
-value|(GIMP_CONTEXT_TOOL_MASK       | \                                        GIMP_CONTEXT_PAINT_INFO_MASK | \                                        GIMP_CONTEXT_FOREGROUND_MASK | \                                        GIMP_CONTEXT_BACKGROUND_MASK | \                                        GIMP_CONTEXT_BRUSH_MASK      | \                                        GIMP_CONTEXT_PATTERN_MASK    | \                                        GIMP_CONTEXT_GRADIENT_MASK)
+value|(GIMP_CONTEXT_TOOL_MASK       | \                                        GIMP_CONTEXT_PAINT_INFO_MASK | \                                        GIMP_CONTEXT_FOREGROUND_MASK | \                                        GIMP_CONTEXT_BACKGROUND_MASK | \                                        GIMP_CONTEXT_BRUSH_MASK      | \                                        GIMP_CONTEXT_DYNAMICS_MASK   | \                                        GIMP_CONTEXT_PATTERN_MASK    | \                                        GIMP_CONTEXT_GRADIENT_MASK)
 end_define
 
 begin_define
