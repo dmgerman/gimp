04a563f0db567ebd58c4a641c8478f8f1c3024fc
Michael Natterer <mitch@gimp.org>
2010-02-15 00:47:15 +0100

    app: grey out the names of unplugged devices
    Former-commit-id: f0d4cf68c0e82941ab840b70e2a969cd4fbc3563

--- app/widgets/gimpdeviceeditor.c
+++ app/widgets/gimpdeviceeditor.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* GIMP - The GNU Image Manipulation Program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * This program is free software: you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program.  If not, see<http://www.gnu.org/licenses/>.  */
+comment|/* GIMP - The GNU Image Manipulation Program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * gimpdeviceeditor.c  * Copyright (C) 2010 Michael Natterer<mitch@gimp.org>  *  * This program is free software: you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program.  If not, see<http://www.gnu.org/licenses/>.  */
 end_comment
 
 begin_include
@@ -89,7 +89,7 @@ end_include
 
 begin_enum
 enum|enum
-DECL|enum|__anon2a0d95800103
+DECL|enum|__anon27b569e80103
 block|{
 DECL|enumerator|PROP_0
 name|PROP_0
@@ -1223,6 +1223,17 @@ argument_list|,
 name|GIMP_CONTAINER_TREE_VIEW_COLUMN_USER_DATA
 argument_list|,
 name|widget
+argument_list|,
+name|GIMP_CONTAINER_TREE_VIEW_COLUMN_NAME_SENSITIVE
+argument_list|,
+name|gimp_device_info_get_device
+argument_list|(
+name|info
+argument_list|,
+name|NULL
+argument_list|)
+operator|!=
+name|NULL
 argument_list|,
 operator|-
 literal|1
