feaea3c5e3fee6ea8dc442db6dc7b22f3f1cfea3
Michael Natterer <mitch@gimp.org>
2010-02-14 22:08:32 +0100

    app: sort plugged devices to the start of the list
    and sort plugged/unplugged devices alphabetically
    
    
    Former-commit-id: 3facba67d4b995aefe4241b242f39f35ec0085d0

--- app/widgets/gimpdeviceinfo.h
+++ app/widgets/gimpdeviceinfo.h
@@ -386,6 +386,21 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|gint
+name|gimp_device_info_compare
+parameter_list|(
+name|GimpDeviceInfo
+modifier|*
+name|a
+parameter_list|,
+name|GimpDeviceInfo
+modifier|*
+name|b
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_macro
 name|G_END_DECLS
 end_macro
