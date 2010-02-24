e19548e1f0141a0efc292be60a611a933ab59598
Alexia Death <alexiadeath@gmail.com>
2010-02-24 02:09:43 +0200

    app: Make pressure at stroke ends nicer
    Former-commit-id: ede02a68b992483c11e59b28659f37bfe83c7827

--- app/display/gimpdisplayshell-callbacks.c
+++ app/display/gimpdisplayshell-callbacks.c
@@ -3859,15 +3859,6 @@ name|control
 argument_list|)
 condition|)
 block|{
-name|g_array_append_val
-argument_list|(
-name|shell
-operator|->
-name|event_queue
-argument_list|,
-name|image_coords
-argument_list|)
-expr_stmt|;
 if|if
 condition|(
 name|shell
