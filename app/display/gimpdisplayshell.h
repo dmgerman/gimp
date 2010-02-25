6e5b67c03cdefd5e80bc2013b8d9329e344d02e7
Michael Natterer <mitch@gimp.org>
2010-02-26 00:09:54 +0100

    app: add API to move an existing image-coords overlay around
    Former-commit-id: ff01c2d77ecc73e0c75d30a0d22784663333d256

--- app/display/gimpdisplayshell.h
+++ app/display/gimpdisplayshell.h
@@ -826,6 +826,27 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
+name|void
+name|gimp_display_shell_move_overlay
+parameter_list|(
+name|GimpDisplayShell
+modifier|*
+name|shell
+parameter_list|,
+name|GtkWidget
+modifier|*
+name|child
+parameter_list|,
+name|gdouble
+name|image_x
+parameter_list|,
+name|gdouble
+name|image_y
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
 name|GimpImageWindow
 modifier|*
 name|gimp_display_shell_get_window
