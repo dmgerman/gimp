7fa92ac082d6285f0a66b6f8d96ec318b006f7c2
Martin Nordholts <martinn@src.gnome.org>
2010-03-07 11:15:16 +0100

    app: Add gimp_action_group_set_action_tooltip()
    Former-commit-id: 15344ffd6dd41febd6e542f1e5e74186c149bec4

--- app/widgets/gimpactiongroup.h
+++ app/widgets/gimpactiongroup.h
@@ -772,6 +772,27 @@ end_function_decl
 
 begin_function_decl
 name|void
+name|gimp_action_group_set_action_tooltip
+parameter_list|(
+name|GimpActionGroup
+modifier|*
+name|group
+parameter_list|,
+specifier|const
+name|gchar
+modifier|*
+name|action_name
+parameter_list|,
+specifier|const
+name|gchar
+modifier|*
+name|tooltip
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|void
 name|gimp_action_group_set_action_color
 parameter_list|(
 name|GimpActionGroup
