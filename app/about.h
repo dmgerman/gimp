7f16cc721df787abd3bd6aca4d64613a10d31834
Sven Neumann <sven@gimp.org>
2010-07-09 00:16:39 +0200

    app: update copyright, it's 2010 for a while now ...
    Former-commit-id: fb6e561f3f4dffe3cc696692b1b91484f70eaabd

--- app/about.h
+++ app/about.h
@@ -40,7 +40,7 @@ define|#
 directive|define
 name|GIMP_COPYRIGHT
 define|\
-value|_("Copyright Â© 1995-2009\n" \     "Spencer Kimball, Peter Mattis and the GIMP Development Team")
+value|_("Copyright Â© 1995-2010\n" \     "Spencer Kimball, Peter Mattis and the GIMP Development Team")
 end_define
 
 begin_define
