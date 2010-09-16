207908c1545985ef7402f19ef7614029f9e9fa3b
Michael Natterer <mitch@gimp.org>
2010-09-16 20:36:14 +0200

    pdb: #include "gimpgrouplayer.h" in layers-cmds.c
    Former-commit-id: aa55a982497e910862d4feef9818f16ba5f37c27

--- app/pdb/layer-cmds.c
+++ app/pdb/layer-cmds.c
@@ -46,6 +46,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|"core/gimpgrouplayer.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|"core/gimpimage-undo.h"
 end_include
 
