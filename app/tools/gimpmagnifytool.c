3a55e7919bebf3b8bde85dfc83c1a7c1c34361a5
Michael Natterer <mitch@gimp.org>
2010-09-22 22:25:09 +0200

    app: enable cairo drawing in the zoom tool
    Former-commit-id: 55369b5f22c454f457938076d9567bbbff38875f

--- app/tools/gimpmagnifytool.c
+++ app/tools/gimpmagnifytool.c
@@ -502,6 +502,15 @@ argument_list|,
 name|GIMP_CURSOR_MODIFIER_MINUS
 argument_list|)
 expr_stmt|;
+name|GIMP_DRAW_TOOL
+argument_list|(
+name|tool
+argument_list|)
+operator|->
+name|use_cairo
+operator|=
+name|TRUE
+expr_stmt|;
 block|}
 end_function
 
