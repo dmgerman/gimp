1000efdf8891aa290beec457caf906568ee96018
Michael Natterer <mitch@gimp.org>
2010-09-23 14:52:23 +0200

    app: enable cairo drawing for the move tool
    Former-commit-id: 76be01d02648e51003546416352a24112349967c

--- app/tools/gimpmovetool.c
+++ app/tools/gimpmovetool.c
@@ -708,6 +708,15 @@ argument_list|,
 name|GIMP_TOOL_CURSOR_MOVE
 argument_list|)
 expr_stmt|;
+name|GIMP_DRAW_TOOL
+argument_list|(
+name|tool
+argument_list|)
+operator|->
+name|use_cairo
+operator|=
+name|TRUE
+expr_stmt|;
 block|}
 end_function
 
