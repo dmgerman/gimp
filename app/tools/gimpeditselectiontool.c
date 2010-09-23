9c202d6c410c9e83be3c867ad776f970194626f6
Michael Natterer <mitch@gimp.org>
2010-09-23 23:47:20 +0200

    app: enable cairo drawing for the edit selection tool
    Former-commit-id: a75b298946af86554403337034a17792ab3725f0

--- app/tools/gimpeditselectiontool.c
+++ app/tools/gimpeditselectiontool.c
@@ -624,6 +624,15 @@ name|constrain
 operator|=
 name|FALSE
 expr_stmt|;
+name|GIMP_DRAW_TOOL
+argument_list|(
+name|tool
+argument_list|)
+operator|->
+name|use_cairo
+operator|=
+name|TRUE
+expr_stmt|;
 block|}
 end_function
 
