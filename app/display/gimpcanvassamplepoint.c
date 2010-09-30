eab6d01b795565f9853c9279ed84e63e131cf1f6
Michael Natterer <mitch@gimp.org>
2010-09-30 13:28:04 +0200

    app: don't include "core/gimpsamplepoint.h"
    Former-commit-id: bab2c37f489bfcf6a5ed664390e6e7badc9ef461

--- app/display/gimpcanvassamplepoint.c
+++ app/display/gimpcanvassamplepoint.c
@@ -42,12 +42,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|"core/gimpsamplepoint.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"gimpcanvas.h"
 end_include
 
@@ -85,7 +79,7 @@ end_define
 
 begin_enum
 enum|enum
-DECL|enum|__anon2c842e8e0103
+DECL|enum|__anon2c6c59820103
 block|{
 DECL|enumerator|PROP_0
 name|PROP_0
