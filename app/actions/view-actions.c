809df69addd4d524d74921806431f531aa724f43
Michael Natterer <mitch@gimp.org>
2010-10-03 02:25:53 +0200

    app: remove obsolete include
    Former-commit-id: ff6d0f36b33f92dce0f08a9a98ad2693bbe75c91

--- app/actions/view-actions.c
+++ app/actions/view-actions.c
@@ -132,12 +132,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|"display/gimpdisplayshell-selection.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"display/gimpimagewindow.h"
 end_include
 
