e72b976e7a4d7f8062b09dccb86640280fb256d0
Michael Natterer <mitch@gimp.org>
2010-10-15 00:15:48 +0200

    libgimpwidgets: add gimp_unit_store_set,get_has_pixels()
    to allow having unit combos without "pixels". Adapt GimpUnitComboBox
    to not assume that the unit is equal to the index in the store and
    enable "menu_has_pixels" in GimpSizeEntry again.
    
    
    Former-commit-id: e5faf090ca5f1420502efd3c40321d1d91a74548

--- libgimpwidgets/gimpsizeentry.c
+++ libgimpwidgets/gimpsizeentry.c
@@ -76,7 +76,7 @@ end_define
 
 begin_enum
 enum|enum
-DECL|enum|__anon2b0dafb20103
+DECL|enum|__anon2bf898f10103
 block|{
 DECL|enumerator|VALUE_CHANGED
 name|VALUE_CHANGED
@@ -1270,6 +1270,16 @@ operator|->
 name|number_of_fields
 argument_list|)
 expr_stmt|;
+name|gimp_unit_store_set_has_pixels
+argument_list|(
+name|store
+argument_list|,
+name|gse
+operator|->
+name|menu_show_pixels
+argument_list|)
+expr_stmt|;
+comment|/* FIXME menu_show_percent */
 name|gse
 operator|->
 name|unitmenu
