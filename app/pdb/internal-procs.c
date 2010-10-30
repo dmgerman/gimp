6358d1fccc28c1a3fce918a375d713fac90e7ddb
Michael Natterer <mitch@gimp.org>
2010-10-30 22:06:20 +0200

    pdb: add new selection API which has less parameters than the old one
    and uses the new context properties instead. This is work in progress
    and subject to change. Don't use it in plug-ins/scripts yet!
    
    
    Former-commit-id: 457a46a04c338f71db6dcc0f43bc0afdcde30353

--- app/pdb/internal-procs.c
+++ app/pdb/internal-procs.c
@@ -38,7 +38,7 @@ file|"internal-procs.h"
 end_include
 
 begin_comment
-comment|/* 634 procedures registered total */
+comment|/* 641 procedures registered total */
 end_comment
 
 begin_function
@@ -175,6 +175,11 @@ name|pdb
 argument_list|)
 expr_stmt|;
 name|register_image_procs
+argument_list|(
+name|pdb
+argument_list|)
+expr_stmt|;
+name|register_image_select_procs
 argument_list|(
 name|pdb
 argument_list|)
