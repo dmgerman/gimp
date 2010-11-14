44bde6d9c4832aae885c25f791e94e7dce22cf1d
Michael Natterer <mitch@gimp.org>
2010-11-15 00:23:18 +0100

    app: remove unused #includes
    Former-commit-id: 7a3dc6ee38e9f8de0e6d5ba59df30bf560234c56

--- app/display/gimpdisplayshell-expose.c
+++ app/display/gimpdisplayshell-expose.c
@@ -24,12 +24,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|"gimpcanvasitem.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"gimpdisplayshell.h"
 end_include
 
--- app/tools/gimpdrawtool.c
+++ app/tools/gimpdrawtool.c
@@ -168,12 +168,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|"display/gimpdisplayshell-expose.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"display/gimpdisplayshell-items.h"
 end_include
 
