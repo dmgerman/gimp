e2bad048dcab53700bb5a7a461771ebc202fbca2
Omari Stephens <xsdg@xsdg.org>
2011-01-24 18:49:08 +0000

    Add missing parens around macro arguments
    Former-commit-id: 14276add926540712d450033c1720e9588e58726

--- libgimpwidgets/gimpcairo-utils.h
+++ libgimpwidgets/gimpcairo-utils.h
@@ -264,7 +264,7 @@ parameter_list|,
 name|a
 parameter_list|)
 define|\
-value|G_STMT_START {                                   \     const guint tr = (a) * (r) + 0x80;             \     const guint tg = (a) * (g) + 0x80;             \     const guint tb = (a) * (b) + 0x80;             \     d[0] = (((tb)>> 8) + (tb))>> 8;              \     d[1] = (((tg)>> 8) + (tg))>> 8;              \     d[2] = (((tr)>> 8) + (tr))>> 8;              \     d[3] = (a);                                    \   } G_STMT_END
+value|G_STMT_START {                                   \     const guint tr = (a) * (r) + 0x80;             \     const guint tg = (a) * (g) + 0x80;             \     const guint tb = (a) * (b) + 0x80;             \     (d)[0] = (((tb)>> 8) + (tb))>> 8;            \     (d)[1] = (((tg)>> 8) + (tg))>> 8;            \     (d)[2] = (((tr)>> 8) + (tr))>> 8;            \     (d)[3] = (a);                                  \   } G_STMT_END
 end_define
 
 begin_else
@@ -289,7 +289,7 @@ parameter_list|,
 name|a
 parameter_list|)
 define|\
-value|G_STMT_START {                                   \     const guint tr = (a) * (r) + 0x80;             \     const guint tg = (a) * (g) + 0x80;             \     const guint tb = (a) * (b) + 0x80;             \     d[0] = (a);                                    \     d[1] = (((tr)>> 8) + (tr))>> 8;              \     d[2] = (((tg)>> 8) + (tg))>> 8;              \     d[3] = (((tb)>> 8) + (tb))>> 8;              \   } G_STMT_END
+value|G_STMT_START {                                   \     const guint tr = (a) * (r) + 0x80;             \     const guint tg = (a) * (g) + 0x80;             \     const guint tb = (a) * (b) + 0x80;             \     (d)[0] = (a);                                  \     (d)[1] = (((tr)>> 8) + (tr))>> 8;            \     (d)[2] = (((tg)>> 8) + (tg))>> 8;            \     (d)[3] = (((tb)>> 8) + (tb))>> 8;            \   } G_STMT_END
 end_define
 
 begin_endif
@@ -326,7 +326,7 @@ parameter_list|,
 name|a
 parameter_list|)
 define|\
-value|G_STMT_START {                                   \     const guint tb = s[0];                         \     const guint tg = s[1];                         \     const guint tr = s[2];                         \     const guint ta = s[3];                         \     (r) = (tr<< 8) / (ta + 1);                    \     (g) = (tg<< 8) / (ta + 1);                    \     (b) = (tb<< 8) / (ta + 1);                    \     (a) = ta;                                      \   } G_STMT_END
+value|G_STMT_START {                                   \     const guint tb = (s)[0];                       \     const guint tg = (s)[1];                       \     const guint tr = (s)[2];                       \     const guint ta = (s)[3];                       \     (r) = (tr<< 8) / (ta + 1);                    \     (g) = (tg<< 8) / (ta + 1);                    \     (b) = (tb<< 8) / (ta + 1);                    \     (a) = ta;                                      \   } G_STMT_END
 end_define
 
 begin_else
@@ -351,7 +351,7 @@ parameter_list|,
 name|a
 parameter_list|)
 define|\
-value|G_STMT_START {                                   \     const guint ta = s[0];                         \     const guint tr = s[1];                         \     const guint tg = s[2];                         \     const guint tb = s[3];                         \     (r) = (tr<< 8) / (ta + 1);                    \     (g) = (tg<< 8) / (ta + 1);                    \     (b) = (tb<< 8) / (ta + 1);                    \     (a) = ta;                                      \   } G_STMT_END
+value|G_STMT_START {                                   \     const guint ta = (s)[0];                       \     const guint tr = (s)[1];                       \     const guint tg = (s)[2];                       \     const guint tb = (s)[3];                       \     (r) = (tr<< 8) / (ta + 1);                    \     (g) = (tg<< 8) / (ta + 1);                    \     (b) = (tb<< 8) / (ta + 1);                    \     (a) = ta;                                      \   } G_STMT_END
 end_define
 
 begin_endif
