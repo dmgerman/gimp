96b9fbcc6ab75fdbaeb8b9c2a173e692ab5905fc
Michael Natterer <mitch@gimp.org>
2011-03-22 23:41:42 +0100

    app: turn GimpFilteredContainer into a general purpose filter
    Former-commit-id: f3a99ed8f7311818f0ad46832e9ff1a7172c2362

--- app/core/gimpfilteredcontainer.h
+++ app/core/gimpfilteredcontainer.h
@@ -1,12 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
-begin_if
-if|#
-directive|if
-literal|0
-end_if
-
 begin_comment
-comment|/* GIMP - The GNU Image Manipulation Program  * Copyright (C) 1995-1997 Spencer Kimball and Peter Mattis  *  * gimpfilteredcontainer.h  * Copyright (C) 2008 Aurimas JuÅ¡ka<aurisj@svn.gnome.org>  *  * This program is free software: you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program.  If not, see<http://www.gnu.org/licenses/>.  */
+comment|/* GIMP - The GNU Image Manipulation Program  * Copyright (C) 1995-1997 Spencer Kimball and Peter Mattis  *  * gimpfilteredcontainer.h  * Copyright (C) 2008 Aurimas JuÅ¡ka<aurisj@svn.gnome.org>  *               2011 Michael Natterer<mitch@gimp.org>  *  * This program is free software: you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program.  If not, see<http://www.gnu.org/licenses/>.  */
 end_comment
 
 begin_ifndef
@@ -16,6 +10,7 @@ name|__GIMP_FILTERED_CONTAINER_H__
 end_ifndef
 
 begin_define
+DECL|macro|__GIMP_FILTERED_CONTAINER_H__
 define|#
 directive|define
 name|__GIMP_FILTERED_CONTAINER_H__
@@ -28,6 +23,7 @@ file|"gimplist.h"
 end_include
 
 begin_define
+DECL|macro|GIMP_TYPE_FILTERED_CONTAINER
 define|#
 directive|define
 name|GIMP_TYPE_FILTERED_CONTAINER
@@ -35,6 +31,7 @@ value|(gimp_filtered_container_get_type ())
 end_define
 
 begin_define
+DECL|macro|GIMP_FILTERED_CONTAINER (obj)
 define|#
 directive|define
 name|GIMP_FILTERED_CONTAINER
@@ -45,6 +42,7 @@ value|(G_TYPE_CHECK_INSTANCE_CAST ((obj), GIMP_TYPE_FILTERED_CONTAINER, GimpFilt
 end_define
 
 begin_define
+DECL|macro|GIMP_FILTERED_CONTAINER_CLASS (klass)
 define|#
 directive|define
 name|GIMP_FILTERED_CONTAINER_CLASS
@@ -55,6 +53,7 @@ value|(G_TYPE_CHECK_CLASS_CAST ((klass), GIMP_TYPE_FILTERED_CONTAINER, GimpFilte
 end_define
 
 begin_define
+DECL|macro|GIMP_IS_FILTERED_CONTAINER (obj)
 define|#
 directive|define
 name|GIMP_IS_FILTERED_CONTAINER
@@ -65,6 +64,7 @@ value|(G_TYPE_CHECK_INSTANCE_TYPE ((obj), GIMP_TYPE_FILTERED_CONTAINER))
 end_define
 
 begin_define
+DECL|macro|GIMP_IS_FILTERED_CONTAINER_CLASS (class)
 define|#
 directive|define
 name|GIMP_IS_FILTERED_CONTAINER_CLASS
@@ -75,6 +75,7 @@ value|(G_TYPE_CHECK_CLASS_TYPE ((class), GIMP_TYPE_FILTERED_CONTAINER))
 end_define
 
 begin_define
+DECL|macro|GIMP_FILTERED_CONTAINER_GET_CLASS (obj)
 define|#
 directive|define
 name|GIMP_FILTERED_CONTAINER_GET_CLASS
@@ -84,8 +85,83 @@ parameter_list|)
 value|(G_TYPE_INSTANCE_GET_CLASS ((obj), GIMP_TYPE_FILTERED_CONTAINER, GimpFilteredContainerClass))
 end_define
 
+begin_typedef
+DECL|typedef|GimpFilteredContainerClass
+typedef|typedef
+name|struct
+name|_GimpFilteredContainerClass
+name|GimpFilteredContainerClass
+typedef|;
+end_typedef
+
+begin_struct
+DECL|struct|_GimpFilteredContainer
+struct|struct
+name|_GimpFilteredContainer
+block|{
+DECL|member|parent_instance
+name|GimpList
+name|parent_instance
+decl_stmt|;
+DECL|member|src_container
+name|GimpContainer
+modifier|*
+name|src_container
+decl_stmt|;
+DECL|member|filter_func
+name|GimpObjectFilterFunc
+name|filter_func
+decl_stmt|;
+DECL|member|filter_data
+name|gpointer
+name|filter_data
+decl_stmt|;
+block|}
+struct|;
+end_struct
+
+begin_struct
+DECL|struct|_GimpFilteredContainerClass
+struct|struct
+name|_GimpFilteredContainerClass
+block|{
+DECL|member|parent_class
+name|GimpContainerClass
+name|parent_class
+decl_stmt|;
+block|}
+struct|;
+end_struct
+
+begin_decl_stmt
+name|GType
+name|gimp_filtered_container_get_type
+argument_list|(
+name|void
+argument_list|)
+name|G_GNUC_CONST
+decl_stmt|;
+end_decl_stmt
+
+begin_function_decl
+name|GimpContainer
+modifier|*
+name|gimp_filtered_container_new
+parameter_list|(
+name|GimpContainer
+modifier|*
+name|src_container
+parameter_list|,
+name|GimpObjectFilterFunc
+name|filter_func
+parameter_list|,
+name|gpointer
+name|filter_data
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
-unit|typedef struct _GimpFilteredContainerClass GimpFilteredContainerClass;  struct _GimpFilteredContainer {   GimpList       parent_instance;    GimpContainer *src_container;   GList         *filter;   GHashTable    *tag_ref_counts;   gint           tag_count; };  struct _GimpFilteredContainerClass {   GimpContainerClass  parent_class;    void (* tag_count_changed) (GimpFilteredContainer *container,                               gint                   count); };   GType           gimp_filtered_container_get_type      (void) G_GNUC_CONST;  GimpContainer * gimp_filtered_container_new           (GimpContainer         *src_container,                                                        GCompareFunc           sort_func);  void            gimp_filtered_container_set_filter    (GimpFilteredContainer *filtered_container,                                                        GList                 *tags); const GList   * gimp_filtered_container_get_filter    (GimpFilteredContainer *filtered_container);  gint            gimp_filtered_container_get_tag_count (GimpFilteredContainer *container);
 endif|#
 directive|endif
 end_endif
@@ -93,11 +169,6 @@ end_endif
 begin_comment
 comment|/* __GIMP_FILTERED_CONTAINER_H__ */
 end_comment
-
-begin_endif
-endif|#
-directive|endif
-end_endif
 
 end_unit
 
