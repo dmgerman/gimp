fba416e10276fb92aee3190c75949adf87b5b4d0
Mukund Sivaraman <muks@banu.com>
2011-03-26 10:55:21 +0530

    Remove broken include
    Former-commit-id: 41ab7d8e10e54cbb5d10acbfd18e93a272f07ef5

--- app/core/gimp-edit.c
+++ app/core/gimp-edit.c
@@ -54,12 +54,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|"base/tile-manager-crop.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"paint-funcs/paint-funcs.h"
 end_include
 
