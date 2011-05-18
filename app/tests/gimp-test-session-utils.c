a5f8b9deeb3a27e80367ccb3ed6e27be757b3f2e
Martin Nordholts <martinn@src.gnome.org>
2011-05-18 21:06:20 +0200

    app/tests: Add test for GIMP 2.8 multi window session files
    Move code in test-session-management.c to a common place so we can
    have several variants of session management tests. We can't easily run
    all session management tests in a single process, because some parts
    of Gimp (the GObject) initialization eeks if done twice in the same
    process.
    
    Rename test-session-management.c to test-session-2-6-compatibility.c
    and create a new test for GIMP 2.8 multi-window mode session files,
    namely test-session-2-8-compatibility-multi-window.c. Copy and rename
    sessionrc files accordingly.
    
    
    Former-commit-id: 87e69d23122cef7c145a4d2e0bc619034183ca46

--- app/tests/gimp-test-session-utils.c
+++ app/tests/gimp-test-session-utils.c
@@ -1,7 +1,13 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* GIMP - The GNU Image Manipulation Program  * Copyright (C) 2009 Martin Nordholts  *  * This program is free software: you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program.  If not, see<http://www.gnu.org/licenses/>.  */
+comment|/* GIMP - The GNU Image Manipulation Program  * Copyright (C) 2011 Martin Nordholts<martinn@src.gnome.org>  *  * This program is free software: you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program.  If not, see<http://www.gnu.org/licenses/>.  */
 end_comment
+
+begin_include
+include|#
+directive|include
+file|"config.h"
+end_include
 
 begin_include
 include|#
@@ -87,37 +93,16 @@ directive|include
 file|"gimp-app-test-utils.h"
 end_include
 
-begin_define
-DECL|macro|ADD_TEST (function)
-define|#
-directive|define
-name|ADD_TEST
-parameter_list|(
-name|function
-parameter_list|)
-define|\
-value|g_test_add ("/gimp-session-management/" #function, \               GimpTestFixture, \               NULL, \               NULL, \               function, \               NULL);
-end_define
+begin_include
+include|#
+directive|include
+file|"gimp-test-session-utils.h"
+end_include
 
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2b8bad180108
-block|{
-DECL|member|dummy
-name|int
-name|dummy
-decl_stmt|;
-DECL|typedef|GimpTestFixture
-block|}
-name|GimpTestFixture
-typedef|;
-end_typedef
-
-begin_typedef
-typedef|typedef
-struct|struct
-DECL|struct|__anon2b8bad180208
+DECL|struct|__anon2c1a83980108
 block|{
 DECL|member|filename
 name|gchar
@@ -461,21 +446,33 @@ block|}
 end_function
 
 begin_comment
-comment|/**  * Tests that a sessionrc from GIMP 2.6 is loaded and written (thus  * also interpreted) like we expect.  **/
+comment|/**  * gimp_test_session_load_and_write_session_files:  * @loaded_sessionrc:   The name of the file of the sessionrc file to  *                      load  * @loaded_dockrc:      The name of the file of the dockrc file to load  * @expected_sessionrc: The name of the file with the expected  *                      sessionrc file content  * @expected_dockrc:    The name of the file with the expected dockrc  *                      file content  *  * Utility function for the various session management tests. We can't  * easily have all those tests in a single program several Gimp  * instance can't easily be initialized in the same process.  **/
 end_comment
 
 begin_function
-specifier|static
 name|void
-DECL|function|session_2_6_compatibility (GimpTestFixture * fixture,gconstpointer data)
-name|session_2_6_compatibility
+DECL|function|gimp_test_session_load_and_write_session_files (const gchar * loaded_sessionrc,const gchar * loaded_dockrc,const gchar * expected_sessionrc,const gchar * expected_dockrc)
+name|gimp_test_session_load_and_write_session_files
 parameter_list|(
-name|GimpTestFixture
+specifier|const
+name|gchar
 modifier|*
-name|fixture
+name|loaded_sessionrc
 parameter_list|,
-name|gconstpointer
-name|data
+specifier|const
+name|gchar
+modifier|*
+name|loaded_dockrc
+parameter_list|,
+specifier|const
+name|gchar
+modifier|*
+name|expected_sessionrc
+parameter_list|,
+specifier|const
+name|gchar
+modifier|*
+name|expected_dockrc
 parameter_list|)
 block|{
 name|Gimp
@@ -570,14 +567,14 @@ name|sessionrc_filename
 operator|=
 name|gimp_personal_rc_file
 argument_list|(
-literal|"sessionrc-expected"
+name|expected_sessionrc
 argument_list|)
 expr_stmt|;
 name|dockrc_filename
 operator|=
 name|gimp_personal_rc_file
 argument_list|(
-literal|"dockrc-expected"
+name|expected_dockrc
 argument_list|)
 expr_stmt|;
 comment|/* Remeber the modtimes and MD5s */
@@ -603,6 +600,27 @@ name|initial_dockrc_state
 argument_list|)
 argument_list|)
 expr_stmt|;
+comment|/* Use specific input files when restoring the session */
+name|g_setenv
+argument_list|(
+literal|"GIMP_TESTING_SESSIONRC_NAME"
+argument_list|,
+name|loaded_sessionrc
+argument_list|,
+name|TRUE
+comment|/*overwrite*/
+argument_list|)
+expr_stmt|;
+name|g_setenv
+argument_list|(
+literal|"GIMP_TESTING_DOCKRC_NAME"
+argument_list|,
+name|loaded_dockrc
+argument_list|,
+name|TRUE
+comment|/*overwrite*/
+argument_list|)
+expr_stmt|;
 comment|/* Start up GIMP */
 name|gimp
 operator|=
@@ -622,6 +640,17 @@ argument_list|(
 literal|"GIMP_TESTING_ABS_TOP_BUILDDIR"
 argument_list|,
 literal|"app/tests/gimpdir-output"
+argument_list|)
+expr_stmt|;
+comment|/* Use normal output names */
+name|g_unsetenv
+argument_list|(
+literal|"GIMP_TESTING_SESSIONRC_NAME"
+argument_list|)
+expr_stmt|;
+name|g_unsetenv
+argument_list|(
+literal|"GIMP_TESTING_DOCKRC_NAME"
 argument_list|)
 expr_stmt|;
 name|g_free
@@ -708,47 +737,6 @@ name|final_dockrc_state
 argument_list|)
 argument_list|)
 expr_stmt|;
-block|}
-end_function
-
-begin_function
-DECL|function|main (int argc,char ** argv)
-name|int
-name|main
-parameter_list|(
-name|int
-name|argc
-parameter_list|,
-name|char
-modifier|*
-modifier|*
-name|argv
-parameter_list|)
-block|{
-name|gimp_test_bail_if_no_display
-argument_list|()
-expr_stmt|;
-name|gtk_test_init
-argument_list|(
-operator|&
-name|argc
-argument_list|,
-operator|&
-name|argv
-argument_list|,
-name|NULL
-argument_list|)
-expr_stmt|;
-name|ADD_TEST
-argument_list|(
-name|session_2_6_compatibility
-argument_list|)
-expr_stmt|;
-comment|/* Don't bother freeing stuff, the process is short-lived */
-return|return
-name|g_test_run
-argument_list|()
-return|;
 block|}
 end_function
 
