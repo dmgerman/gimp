d549550d7701a539e93049ab3b99bebeeca373b8
Martin Nordholts <martinn@src.gnome.org>
2011-08-09 19:51:36 +0200

    Revert "app: #include "gimptoolpalette.h" in gimptoolpalette.c"
    This reverts commit 02cfc44ebbbf4303514f02e8820a31efd504d956 [formerly c19560b08c4c8d63658f7807a75588c57ab083f1].
    It was already included, don't know how I could miss that.
    
    
    Former-commit-id: a5ef51137d0b06a697efa36b23b2beaf9e5a99be

--- app/widgets/gimptoolpalette.c
+++ app/widgets/gimptoolpalette.c
@@ -66,12 +66,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|"gimptoolpalette.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"gimpuimanager.h"
 end_include
 
