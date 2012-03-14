115aeacd2bb5e42af58dcdd70644668f45e51bbd
Michael Natterer <mitch@gimp.org>
2012-03-14 01:35:54 +0100

    app: remove the pointless core wrappers for the below simplified color stuff
    Instead, provide gimp_drawable_apply_operation_with_config() and call
    that from the PDB wrappers directly.
    
    
    Former-commit-id: 0623c7f3656daf64375c90ea913515cd63a8b4e8

--- app/actions/drawable-commands.c
+++ app/actions/drawable-commands.c
@@ -42,12 +42,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|"core/gimpdrawable-desaturate.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"core/gimpdrawable-equalize.h"
 end_include
 
