07c53e5c9d6ccb65e9d7f64d14a19c5c792f4d73
Michael Natterer <mitch@gimp.org>
2012-03-30 01:36:40 +0200

    app: remove gimpdrawable-process.[ch]
    Former-commit-id: 29e7837441782421f278c946ce7d8f268869520a

--- app/core/gimpdrawable-levels.c
+++ app/core/gimpdrawable-levels.c
@@ -54,12 +54,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|"gimpdrawable-process.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"gimphistogram.h"
 end_include
 
