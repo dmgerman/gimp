482efd1691f546929a21f3d9f41a5dc6ec906bbc
Michael Natterer <mitch@gimp.org>
2012-04-07 01:34:44 +0200

    app: add gimp_drawable_get_base_type()
    Former-commit-id: f6f7d5302025cc5660816c5e847d41f1c10518d4

--- app/core/gimpdrawable.h
+++ app/core/gimpdrawable.h
@@ -1043,6 +1043,18 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
+name|GimpImageType
+name|gimp_drawable_get_base_type
+parameter_list|(
+specifier|const
+name|GimpDrawable
+modifier|*
+name|drawable
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
 name|gboolean
 name|gimp_drawable_is_rgb
 parameter_list|(
