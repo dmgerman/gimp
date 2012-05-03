c5ccca559e1c800f24b61ef0254b2f9a89ed21ac
Øyvind Kolås <pippin@gimp.org>
2012-05-03 03:57:13 +0200

    plugins: hide blur plug-in from menus
    Former-commit-id: b3776e2591e0c60662229f2a92d1350b6b4a6a21

--- plug-ins/common/blur.c
+++ plug-ins/common/blur.c
@@ -244,13 +244,6 @@ argument_list|,
 name|NULL
 argument_list|)
 expr_stmt|;
-name|gimp_plugin_menu_register
-argument_list|(
-name|PLUG_IN_PROC
-argument_list|,
-literal|"<Image>/Filters/Blur"
-argument_list|)
-expr_stmt|;
 block|}
 end_function
 
