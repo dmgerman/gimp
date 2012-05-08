1a1da24aed80af0bbeb4690eccf5657353aff5c4
Øyvind Kolås <pippin@gimp.org>
2012-05-08 21:21:55 +0200

    plug-ins: remove normalize plugin from menus
    This is the same as auto-levels
    
    
    Former-commit-id: 9b7860cbfc6171186fb9eea39dc6e4e40e024e64

--- plug-ins/common/contrast-normalize.c
+++ plug-ins/common/contrast-normalize.c
@@ -220,13 +220,6 @@ argument_list|,
 name|NULL
 argument_list|)
 expr_stmt|;
-name|gimp_plugin_menu_register
-argument_list|(
-name|PLUG_IN_PROC
-argument_list|,
-literal|"<Image>/Colors/Auto"
-argument_list|)
-expr_stmt|;
 block|}
 end_function
 
@@ -793,7 +786,7 @@ end_function
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon27458cac0108
+DECL|struct|__anon2bf737520108
 block|{
 DECL|member|lut
 name|guchar
