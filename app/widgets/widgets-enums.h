567f6fa9756209e4ad77a13a6d871bac046d9227
Michael Natterer <mitch@gimp.org>
2012-08-26 23:59:05 +0200

    app,cursors: add side and corner cursors in 45° steps
    So sides also for corners and corners also for sides, just to
    confuse the reader.
    
    
    Former-commit-id: 85b1f160040c085645a33273fc30d1ce01a56f0a

--- app/widgets/widgets-enums.h
+++ app/widgets/widgets-enums.h
@@ -41,7 +41,7 @@ end_decl_stmt
 begin_typedef
 typedef|typedef
 enum|enum
-DECL|enum|__anon296733670103
+DECL|enum|__anon2bc07e330103
 block|{
 DECL|enumerator|GIMP_ACTIVE_COLOR_FOREGROUND
 name|GIMP_ACTIVE_COLOR_FOREGROUND
@@ -77,7 +77,7 @@ end_decl_stmt
 begin_typedef
 typedef|typedef
 enum|enum
-DECL|enum|__anon296733670203
+DECL|enum|__anon2bc07e330203
 block|{
 DECL|enumerator|GIMP_COLOR_DIALOG_OK
 name|GIMP_COLOR_DIALOG_OK
@@ -114,7 +114,7 @@ end_decl_stmt
 begin_typedef
 typedef|typedef
 enum|enum
-DECL|enum|__anon296733670303
+DECL|enum|__anon2bc07e330303
 block|{
 DECL|enumerator|GIMP_COLOR_FRAME_MODE_PIXEL
 name|GIMP_COLOR_FRAME_MODE_PIXEL
@@ -158,7 +158,7 @@ end_decl_stmt
 begin_typedef
 typedef|typedef
 enum|enum
-DECL|enum|__anon296733670403
+DECL|enum|__anon2bc07e330403
 block|{
 DECL|enumerator|GIMP_COLOR_PICK_MODE_NONE
 name|GIMP_COLOR_PICK_MODE_NONE
@@ -202,7 +202,7 @@ end_decl_stmt
 begin_typedef
 typedef|typedef
 enum|enum
-DECL|enum|__anon296733670503
+DECL|enum|__anon2bc07e330503
 block|{
 DECL|enumerator|GIMP_COLOR_PICK_STATE_NEW
 name|GIMP_COLOR_PICK_STATE_NEW
@@ -236,7 +236,7 @@ end_decl_stmt
 begin_typedef
 typedef|typedef
 enum|enum
-DECL|enum|__anon296733670603
+DECL|enum|__anon2bc07e330603
 block|{
 DECL|enumerator|GIMP_HISTOGRAM_SCALE_LINEAR
 name|GIMP_HISTOGRAM_SCALE_LINEAR
@@ -272,7 +272,7 @@ end_decl_stmt
 begin_typedef
 typedef|typedef
 enum|enum
-DECL|enum|__anon296733670703
+DECL|enum|__anon2bc07e330703
 block|{
 DECL|enumerator|GIMP_TAB_STYLE_ICON
 name|GIMP_TAB_STYLE_ICON
@@ -340,7 +340,7 @@ end_decl_stmt
 begin_typedef
 typedef|typedef
 enum|enum
-DECL|enum|__anon296733670803
+DECL|enum|__anon2bc07e330803
 block|{
 DECL|enumerator|GIMP_TAG_ENTRY_MODE_QUERY
 name|GIMP_TAG_ENTRY_MODE_QUERY
@@ -361,7 +361,7 @@ begin_typedef
 typedef|typedef
 enum|enum
 comment|/*< skip>*/
-DECL|enum|__anon296733670903
+DECL|enum|__anon2bc07e330903
 block|{
 DECL|enumerator|GIMP_VIEW_BG_CHECKS
 name|GIMP_VIEW_BG_CHECKS
@@ -378,7 +378,7 @@ begin_typedef
 typedef|typedef
 enum|enum
 comment|/*< skip>*/
-DECL|enum|__anon296733670a03
+DECL|enum|__anon2bc07e330a03
 block|{
 DECL|enumerator|GIMP_VIEW_BORDER_BLACK
 name|GIMP_VIEW_BORDER_BLACK
@@ -401,7 +401,7 @@ begin_typedef
 typedef|typedef
 enum|enum
 comment|/*< skip>*/
-DECL|enum|__anon296733670b03
+DECL|enum|__anon2bc07e330b03
 block|{
 DECL|enumerator|GIMP_DND_TYPE_NONE
 name|GIMP_DND_TYPE_NONE
@@ -542,7 +542,7 @@ begin_typedef
 typedef|typedef
 enum|enum
 comment|/*< skip>*/
-DECL|enum|__anon296733670c03
+DECL|enum|__anon2bc07e330c03
 block|{
 DECL|enumerator|GIMP_DROP_NONE
 name|GIMP_DROP_NONE
@@ -562,7 +562,7 @@ begin_typedef
 typedef|typedef
 enum|enum
 comment|/*< skip>*/
-DECL|enum|__anon296733670d03
+DECL|enum|__anon2bc07e330d03
 block|{
 DECL|enumerator|GIMP_CURSOR_NONE
 name|GIMP_CURSOR_NONE
@@ -594,17 +594,35 @@ block|,
 DECL|enumerator|GIMP_CURSOR_CORNER_TOP_LEFT
 name|GIMP_CURSOR_CORNER_TOP_LEFT
 block|,
+DECL|enumerator|GIMP_CURSOR_CORNER_TOP
+name|GIMP_CURSOR_CORNER_TOP
+block|,
 DECL|enumerator|GIMP_CURSOR_CORNER_TOP_RIGHT
 name|GIMP_CURSOR_CORNER_TOP_RIGHT
+block|,
+DECL|enumerator|GIMP_CURSOR_CORNER_LEFT
+name|GIMP_CURSOR_CORNER_LEFT
+block|,
+DECL|enumerator|GIMP_CURSOR_CORNER_RIGHT
+name|GIMP_CURSOR_CORNER_RIGHT
 block|,
 DECL|enumerator|GIMP_CURSOR_CORNER_BOTTOM_LEFT
 name|GIMP_CURSOR_CORNER_BOTTOM_LEFT
 block|,
+DECL|enumerator|GIMP_CURSOR_CORNER_BOTTOM
+name|GIMP_CURSOR_CORNER_BOTTOM
+block|,
 DECL|enumerator|GIMP_CURSOR_CORNER_BOTTOM_RIGHT
 name|GIMP_CURSOR_CORNER_BOTTOM_RIGHT
 block|,
+DECL|enumerator|GIMP_CURSOR_SIDE_TOP_LEFT
+name|GIMP_CURSOR_SIDE_TOP_LEFT
+block|,
 DECL|enumerator|GIMP_CURSOR_SIDE_TOP
 name|GIMP_CURSOR_SIDE_TOP
+block|,
+DECL|enumerator|GIMP_CURSOR_SIDE_TOP_RIGHT
+name|GIMP_CURSOR_SIDE_TOP_RIGHT
 block|,
 DECL|enumerator|GIMP_CURSOR_SIDE_LEFT
 name|GIMP_CURSOR_SIDE_LEFT
@@ -612,8 +630,14 @@ block|,
 DECL|enumerator|GIMP_CURSOR_SIDE_RIGHT
 name|GIMP_CURSOR_SIDE_RIGHT
 block|,
+DECL|enumerator|GIMP_CURSOR_SIDE_BOTTOM_LEFT
+name|GIMP_CURSOR_SIDE_BOTTOM_LEFT
+block|,
 DECL|enumerator|GIMP_CURSOR_SIDE_BOTTOM
 name|GIMP_CURSOR_SIDE_BOTTOM
+block|,
+DECL|enumerator|GIMP_CURSOR_SIDE_BOTTOM_RIGHT
+name|GIMP_CURSOR_SIDE_BOTTOM_RIGHT
 block|,
 DECL|enumerator|GIMP_CURSOR_LAST
 name|GIMP_CURSOR_LAST
@@ -627,7 +651,7 @@ begin_typedef
 typedef|typedef
 enum|enum
 comment|/*< skip>*/
-DECL|enum|__anon296733670e03
+DECL|enum|__anon2bc07e330e03
 block|{
 DECL|enumerator|GIMP_TOOL_CURSOR_NONE
 name|GIMP_TOOL_CURSOR_NONE
@@ -752,7 +776,7 @@ begin_typedef
 typedef|typedef
 enum|enum
 comment|/*< skip>*/
-DECL|enum|__anon296733670f03
+DECL|enum|__anon2bc07e330f03
 block|{
 DECL|enumerator|GIMP_CURSOR_MODIFIER_NONE
 name|GIMP_CURSOR_MODIFIER_NONE
@@ -808,7 +832,7 @@ begin_typedef
 typedef|typedef
 enum|enum
 comment|/*< skip>*/
-DECL|enum|__anon296733671003
+DECL|enum|__anon2bc07e331003
 block|{
 DECL|enumerator|GIMP_DEVICE_VALUE_MODE
 name|GIMP_DEVICE_VALUE_MODE
@@ -882,7 +906,7 @@ begin_typedef
 typedef|typedef
 enum|enum
 comment|/*< skip>*/
-DECL|enum|__anon296733671103
+DECL|enum|__anon2bc07e331103
 block|{
 DECL|enumerator|GIMP_FILE_CHOOSER_ACTION_OPEN
 name|GIMP_FILE_CHOOSER_ACTION_OPEN
@@ -902,7 +926,7 @@ begin_typedef
 typedef|typedef
 enum|enum
 comment|/*< skip>*/
-DECL|enum|__anon296733671203
+DECL|enum|__anon2bc07e331203
 block|{
 DECL|enumerator|GIMP_DIALOGS_SHOWN
 name|GIMP_DIALOGS_SHOWN
