5f320df2b99501fe3877455c62e3aa718e3931fe
Michael Natterer <mitch@gimp.org>
2012-09-21 19:18:34 +0200

    plug-ins: port file-bmp to GEGL
    Former-commit-id: 244b3766bdb2e890b15d0dcd8e4eadb30256a75e

--- plug-ins/file-bmp/bmp.c
+++ plug-ins/file-bmp/bmp.c
@@ -577,6 +577,13 @@ name|data
 operator|.
 name|d_int32
 expr_stmt|;
+name|gegl_init
+argument_list|(
+name|NULL
+argument_list|,
+name|NULL
+argument_list|)
+expr_stmt|;
 name|INIT_I18N
 argument_list|()
 expr_stmt|;
