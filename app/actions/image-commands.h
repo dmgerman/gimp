1d58a94fa61dfbd506962259691236b1bbe2bd12
Michael Natterer <mitch@gimp.org>
2012-09-22 20:35:36 +0200

    app: implement "Crop Image/Layer to Content" menu items in the core
    in a few lines using existing functionality, as replacement for the
    autocrop plugin. Rename the existing "crop" actions to
    "crop-to-selection".
    
    
    Former-commit-id: 796a9474d8e10cca53162a6df196636cfbf89e2a

--- app/actions/image-commands.h
+++ app/actions/image-commands.h
@@ -172,7 +172,21 @@ end_function_decl
 
 begin_function_decl
 name|void
-name|image_crop_cmd_callback
+name|image_crop_to_selection_cmd_callback
+parameter_list|(
+name|GtkAction
+modifier|*
+name|action
+parameter_list|,
+name|gpointer
+name|data
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|void
+name|image_crop_to_content_cmd_callback
 parameter_list|(
 name|GtkAction
 modifier|*
