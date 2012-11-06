b53bc6066ed98bb76c442d02d07d73bb2d835349
Michael Natterer <mitch@gimp.org>
2012-11-06 22:22:02 +0100

    Bug 687375 - Text tool: unnecessarily insists on having a layer before working
    There is no reason to disallow the text tool from working on a
    previously empty image, so allow it.
    
    
    Former-commit-id: eba022f01bba181bcdf07ba908e9c2638b44b639

--- app/tools/gimptexttool.c
+++ app/tools/gimptexttool.c
@@ -1157,6 +1157,15 @@ argument_list|,
 name|TRUE
 argument_list|)
 expr_stmt|;
+name|gimp_tool_control_set_handle_empty_image
+argument_list|(
+name|tool
+operator|->
+name|control
+argument_list|,
+name|TRUE
+argument_list|)
+expr_stmt|;
 name|gimp_tool_control_set_wants_click
 argument_list|(
 name|tool
