dmgerman/gimp

commit 63918899c860fa3ec4674585e4e743e565607921

Bug 721135 - Unable to export to .png - error: known incorrect sRGB profile
Some libpng errors can safely be marked as nonfatal, which is much
better than simply failing in those cases.

Thanks to John Bowler for pointing out this solution.


Former-commit-id: 7c493f76f836095adad08205fdfd4522e05e0bd4

plug-ins/common/file-png.c
@@ -162,7 +162,7 @@ end_comment
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2a108ac20108
+DECL|struct|__anon27613c5e0108
 block|{
 DECL|member|interlaced
 name|gboolean
@@ -225,7 +225,7 @@ end_typedef
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2a108ac20208
+DECL|struct|__anon27613c5e0208
 block|{
 DECL|member|run
 name|gboolean
@@ -309,7 +309,7 @@ end_comment
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2a108ac20308
+DECL|struct|__anon27613c5e0308
 block|{
 DECL|member|has_trns
 name|gboolean
@@ -2756,6 +2756,14 @@ return|return
 name|image
 return|;
 block|}
+comment|/* Change some libpng errors to warnings (e.g. bug 721135) */
+name|png_set_benign_errors
+argument_list|(
+name|pp
+argument_list|,
+name|TRUE
+argument_list|)
+expr_stmt|;
 comment|/*    * Open the file and initialize the PNG read "engine"...    */
 name|fp
 operator|=
@@ -5178,6 +5186,14 @@ return|return
 name|FALSE
 return|;
 block|}
+comment|/* Change some libpng errors to warnings (e.g. bug 721135) */
+name|png_set_benign_errors
+argument_list|(
+name|pp
+argument_list|,
+name|TRUE
+argument_list|)
+expr_stmt|;
 comment|/*    * Open the file and initialize the PNG write "engine"...    */
 name|fp
 operator|=

commit 6132202accafcc36cb35dd131b39379eab91f604

plug-ins: Fix build with libpng versions < 1.4.
This was broken in 63918899c860fa3ec4674585e4e743e565607921 [formerly 7c493f76f836095adad08205fdfd4522e05e0bd4].


Former-commit-id: d296a2c08a00f643177c7bacc0166d60417f579a

plug-ins/common/file-png.c
@@ -162,7 +162,7 @@ end_comment
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon27613c5e0108
+DECL|struct|__anon29b5a7750108
 block|{
 DECL|member|interlaced
 name|gboolean
@@ -225,7 +225,7 @@ end_typedef
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon27613c5e0208
+DECL|struct|__anon29b5a7750208
 block|{
 DECL|member|run
 name|gboolean
@@ -309,7 +309,7 @@ end_comment
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon27613c5e0308
+DECL|struct|__anon29b5a7750308
 block|{
 DECL|member|has_trns
 name|gboolean
@@ -2756,6 +2756,9 @@ return|return
 name|image
 return|;
 block|}
+ifdef|#
+directive|ifdef
+name|PNG_BENIGN_ERRORS_SUPPORTED
 comment|/* Change some libpng errors to warnings (e.g. bug 721135) */
 name|png_set_benign_errors
 argument_list|(
@@ -2764,6 +2767,8 @@ argument_list|,
 name|TRUE
 argument_list|)
 expr_stmt|;
+endif|#
+directive|endif
 comment|/*    * Open the file and initialize the PNG read "engine"...    */
 name|fp
 operator|=
@@ -5186,6 +5191,9 @@ return|return
 name|FALSE
 return|;
 block|}
+ifdef|#
+directive|ifdef
+name|PNG_BENIGN_ERRORS_SUPPORTED
 comment|/* Change some libpng errors to warnings (e.g. bug 721135) */
 name|png_set_benign_errors
 argument_list|(
@@ -5194,6 +5202,8 @@ argument_list|,
 name|TRUE
 argument_list|)
 expr_stmt|;
+endif|#
+directive|endif
 comment|/*    * Open the file and initialize the PNG write "engine"...    */
 name|fp
 operator|=