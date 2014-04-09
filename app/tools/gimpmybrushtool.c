c665e6526dc0756c43e77ca8f08b7849419b4ff2
Michael Natterer <mitch@gimp.org>
2014-04-09 15:41:56 +0200

    app: use gimp_paint_tool_set_draw_circle() in the MyPaint brush tool
    Former-commit-id: dd767bca37be8e2399effe31ca657d42554024a0

--- app/tools/gimpmybrushtool.c
+++ app/tools/gimpmybrushtool.c
@@ -24,6 +24,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|"libgimpmath/gimpmath.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|"libgimpwidgets/gimpwidgets.h"
 end_include
 
@@ -70,7 +76,6 @@ file|"gimp-intl.h"
 end_include
 
 begin_macro
-DECL|function|G_DEFINE_TYPE (GimpMybrushTool,gimp_mybrush_tool,GIMP_TYPE_PAINT_TOOL)
 name|G_DEFINE_TYPE
 argument_list|(
 argument|GimpMybrushTool
@@ -89,8 +94,21 @@ name|parent_class
 value|gimp_mybrush_tool_parent_class
 end_define
 
+begin_function_decl
+specifier|static
+name|void
+name|gimp_mybrush_tool_draw
+parameter_list|(
+name|GimpDrawTool
+modifier|*
+name|draw_tool
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_function
 name|void
+DECL|function|gimp_mybrush_tool_register (GimpToolRegisterCallback callback,gpointer data)
 name|gimp_mybrush_tool_register
 parameter_list|(
 name|GimpToolRegisterCallback
@@ -160,7 +178,23 @@ name|GimpMybrushToolClass
 modifier|*
 name|klass
 parameter_list|)
-block|{ }
+block|{
+name|GimpDrawToolClass
+modifier|*
+name|draw_tool_class
+init|=
+name|GIMP_DRAW_TOOL_CLASS
+argument_list|(
+name|klass
+argument_list|)
+decl_stmt|;
+name|draw_tool_class
+operator|->
+name|draw
+operator|=
+name|gimp_mybrush_tool_draw
+expr_stmt|;
+block|}
 end_function
 
 begin_function
@@ -200,6 +234,62 @@ name|mybrush_tool
 argument_list|)
 argument_list|,
 name|GIMP_COLOR_PICK_MODE_FOREGROUND
+argument_list|)
+expr_stmt|;
+block|}
+end_function
+
+begin_function
+specifier|static
+name|void
+DECL|function|gimp_mybrush_tool_draw (GimpDrawTool * draw_tool)
+name|gimp_mybrush_tool_draw
+parameter_list|(
+name|GimpDrawTool
+modifier|*
+name|draw_tool
+parameter_list|)
+block|{
+name|GimpPaintTool
+modifier|*
+name|paint_tool
+init|=
+name|GIMP_PAINT_TOOL
+argument_list|(
+name|draw_tool
+argument_list|)
+decl_stmt|;
+name|GimpMybrushOptions
+modifier|*
+name|options
+init|=
+name|GIMP_MYBRUSH_TOOL_GET_OPTIONS
+argument_list|(
+name|draw_tool
+argument_list|)
+decl_stmt|;
+name|gimp_paint_tool_set_draw_circle
+argument_list|(
+name|paint_tool
+argument_list|,
+name|TRUE
+argument_list|,
+name|exp
+argument_list|(
+name|options
+operator|->
+name|radius
+argument_list|)
+argument_list|)
+expr_stmt|;
+name|GIMP_DRAW_TOOL_CLASS
+argument_list|(
+name|parent_class
+argument_list|)
+operator|->
+name|draw
+argument_list|(
+name|draw_tool
 argument_list|)
 expr_stmt|;
 block|}
