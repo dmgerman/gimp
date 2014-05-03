d2d1b0e7e72914232a1dd5923e482cc74a7d8f81
Michael Natterer <mitch@gimp.org>
2014-05-03 20:24:09 +0200

    app: connect the warp tool's effect size to the standard tool size action
    Former-commit-id: 7d398da360d1d08d9cac0d9fd4cfe38e6c364b61

--- app/tools/gimpwarptool.c
+++ app/tools/gimpwarptool.c
@@ -777,6 +777,15 @@ argument_list|,
 name|GIMP_TOOL_CURSOR_PERSPECTIVE
 argument_list|)
 expr_stmt|;
+name|gimp_tool_control_set_action_size
+argument_list|(
+name|tool
+operator|->
+name|control
+argument_list|,
+literal|"tools/tools-warp-effect-size-set"
+argument_list|)
+expr_stmt|;
 block|}
 end_function
 
