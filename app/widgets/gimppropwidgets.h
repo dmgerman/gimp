2a639e7e49369e35cb83da98504c2aca29b30794
Michael Natterer <mitch@gimp.org>
2014-05-25 21:52:39 +0200

    app: add gimp_prop_angle_dial_new() which handles deg and rad
    Former-commit-id: 4ec888a771c5452fcb2189a0d1ddfcb7e45e140e

--- app/widgets/gimppropwidgets.h
+++ app/widgets/gimppropwidgets.h
@@ -188,6 +188,23 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|GtkWidget
+modifier|*
+name|gimp_prop_angle_dial_new
+parameter_list|(
+name|GObject
+modifier|*
+name|config
+parameter_list|,
+specifier|const
+name|gchar
+modifier|*
+name|property_name
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_comment
 comment|/*  GParamObject (GimpViewable)  */
 end_comment
