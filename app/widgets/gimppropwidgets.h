5357d6bee8beffed1355ddb2275e7e8356e5d3cb
Michael Natterer <mitch@gimp.org>
2014-05-30 00:33:02 +0200

    app: add gimp_prop_polar_new()
    Former-commit-id: fe7aa1384da90ddfe6484e23567b920b02bc1b64

--- app/widgets/gimppropwidgets.h
+++ app/widgets/gimppropwidgets.h
@@ -232,6 +232,28 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|GtkWidget
+modifier|*
+name|gimp_prop_polar_new
+parameter_list|(
+name|GObject
+modifier|*
+name|config
+parameter_list|,
+specifier|const
+name|gchar
+modifier|*
+name|angle_property_name
+parameter_list|,
+specifier|const
+name|gchar
+modifier|*
+name|radius_property_name
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_comment
 comment|/*  GParamObject (GimpViewable)  */
 end_comment
