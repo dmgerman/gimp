db20353659cf60fa570409761ed36ca94cb66650
Michael Natterer <mitch@gimp.org>
2014-06-05 15:50:05 +0200

    libgimp: update docs of gimp_image_metadata_save_prepare()
    Former-commit-id: 16e01dc193622e9127f2238e40a1b11563e517df

--- libgimp/gimpmetadata.c
+++ libgimp/gimpmetadata.c
@@ -444,7 +444,7 @@ block|}
 end_function
 
 begin_comment
-comment|/**  * gimp_image_metadata_save_prepare:  * @image_ID:  The image  * @mime_type: The saved file's mime-type  *  * Gets the image metadata for saving it using  * gimp_image_metadata_save_finish().  *  * Returns: The image's metadata, prepared for saving.  *  * Since: GIMP 2.10  */
+comment|/**  * gimp_image_metadata_save_prepare:  * @image_ID:        The image  * @mime_type:       The saved file's mime-type  * @suggested_flags: Suggested default values for the @flags passed to  *                   gimp_image_metadata_save_finish()  *  * Gets the image metadata for saving it using  * gimp_image_metadata_save_finish().  *  * The @suggested_flags are determined from what kind of metadata  * (Exif, XMP, ...) is actually present in the image. The suggested  * value for GIMP_METADATA_SAVE_THUMBNAIL is determined by whether  * there was a thumbnail in the previously imported image.  *  * Returns: The image's metadata, prepared for saving.  *  * Since: GIMP 2.10  */
 end_comment
 
 begin_function
