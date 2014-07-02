dmgerman/gimp

commit 87b01c3ad186a3caf38e629eb69cc98aafb71cb0

libgimpconfig: port GimpConfigWriter to GIO
Former-commit-id: 53b5822926bf93cbc30c181ee9c5916c3ac21034

libgimpconfig/gimpconfigwriter.c
@@ -12,54 +12,13 @@ end_include
 begin_include
 include|#
 directive|include
-file|<errno.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<fcntl.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<string.h>
-end_include
-
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|HAVE_UNISTD_H
-end_ifdef
-
-begin_include
-include|#
-directive|include
-file|<unistd.h>
-end_include
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_include
-include|#
-directive|include
-file|<sys/types.h>
 end_include
 
 begin_include
 include|#
 directive|include
 file|<gio/gio.h>
-end_include
-
-begin_include
-include|#
-directive|include
-file|<glib/gstdio.h>
 end_include
 
 begin_ifdef
@@ -71,7 +30,18 @@ end_ifdef
 begin_include
 include|#
 directive|include
-file|<io.h>
+file|<gio/gwin32outputstream.h>
+end_include
+
+begin_else
+else|#
+directive|else
+end_else
+
+begin_include
+include|#
+directive|include
+file|<gio/gunixoutputstream.h>
 end_include
 
 begin_endif
@@ -136,19 +106,15 @@ DECL|struct|_GimpConfigWriter
 struct|struct
 name|_GimpConfigWriter
 block|{
-DECL|member|fd
-name|gint
-name|fd
-decl_stmt|;
-DECL|member|filename
-name|gchar
+DECL|member|output
+name|GOutputStream
 modifier|*
-name|filename
+name|output
 decl_stmt|;
-DECL|member|tmpname
-name|gchar
+DECL|member|file
+name|GFile
 modifier|*
-name|tmpname
+name|file
 decl_stmt|;
 DECL|member|error
 name|GError
@@ -231,13 +197,23 @@ modifier|*
 name|writer
 parameter_list|)
 block|{
+name|gsize
+name|bytes_written
+decl_stmt|;
+name|GError
+modifier|*
+name|error
+init|=
+name|NULL
+decl_stmt|;
 if|if
 condition|(
-name|write
+operator|!
+name|g_output_stream_write_all
 argument_list|(
 name|writer
 operator|->
-name|fd
+name|output
 argument_list|,
 name|writer
 operator|->
@@ -250,10 +226,42 @@ operator|->
 name|buffer
 operator|->
 name|len
+argument_list|,
+operator|&
+name|bytes_written
+argument_list|,
+name|NULL
+argument_list|,
+operator|&
+name|error
 argument_list|)
-operator|<
-literal|0
 condition|)
+block|{
+specifier|const
+name|gchar
+modifier|*
+name|path
+decl_stmt|;
+if|if
+condition|(
+name|writer
+operator|->
+name|file
+condition|)
+name|path
+operator|=
+name|gimp_file_get_utf8_name
+argument_list|(
+name|writer
+operator|->
+name|file
+argument_list|)
+expr_stmt|;
+else|else
+name|path
+operator|=
+literal|"file descriptor"
+expr_stmt|;
 name|g_set_error
 argument_list|(
 operator|&
@@ -270,19 +278,20 @@ argument_list|(
 literal|"Error writing to '%s': %s"
 argument_list|)
 argument_list|,
-name|gimp_filename_to_utf8
-argument_list|(
-name|writer
-operator|->
-name|filename
-argument_list|)
+name|path
 argument_list|,
-name|g_strerror
-argument_list|(
-name|errno
-argument_list|)
+name|error
+operator|->
+name|message
 argument_list|)
 expr_stmt|;
+name|g_clear_error
+argument_list|(
+operator|&
+name|error
+argument_list|)
+expr_stmt|;
+block|}
 name|g_string_truncate
 argument_list|(
 name|writer
@@ -398,14 +407,9 @@ name|GimpConfigWriter
 modifier|*
 name|writer
 decl_stmt|;
-name|gchar
+name|GFile
 modifier|*
-name|tmpname
-init|=
-name|NULL
-decl_stmt|;
-name|gint
-name|fd
+name|file
 decl_stmt|;
 name|g_return_val_if_fail
 argument_list|(
@@ -430,174 +434,31 @@ argument_list|,
 name|NULL
 argument_list|)
 expr_stmt|;
-if|if
-condition|(
+name|file
+operator|=
+name|g_file_new_for_path
+argument_list|(
+name|filename
+argument_list|)
+expr_stmt|;
+name|writer
+operator|=
+name|gimp_config_writer_new_gfile
+argument_list|(
+name|file
+argument_list|,
 name|atomic
-condition|)
-block|{
-name|tmpname
-operator|=
-name|g_strconcat
-argument_list|(
-name|filename
-argument_list|,
-literal|"XXXXXX"
-argument_list|,
-name|NULL
-argument_list|)
-expr_stmt|;
-name|fd
-operator|=
-name|g_mkstemp
-argument_list|(
-name|tmpname
-argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|fd
-operator|==
-operator|-
-literal|1
-condition|)
-block|{
-name|g_set_error
-argument_list|(
-name|error
-argument_list|,
-name|GIMP_CONFIG_ERROR
-argument_list|,
-name|GIMP_CONFIG_ERROR_WRITE
-argument_list|,
-name|_
-argument_list|(
-literal|"Could not create temporary file for '%s': %s"
-argument_list|)
-argument_list|,
-name|gimp_filename_to_utf8
-argument_list|(
-name|filename
-argument_list|)
-argument_list|,
-name|g_strerror
-argument_list|(
-name|errno
-argument_list|)
-argument_list|)
-expr_stmt|;
-name|g_free
-argument_list|(
-name|tmpname
-argument_list|)
-expr_stmt|;
-return|return
-name|NULL
-return|;
-block|}
-block|}
-else|else
-block|{
-name|fd
-operator|=
-name|g_creat
-argument_list|(
-name|filename
-argument_list|,
-literal|0644
-argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|fd
-operator|==
-operator|-
-literal|1
-condition|)
-block|{
-name|g_set_error
-argument_list|(
-name|error
-argument_list|,
-name|GIMP_CONFIG_ERROR
-argument_list|,
-name|GIMP_CONFIG_ERROR_WRITE
-argument_list|,
-name|_
-argument_list|(
-literal|"Could not open '%s' for writing: %s"
-argument_list|)
-argument_list|,
-name|gimp_filename_to_utf8
-argument_list|(
-name|filename
-argument_list|)
-argument_list|,
-name|g_strerror
-argument_list|(
-name|errno
-argument_list|)
-argument_list|)
-expr_stmt|;
-return|return
-name|NULL
-return|;
-block|}
-block|}
-name|writer
-operator|=
-name|g_slice_new0
-argument_list|(
-name|GimpConfigWriter
-argument_list|)
-expr_stmt|;
-name|writer
-operator|->
-name|fd
-operator|=
-name|fd
-expr_stmt|;
-name|writer
-operator|->
-name|filename
-operator|=
-name|g_strdup
-argument_list|(
-name|filename
-argument_list|)
-expr_stmt|;
-name|writer
-operator|->
-name|tmpname
-operator|=
-name|tmpname
-expr_stmt|;
-name|writer
-operator|->
-name|buffer
-operator|=
-name|g_string_new
-argument_list|(
-name|NULL
-argument_list|)
-expr_stmt|;
-if|if
-condition|(
-name|header
-condition|)
-block|{
-name|gimp_config_writer_comment
-argument_list|(
-name|writer
 argument_list|,
 name|header
+argument_list|,
+name|error
 argument_list|)
 expr_stmt|;
-name|gimp_config_writer_linefeed
+name|g_object_unref
 argument_list|(
-name|writer
+name|file
 argument_list|)
 expr_stmt|;
-block|}
 return|return
 name|writer
 return|;
@@ -636,9 +497,15 @@ name|GimpConfigWriter
 modifier|*
 name|writer
 decl_stmt|;
-name|gchar
+name|GOutputStream
 modifier|*
-name|path
+name|output
+decl_stmt|;
+name|GError
+modifier|*
+name|my_error
+init|=
+name|NULL
 decl_stmt|;
 name|g_return_val_if_fail
 argument_list|(
@@ -650,31 +517,192 @@ argument_list|,
 name|NULL
 argument_list|)
 expr_stmt|;
-name|path
+name|g_return_val_if_fail
+argument_list|(
+name|error
+operator|==
+name|NULL
+operator|||
+operator|*
+name|error
+operator|==
+name|NULL
+argument_list|,
+name|NULL
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|atomic
+condition|)
+block|{
+name|output
 operator|=
-name|g_file_get_path
+name|G_OUTPUT_STREAM
+argument_list|(
+name|g_file_replace
+argument_list|(
+name|file
+argument_list|,
+name|NULL
+argument_list|,
+name|FALSE
+argument_list|,
+name|G_FILE_CREATE_NONE
+argument_list|,
+name|NULL
+argument_list|,
+operator|&
+name|my_error
+argument_list|)
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+operator|!
+name|output
+condition|)
+name|g_set_error
+argument_list|(
+name|error
+argument_list|,
+name|GIMP_CONFIG_ERROR
+argument_list|,
+name|GIMP_CONFIG_ERROR_WRITE
+argument_list|,
+name|_
+argument_list|(
+literal|"Could not create temporary file for '%s': %s"
+argument_list|)
+argument_list|,
+name|gimp_file_get_utf8_name
+argument_list|(
+name|file
+argument_list|)
+argument_list|,
+name|my_error
+operator|->
+name|message
+argument_list|)
+expr_stmt|;
+block|}
+else|else
+block|{
+name|output
+operator|=
+name|G_OUTPUT_STREAM
+argument_list|(
+name|g_file_replace
+argument_list|(
+name|file
+argument_list|,
+name|NULL
+argument_list|,
+name|FALSE
+argument_list|,
+name|G_FILE_CREATE_REPLACE_DESTINATION
+argument_list|,
+name|NULL
+argument_list|,
+operator|&
+name|my_error
+argument_list|)
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+operator|!
+name|output
+condition|)
+name|g_set_error
+argument_list|(
+name|error
+argument_list|,
+name|GIMP_CONFIG_ERROR
+argument_list|,
+name|GIMP_CONFIG_ERROR_WRITE
+argument_list|,
+name|_
+argument_list|(
+literal|"Could not open '%s' for writing: %s"
+argument_list|)
+argument_list|,
+name|gimp_file_get_utf8_name
+argument_list|(
+name|file
+argument_list|)
+argument_list|,
+name|my_error
+operator|->
+name|message
+argument_list|)
+expr_stmt|;
+block|}
+if|if
+condition|(
+operator|!
+name|output
+condition|)
+block|{
+name|g_clear_error
+argument_list|(
+operator|&
+name|my_error
+argument_list|)
+expr_stmt|;
+return|return
+name|NULL
+return|;
+block|}
+name|writer
+operator|=
+name|g_slice_new0
+argument_list|(
+name|GimpConfigWriter
+argument_list|)
+expr_stmt|;
+name|writer
+operator|->
+name|output
+operator|=
+name|output
+expr_stmt|;
+name|writer
+operator|->
+name|file
+operator|=
+name|g_object_ref
 argument_list|(
 name|file
 argument_list|)
 expr_stmt|;
 name|writer
+operator|->
+name|buffer
 operator|=
-name|gimp_config_writer_new_file
+name|g_string_new
 argument_list|(
-name|path
-argument_list|,
-name|atomic
+name|NULL
+argument_list|)
+expr_stmt|;
+if|if
+condition|(
+name|header
+condition|)
+block|{
+name|gimp_config_writer_comment
+argument_list|(
+name|writer
 argument_list|,
 name|header
-argument_list|,
-name|error
 argument_list|)
 expr_stmt|;
-name|g_free
+name|gimp_config_writer_linefeed
 argument_list|(
-name|path
+name|writer
 argument_list|)
 expr_stmt|;
+block|}
 return|return
 name|writer
 return|;
@@ -715,12 +743,39 @@ argument_list|(
 name|GimpConfigWriter
 argument_list|)
 expr_stmt|;
+ifdef|#
+directive|ifdef
+name|G_OS_WIN32
 name|writer
 operator|->
-name|fd
+name|output
 operator|=
+name|g_win32_input_stream_new
+argument_list|(
+operator|(
+name|HANDLE
+operator|*
+operator|)
 name|fd
+argument_list|,
+name|FALSE
+argument_list|)
 expr_stmt|;
+else|#
+directive|else
+name|writer
+operator|->
+name|output
+operator|=
+name|g_unix_output_stream_new
+argument_list|(
+name|fd
+argument_list|,
+name|FALSE
+argument_list|)
+expr_stmt|;
+endif|#
+directive|endif
 name|writer
 operator|->
 name|buffer
@@ -1519,7 +1574,7 @@ if|if
 condition|(
 name|writer
 operator|->
-name|fd
+name|output
 condition|)
 name|gimp_config_writer_flush
 argument_list|(
@@ -1633,7 +1688,7 @@ if|if
 condition|(
 name|writer
 operator|->
-name|fd
+name|output
 condition|)
 block|{
 name|success
@@ -1645,18 +1700,17 @@ argument_list|,
 name|error
 argument_list|)
 expr_stmt|;
-name|g_free
+if|if
+condition|(
+name|writer
+operator|->
+name|file
+condition|)
+name|g_object_unref
 argument_list|(
 name|writer
 operator|->
-name|filename
-argument_list|)
-expr_stmt|;
-name|g_free
-argument_list|(
-name|writer
-operator|->
-name|tmpname
+name|file
 argument_list|)
 expr_stmt|;
 name|g_string_free
@@ -1667,13 +1721,6 @@ name|buffer
 argument_list|,
 name|TRUE
 argument_list|)
-expr_stmt|;
-block|}
-else|else
-block|{
-name|success
-operator|=
-name|TRUE
 expr_stmt|;
 block|}
 if|if
@@ -1750,22 +1797,39 @@ operator|->
 name|comment
 condition|)
 block|{
+name|gsize
+name|bytes_written
+decl_stmt|;
+name|GError
+modifier|*
+name|error
+init|=
+name|NULL
+decl_stmt|;
 if|if
 condition|(
-name|write
+operator|!
+name|g_output_stream_write_all
 argument_list|(
 name|writer
 operator|->
-name|fd
+name|output
 argument_list|,
 literal|"\n"
 argument_list|,
 literal|1
+argument_list|,
+operator|&
+name|bytes_written
+argument_list|,
+name|NULL
+argument_list|,
+operator|&
+name|error
 argument_list|)
-operator|<
-literal|0
 condition|)
-name|g_set_error_literal
+block|{
+name|g_set_error
 argument_list|(
 operator|&
 name|writer
@@ -1776,12 +1840,30 @@ name|GIMP_CONFIG_ERROR
 argument_list|,
 name|GIMP_CONFIG_ERROR_WRITE
 argument_list|,
-name|g_strerror
+name|_
 argument_list|(
-name|errno
+literal|"Error writing to '%s': %s"
 argument_list|)
+argument_list|,
+name|gimp_file_get_utf8_name
+argument_list|(
+name|writer
+operator|->
+name|file
+argument_list|)
+argument_list|,
+name|error
+operator|->
+name|message
 argument_list|)
 expr_stmt|;
+name|g_clear_error
+argument_list|(
+operator|&
+name|error
+argument_list|)
+expr_stmt|;
+block|}
 block|}
 else|else
 block|{
@@ -2040,13 +2122,19 @@ modifier|*
 name|error
 parameter_list|)
 block|{
+name|GError
+modifier|*
+name|my_error
+init|=
+name|NULL
+decl_stmt|;
 name|g_return_val_if_fail
 argument_list|(
 name|writer
 operator|->
-name|fd
+name|output
 operator|!=
-literal|0
+name|NULL
 argument_list|,
 name|FALSE
 argument_list|)
@@ -2056,7 +2144,7 @@ condition|(
 operator|!
 name|writer
 operator|->
-name|filename
+name|file
 condition|)
 return|return
 name|TRUE
@@ -2068,60 +2156,37 @@ operator|->
 name|error
 condition|)
 block|{
-name|close
+name|g_object_unref
 argument_list|(
 name|writer
 operator|->
-name|fd
+name|output
 argument_list|)
 expr_stmt|;
-if|if
-condition|(
 name|writer
 operator|->
-name|tmpname
-condition|)
-name|g_unlink
-argument_list|(
-name|writer
-operator|->
-name|tmpname
-argument_list|)
+name|output
+operator|=
+name|NULL
 expr_stmt|;
 return|return
 name|FALSE
 return|;
 block|}
-ifdef|#
-directive|ifdef
-name|HAVE_FSYNC
-comment|/* If the final destination exists, we want to sync the newly written    * file to ensure the data is on disk when we rename over the destination.    * otherwise if we get a system crash we can lose both the new and the    * old file on some filesystems. (I.E. those that don't guarantee the    * data is written to the disk before the metadata.)    */
 if|if
 condition|(
-name|writer
-operator|->
-name|tmpname
-operator|&&
-name|g_file_test
+operator|!
+name|g_output_stream_close
 argument_list|(
 name|writer
 operator|->
-name|filename
+name|output
 argument_list|,
-name|G_FILE_TEST_EXISTS
+name|NULL
+argument_list|,
+operator|&
+name|my_error
 argument_list|)
-condition|)
-block|{
-if|if
-condition|(
-name|fsync
-argument_list|(
-name|writer
-operator|->
-name|fd
-argument_list|)
-operator|!=
-literal|0
 condition|)
 block|{
 name|g_set_error
@@ -2134,236 +2199,57 @@ name|GIMP_CONFIG_ERROR_WRITE
 argument_list|,
 name|_
 argument_list|(
-literal|"Error writing to temporary file for '%s': %s\n"
-literal|"The original file has not been touched."
+literal|"Error writing '%s': %s"
 argument_list|)
 argument_list|,
-name|gimp_filename_to_utf8
+name|gimp_file_get_utf8_name
 argument_list|(
 name|writer
 operator|->
-name|filename
+name|file
 argument_list|)
 argument_list|,
-name|g_strerror
-argument_list|(
-name|errno
-argument_list|)
+name|my_error
+operator|->
+name|message
 argument_list|)
 expr_stmt|;
-name|close
+name|g_clear_error
+argument_list|(
+operator|&
+name|my_error
+argument_list|)
+expr_stmt|;
+name|g_object_unref
 argument_list|(
 name|writer
 operator|->
-name|fd
+name|output
 argument_list|)
 expr_stmt|;
-name|g_unlink
-argument_list|(
 name|writer
 operator|->
-name|tmpname
-argument_list|)
+name|output
+operator|=
+name|NULL
 expr_stmt|;
 return|return
 name|FALSE
 return|;
 block|}
-block|}
-endif|#
-directive|endif
-if|if
-condition|(
-name|close
+name|g_object_unref
 argument_list|(
 name|writer
 operator|->
-name|fd
-argument_list|)
-operator|!=
-literal|0
-condition|)
-block|{
-if|if
-condition|(
-name|writer
-operator|->
-name|tmpname
-condition|)
-block|{
-if|if
-condition|(
-name|g_file_test
-argument_list|(
-name|writer
-operator|->
-name|filename
-argument_list|,
-name|G_FILE_TEST_EXISTS
-argument_list|)
-condition|)
-block|{
-name|g_set_error
-argument_list|(
-name|error
-argument_list|,
-name|GIMP_CONFIG_ERROR
-argument_list|,
-name|GIMP_CONFIG_ERROR_WRITE
-argument_list|,
-name|_
-argument_list|(
-literal|"Error writing to temporary file for '%s': %s\n"
-literal|"The original file has not been touched."
-argument_list|)
-argument_list|,
-name|gimp_filename_to_utf8
-argument_list|(
-name|writer
-operator|->
-name|filename
-argument_list|)
-argument_list|,
-name|g_strerror
-argument_list|(
-name|errno
-argument_list|)
+name|output
 argument_list|)
 expr_stmt|;
-block|}
-else|else
-block|{
-name|g_set_error
-argument_list|(
-name|error
-argument_list|,
-name|GIMP_CONFIG_ERROR
-argument_list|,
-name|GIMP_CONFIG_ERROR_WRITE
-argument_list|,
-name|_
-argument_list|(
-literal|"Error writing to temporary file for '%s': %s\n"
-literal|"No file has been created."
-argument_list|)
-argument_list|,
-name|gimp_filename_to_utf8
-argument_list|(
 name|writer
 operator|->
-name|filename
-argument_list|)
-argument_list|,
-name|g_strerror
-argument_list|(
-name|errno
-argument_list|)
-argument_list|)
+name|output
+operator|=
+name|NULL
 expr_stmt|;
-block|}
-name|g_unlink
-argument_list|(
-name|writer
-operator|->
-name|tmpname
-argument_list|)
-expr_stmt|;
-block|}
-else|else
-block|{
-name|g_set_error
-argument_list|(
-name|error
-argument_list|,
-name|GIMP_CONFIG_ERROR
-argument_list|,
-name|GIMP_CONFIG_ERROR_WRITE
-argument_list|,
-name|_
-argument_list|(
-literal|"Error writing to '%s': %s"
-argument_list|)
-argument_list|,
-name|gimp_filename_to_utf8
-argument_list|(
-name|writer
-operator|->
-name|filename
-argument_list|)
-argument_list|,
-name|g_strerror
-argument_list|(
-name|errno
-argument_list|)
-argument_list|)
-expr_stmt|;
-block|}
-return|return
-name|FALSE
-return|;
-block|}
-if|if
-condition|(
-name|writer
-operator|->
-name|tmpname
-condition|)
-block|{
-if|if
-condition|(
-name|g_rename
-argument_list|(
-name|writer
-operator|->
-name|tmpname
-argument_list|,
-name|writer
-operator|->
-name|filename
-argument_list|)
-operator|==
-operator|-
-literal|1
-condition|)
-block|{
-name|g_set_error
-argument_list|(
-name|error
-argument_list|,
-name|GIMP_CONFIG_ERROR
-argument_list|,
-name|GIMP_CONFIG_ERROR_WRITE
-argument_list|,
-name|_
-argument_list|(
-literal|"Could not create '%s': %s"
-argument_list|)
-argument_list|,
-name|gimp_filename_to_utf8
-argument_list|(
-name|writer
-operator|->
-name|filename
-argument_list|)
-argument_list|,
-name|g_strerror
-argument_list|(
-name|errno
-argument_list|)
-argument_list|)
-expr_stmt|;
-name|g_unlink
-argument_list|(
-name|writer
-operator|->
-name|tmpname
-argument_list|)
-expr_stmt|;
-return|return
-name|FALSE
-return|;
-block|}
-block|}
 return|return
 name|TRUE
 return|;

libgimpconfig/Makefile.am
@@ -47,6 +47,8 @@ AM_CPPFLAGS = \
 	-DG_LOG_DOMAIN=\"LibGimpConfig\"	\
 	-DGIMP_CONFIG_COMPILATION		\
 	-I$(top_srcdir)				\
+	$(GIO_UNIX_CFLAGS)			\
+	$(GIO_WINDOWS_CFLAGS)			\
 	$(GEGL_CFLAGS)				\
 	$(CAIRO_CFLAGS)				\
 	$(GDK_PIXBUF_CFLAGS)			\
@@ -113,11 +115,13 @@ libgimpconfig_@GIMP_API_VERSION@_la_LDFLAGS = \
 EXTRA_libgimpconfig_@GIMP_API_VERSION@_la_DEPENDENCIES = $(gimpconfig_def)
 
 libgimpconfig_@GIMP_API_VERSION@_la_LIBADD = \
-	$(libgimpbase)	\
-	$(libgimpcolor)	\
-	$(libgimpmath)	\
-	$(GEGL_LIBS)	\
-	$(CAIRO_LIBS)	\
+	$(libgimpbase)		\
+	$(libgimpcolor)		\
+	$(libgimpmath)		\
+	$(GIO_UNIX_LIBS)	\
+	$(GIO_WINDOWS_LIBS)	\
+	$(GEGL_LIBS)		\
+	$(CAIRO_LIBS)		\
 	$(GDK_PIXBUF_LIBS)
 
 

commit a8d7a20b172d20e6b1a9668af55430be1c78616c

app: fix building for windows.
This was broken in 87b01c3ad186a3caf38e629eb69cc98aafb71cb0 [formerly 53b5822926bf93cbc30c181ee9c5916c3ac21034]


Former-commit-id: 33af6c61b74be59da888ce287cfb6f0fdafc2f9b

libgimpconfig/gimpconfigwriter.c
@@ -750,11 +750,10 @@ name|writer
 operator|->
 name|output
 operator|=
-name|g_win32_input_stream_new
+name|g_win32_output_stream_new
 argument_list|(
 operator|(
-name|HANDLE
-operator|*
+name|gpointer
 operator|)
 name|fd
 argument_list|,