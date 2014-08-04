aa3c9b73fc2ac819619b191586407c5d90c94ac9
Michael Natterer <mitch@gimp.org>
2014-08-04 02:38:24 +0200

    app: add gimp_file_is_executable() to gimp-utils.[ch]
    including PATHEXT magic for windows.
    
    
    Former-commit-id: 9bb3aee1638909b6520271e40bc0706a5c189174

--- app/core/gimp-utils.h
+++ app/core/gimp-utils.h
@@ -493,6 +493,17 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
+name|gboolean
+name|gimp_file_is_executable
+parameter_list|(
+name|GFile
+modifier|*
+name|file
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
 name|void
 name|gimp_create_image_from_buffer
 parameter_list|(
