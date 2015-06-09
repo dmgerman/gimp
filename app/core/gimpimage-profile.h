d2b5fbc7d3508e0dde87f7c844a6a4c64e0def4e
Michael Natterer <mitch@gimp.org>
2015-06-09 18:55:07 +0200

    app: add gimp_image_validate_color_profile()
    and use it from gimp_image_set_icc_profile() and from
    gimp_image_set_color_profile().
    
    
    Former-commit-id: 685f883f3e52856d17e8e15dd4eed90cb9e63ab3

--- app/core/gimpimage-profile.h
+++ app/core/gimpimage-profile.h
@@ -75,6 +75,25 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
+name|gboolean
+name|gimp_image_validate_color_profile
+parameter_list|(
+name|GimpImage
+modifier|*
+name|image
+parameter_list|,
+name|GimpColorProfile
+name|profile
+parameter_list|,
+name|GError
+modifier|*
+modifier|*
+name|error
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
 name|GimpColorProfile
 name|gimp_image_get_color_profile
 parameter_list|(
