c2fcec8dc40fa8dc7de5007b7ac4240535f18fa7
Michael Natterer <mitch@gimp.org>
2015-12-28 16:47:39 +0100

    app: move GimpMybrushPrivate to its own header, perparation for more hacking
    Also, return the error message if the preview can't be loaded.
    
    
    Former-commit-id: 15183c210b68232a6b93bc8e295f0be7224a5d6a

--- app/core/gimpmybrush-load.c
+++ app/core/gimpmybrush-load.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* GIMP - The GNU Image Manipulation Program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * gimpbrush-load.c  *  * This program is free software: you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program.  If not, see<http://www.gnu.org/licenses/>.  */
+comment|/* GIMP - The GNU Image Manipulation Program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * gimpmybrush-load.c  *  * This program is free software: you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program.  If not, see<http://www.gnu.org/licenses/>.  */
 end_comment
 
 begin_include
@@ -43,6 +43,12 @@ begin_include
 include|#
 directive|include
 file|"gimpmybrush-load.h"
+end_include
+
+begin_include
+include|#
+directive|include
+file|"gimpmybrush-private.h"
 end_include
 
 begin_include
@@ -187,7 +193,12 @@ literal|48
 argument_list|,
 literal|48
 argument_list|,
-name|NULL
+name|error
+argument_list|)
+expr_stmt|;
+name|g_free
+argument_list|(
+name|preview_filename
 argument_list|)
 expr_stmt|;
 if|if
@@ -237,11 +248,6 @@ name|pixbuf
 argument_list|)
 expr_stmt|;
 block|}
-name|g_free
-argument_list|(
-name|preview_filename
-argument_list|)
-expr_stmt|;
 if|if
 condition|(
 operator|!
--- app/core/gimpmybrush.c
+++ app/core/gimpmybrush.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* GIMP - The GNU Image Manipulation Program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * This program is free software: you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program.  If not, see<http://www.gnu.org/licenses/>.  */
+comment|/* GIMP - The GNU Image Manipulation Program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * gimpmybrush.c  *  * This program is free software: you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program.  If not, see<http://www.gnu.org/licenses/>.  */
 end_comment
 
 begin_include
@@ -54,6 +54,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|"gimpmybrush-private.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|"gimptagged.h"
 end_include
 
@@ -62,36 +68,6 @@ include|#
 directive|include
 file|"gimp-intl.h"
 end_include
-
-begin_struct
-DECL|struct|_GimpMybrushPrivate
-struct|struct
-name|_GimpMybrushPrivate
-block|{
-DECL|member|json_loaded
-name|gboolean
-name|json_loaded
-decl_stmt|;
-DECL|member|brush_json
-name|gchar
-modifier|*
-name|brush_json
-decl_stmt|;
-DECL|member|radius
-name|gdouble
-name|radius
-decl_stmt|;
-DECL|member|opaque
-name|gdouble
-name|opaque
-decl_stmt|;
-DECL|member|hardness
-name|gdouble
-name|hardness
-decl_stmt|;
-block|}
-struct|;
-end_struct
 
 begin_function_decl
 specifier|static
