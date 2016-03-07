b874b68623206c33cb993794c6524bc2566096e3
Michael Natterer <mitch@gimp.org>
2016-03-07 21:33:42 +0100

    app: add gimpdrawable-fill.[ch]
    for the filling code from gimpdrawable-stroke.[ch]; and clean up and
    untangle the stroking and filling functions.
    
    
    Former-commit-id: f2a98566d515c2782b69474de03e529b4e6455d4

--- app/core/gimpdrawable-fill.h
+++ app/core/gimpdrawable-fill.h
@@ -1,30 +1,30 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* GIMP - The GNU Image Manipulation Program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * gimpdrawable-stroke.h  * Copyright (C) 2003 Simon Budig<simon@gimp.org>  *  * This program is free software: you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program.  If not, see<http://www.gnu.org/licenses/>.  */
+comment|/* GIMP - The GNU Image Manipulation Program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * This program is free software: you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program.  If not, see<http://www.gnu.org/licenses/>.  */
 end_comment
 
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|__GIMP_DRAWABLE_STROKE_H__
+name|__GIMP_DRAWABLE_FILL_H__
 end_ifndef
 
 begin_define
-DECL|macro|__GIMP_DRAWABLE_STROKE_H__
+DECL|macro|__GIMP_DRAWABLE_FILL_H__
 define|#
 directive|define
-name|__GIMP_DRAWABLE_STROKE_H__
+name|__GIMP_DRAWABLE_FILL_H__
 end_define
 
 begin_function_decl
 name|void
-name|gimp_drawable_stroke_boundary
+name|gimp_drawable_fill_boundary
 parameter_list|(
 name|GimpDrawable
 modifier|*
 name|drawable
 parameter_list|,
-name|GimpStrokeOptions
+name|GimpFillOptions
 modifier|*
 name|options
 parameter_list|,
@@ -50,13 +50,13 @@ end_function_decl
 
 begin_function_decl
 name|gboolean
-name|gimp_drawable_stroke_vectors
+name|gimp_drawable_fill_vectors
 parameter_list|(
 name|GimpDrawable
 modifier|*
 name|drawable
 parameter_list|,
-name|GimpStrokeOptions
+name|GimpFillOptions
 modifier|*
 name|options
 parameter_list|,
@@ -77,13 +77,13 @@ end_function_decl
 
 begin_function_decl
 name|void
-name|gimp_drawable_stroke_scan_convert
+name|gimp_drawable_fill_scan_convert
 parameter_list|(
 name|GimpDrawable
 modifier|*
 name|drawable
 parameter_list|,
-name|GimpStrokeOptions
+name|GimpFillOptions
 modifier|*
 name|options
 parameter_list|,
@@ -103,7 +103,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/*  __GIMP_DRAWABLE_STROKE_H__  */
+comment|/* __GIMP_DRAWABLE_FILL_H__ */
 end_comment
 
 end_unit
