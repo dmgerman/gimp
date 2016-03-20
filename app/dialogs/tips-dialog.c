d33ed982165b0b13da8f570f544f73c84f336aef
Michael Natterer <mitch@gimp.org>
2016-03-20 20:53:28 +0100

    Revert "Bug 762279 - "Tip of the day" links opens two links"
    This reverts commit 3ee47291fa9c91f846727949172687266e0be9d8 [formerly dc33afd95b0a7aec7f6a8a4bf69eb8f0eccf7faf].
    
    That hack is not for gtk3...
    
    
    Former-commit-id: 52fe0dad60251eed0cc48f43f05a367d006a4e3e

--- app/dialogs/tips-dialog.c
+++ app/dialogs/tips-dialog.c
@@ -77,7 +77,7 @@ end_include
 
 begin_enum
 enum|enum
-DECL|enum|__anon2bad8dbe0103
+DECL|enum|__anon2aac5b220103
 block|{
 DECL|enumerator|RESPONSE_PREVIOUS
 name|RESPONSE_PREVIOUS
@@ -91,46 +91,6 @@ literal|2
 block|}
 enum|;
 end_enum
-
-begin_comment
-comment|/* eek, see bug 762279 */
-end_comment
-
-begin_function_decl
-name|GtkLinkButtonUriFunc
-name|gtk_link_button_set_uri_hook
-parameter_list|(
-name|GtkLinkButtonUriFunc
-name|func
-parameter_list|,
-name|gpointer
-name|data
-parameter_list|,
-name|GDestroyNotify
-name|destroy
-parameter_list|)
-function_decl|;
-end_function_decl
-
-begin_function_decl
-specifier|static
-name|void
-name|tips_uri_hook
-parameter_list|(
-name|GtkLinkButton
-modifier|*
-name|button
-parameter_list|,
-specifier|const
-name|gchar
-modifier|*
-name|link_
-parameter_list|,
-name|gpointer
-name|user_data
-parameter_list|)
-function_decl|;
-end_function_decl
 
 begin_function_decl
 specifier|static
@@ -947,16 +907,6 @@ argument_list|,
 literal|0
 argument_list|)
 expr_stmt|;
-comment|/* this is deprecated but better than showing two URIs, see bug #762279 */
-name|gtk_link_button_set_uri_hook
-argument_list|(
-name|tips_uri_hook
-argument_list|,
-name|NULL
-argument_list|,
-name|NULL
-argument_list|)
-expr_stmt|;
 name|g_signal_connect
 argument_list|(
 name|more_button
@@ -1140,14 +1090,15 @@ operator|->
 name|text
 argument_list|)
 expr_stmt|;
-name|gtk_link_button_set_visited
+comment|/*  set the URI to unset the "visited" state  */
+name|gtk_link_button_set_uri
 argument_list|(
 name|GTK_LINK_BUTTON
 argument_list|(
 name|more_button
 argument_list|)
 argument_list|,
-name|FALSE
+literal|"http://docs.gimp.org/"
 argument_list|)
 expr_stmt|;
 name|gtk_widget_set_sensitive
@@ -1187,13 +1138,6 @@ name|current_tip
 operator|->
 name|data
 decl_stmt|;
-name|g_signal_stop_emission_by_name
-argument_list|(
-name|button
-argument_list|,
-literal|"clicked"
-argument_list|)
-expr_stmt|;
 if|if
 condition|(
 name|tip
@@ -1213,29 +1157,6 @@ operator|->
 name|help_id
 argument_list|)
 expr_stmt|;
-block|}
-end_function
-
-begin_function
-specifier|static
-name|void
-DECL|function|tips_uri_hook (GtkLinkButton * button,const gchar * link_,gpointer user_data)
-name|tips_uri_hook
-parameter_list|(
-name|GtkLinkButton
-modifier|*
-name|button
-parameter_list|,
-specifier|const
-name|gchar
-modifier|*
-name|link_
-parameter_list|,
-name|gpointer
-name|user_data
-parameter_list|)
-block|{
-comment|/* do nothing */
 block|}
 end_function
 
