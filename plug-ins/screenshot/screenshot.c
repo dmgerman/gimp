3536d05267f17cc0fcc6e3ef9222bd69735112e5
Michael Natterer <mitch@gimp.org>
2016-04-02 21:44:09 +0200

    plug-ins: more cleanup in screenhot, move the icon to its own file
    Former-commit-id: d64120340bf895d39233165de8370acdba75a741

--- plug-ins/screenshot/screenshot.c
+++ plug-ins/screenshot/screenshot.c
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* GIMP - The GNU Image Manipulation Program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * Screenshot plug-in  * Copyright 1998-2007 Sven Neumann<sven@gimp.org>  * Copyright 2003      Henrik Brix Andersen<brix@gimp.org>  * Copyright 2012      Simone Karin Lehmann - OS X patches  *  * This program is free software: you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program.  If not, see<http://www.gnu.org/licenses/>.  */
+comment|/* GIMP - The GNU Image Manipulation Program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * Screenshot plug-in  * Copyright 1998-2007 Sven Neumann<sven@gimp.org>  * Copyright 2003      Henrik Brix Andersen<brix@gimp.org>  * Copyright 2012      Simone Karin Lehmann - OS X patches  * Copyright 2016      Michael Natterer<mitch@gimp.org>  *  * This program is free software: you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program.  If not, see<http://www.gnu.org/licenses/>.  */
 end_comment
 
 begin_include
@@ -36,6 +36,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|"screenshot-icon.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|"screenshot-osx.h"
 end_include
 
@@ -56,144 +62,6 @@ include|#
 directive|include
 file|"libgimp/stdplugins-intl.h"
 end_include
-
-begin_comment
-comment|/* GdkPixbuf RGBA C-Source image dump 1-byte-run-length-encoded */
-end_comment
-
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|__SUNPRO_C
-end_ifdef
-
-begin_pragma
-pragma|#
-directive|pragma
-name|align
-name|4
-name|(
-name|screenshot_icon
-name|)
-end_pragma
-
-begin_endif
-endif|#
-directive|endif
-end_endif
-
-begin_ifdef
-ifdef|#
-directive|ifdef
-name|__GNUC__
-end_ifdef
-
-begin_decl_stmt
-DECL|variable|screenshot_icon
-specifier|static
-specifier|const
-name|guint8
-name|screenshot_icon
-index|[]
-name|__attribute__
-argument_list|(
-operator|(
-name|__aligned__
-argument_list|(
-literal|4
-argument_list|)
-operator|)
-argument_list|)
-init|=
-else|#
-directive|else
-specifier|static
-specifier|const
-name|guint8
-name|screenshot_icon
-index|[]
-operator|=
-endif|#
-directive|endif
-block|{
-literal|""
-comment|/* Pixbuf magic (0x47646b50) */
-literal|"GdkP"
-comment|/* length: header (24) + pixel_data (1582) */
-literal|"\0\0\6F"
-comment|/* pixdata_type (0x2010002) */
-literal|"\2\1\0\2"
-comment|/* rowstride (88) */
-literal|"\0\0\0X"
-comment|/* width (22) */
-literal|"\0\0\0\26"
-comment|/* height (22) */
-literal|"\0\0\0\26"
-comment|/* pixel_data: */
-literal|"\213\0\0\0\0\1\242\242\242\5\203\242\242\242\31\221\0\0\0\0\2\27\27\26"
-literal|"D\40\40\40\372\203)))\375\2\32\32\32\362\26\26\25"
-literal|"0\217\0\0\0\0\2\2"
-literal|"\2\2\322\307\310\307\377\203\377\377\377\377\2\263\263\262\363\0\0\0"
-literal|"\314\217\0\0\0\0\2\6\6\6\334\206\207\205\377\202\252\253\251\377\3\252"
-literal|"\252\251\377ghe\376\1\1\1\320\217\0\0\0\0\2\11\11\11\346./-\345\202G"
-literal|"HE\25\3JKH\32"
-literal|"01/\364\5\5\5\333\212\0\0\0\0\17\26\26\25\31\26\26\25"
-literal|"0\26\26\25g\0\0\0\314+,+\331LMK\375EFD\362ffe\347iig\346lmk\346RSQ\362"
-literal|"\13\13\13\360\0\0\0\314\2\2\2\321\26\26\25V\203\0\0\0\0#\26\26\25\31"
-literal|"\26\26\25"
-literal|"0\26\26\25n\3\3\3\331\2\2\2\332JJI\355\215\215\215\372\246"
-literal|"\246\246\347\267\270\266\362\177\202~\377BC\77\377TUR\377\\]Y\377gid"
-literal|"\377[]X\377\204\206\204\374wxw\276\224\225\224\314LLK\343\26\26\25D\0"
-literal|"\0\0\0\26\26\25D\3\3\3\341\17\17\17\373ghg\377\237\240\236\377\273\274"
-literal|"\272\377\302\303\300\377\272\273\270\377\200\201\177\377zzz\377tws\377"
-literal|"\220\223\217\377\221\225\221\377\224\227\223\377\202\226\232\226\377"
-literal|"K\177\202}\377xyu\377\217\221\215\377\226\227\226\365\0\0\0\371\0\0\0"
-literal|"\0\2\2\2\330\257\261\257\377\275\276\273\377\262\263\260\377UWT\377Q"
-literal|"SP\377suq\377]^[\377\304\304\304\377\303\303\303\377kmi\377MNJ\377WZ"
-literal|"X\377[`_\377aeb\377gid\377}\200{\377SUQ\377FGE\377\200\201\200\377\12"
-literal|"\12\12\357\0\0\0\0\4\4\4\345xyv\377\241\242\236\377\210\212\205\377v"
-literal|"xt\377\220\222\217\377GIF\377+,)\377```\377jji\377UXV\377y\204\210\377"
-literal|"\203\215\220\377~\204\207\377flo\377PW\\\377SWU\377JLI\377452\377tut"
-literal|"\377\12\12\12\357\0\0\0\0\4\4\4\344opm\377\221\223\217\377}\177{\377"
-literal|"\215\217\213\377`b_\377()'\377*+)\377785\377VXV\377u~\202\377nsu\377"
-literal|"VYZ\377OST\377OQS\377JOP\377^cd\377698\377!\"!\377llk\377\12\12\12\357"
-literal|"\0\0\0\0\3\3\3\345npl\377\220\221\216\377\202UWS\377\24GIF\377\40!\37"
-literal|"\377*+)\3779:7\377^ce\377NRT\377UXY\377(,.\377\22\24\25\377!$&\377IL"
-literal|"M\3778=\77\377RVX\377''&\377klk\377\12\12\12\357\0\0\0\1\3\3\3\345no"
-literal|"l\377\220\221\216\377\202UWS\377\24GIF\377\40!\37\377*+)\3778:8\377T"
-literal|"XY\377289\377\23\25\26\377\16\16\16\377\1\1\1\377\2\2\2\377\24\25\25"
-literal|"\377.34\377289\377011\377aa`\377\11\11\11\360\0\0\0\3\3\3\3\345nol\377"
-literal|"\204\205\202\377\202UWS\377\24GIF\377\40!\37\377*+)\377\77BA\37728:\377"
-literal|"\34\40!\377\31\32\32\377\377\377\377\377hhh\377\40\40\40\377\22\22\22"
-literal|"\377.00\377.46\377:>\77\377`a`\377\11\11\10\361\0\0\0\7\3\3\3\345mok"
-literal|"\377z{x\377\202UWS\377\24GIF\377\40!\37\377*+)\377HJI\377.46\377\25\27"
-literal|"\30\377\40\40\40\377hhh\377\232\232\232\377}}}\377'''\3779;;\377/57\377"
-literal|"599\377``_\377\11\11\10\362\0\0\0\15\3\3\3\345lnk\377rsp\377\202UWS\377"
-literal|"\24GIF\377\40!\37\377,-*\377>@>\377.46\377\35\37\40\377\1\1\1\377888"
-literal|"\377\214\214\214\377\213\213\213\377AAA\377JKK\377/57\3776:;\377WXV\377"
-literal|"\6\6\6\364\0\0\0\21\0\0\0\371\201\202\177\377mok\377\202UWS\377iGIF\377"
-literal|"MNM\377RRR\372ghg\364[`b\377+/0\377\17\17\17\377\"\"\"\377'''\377HHH"
-literal|"\377\263\263\263\377SUU\377W\\^\377CFF\374<<<\376\20\20\17\317\0\0\0"
-literal|"\20\24\24\23]\4\4\4\353\177\177~\375|}z\373}~|\374hih\3769:9\371\0\0"
-literal|"\0\347\0\0\0\351\7\7\7\367.46\377256\377\40\40\40\377(((\377:::\377`"
-literal|"ab\3779>\77\377\11\13\13\376\0\0\0\353\0\0\0\300\14\14\13H\0\0\0\15\0"
-literal|"\0\0\4\23\23\22L\0\0\0\332\0\0\0\341\0\0\0\346\0\0\0\341\14\14\13{\0"
-literal|"\0\0V\0\0\0a\0\0\0l\10\11\11\371\\^_\377LOP\377ILL\377LOQ\377QUV\377"
-literal|"\16\17\17\377\34\37\40\244\0\0\0`\0\0\0"
-literal|"4\0\0\0\27\0\0\0\7\0\0\0\1\0"
-literal|"\0\0\3\0\0\0\10\0\0\0\21\0\0\0\34\0\0\0'\0\0\0"
-literal|"0\0\0\0"
-literal|"6\0\0\0=\0\0"
-literal|"\0A\0\0\0F\4\5\5\355\12\13\13\371\17\20\20\376\15\16\16\375\10\10\11"
-literal|"\365\7\10\11j\0\0\0G\0\0\0-\0\0\0\30\0\0\0\13\0\0\0\4\0\0\0\0\0\0\0\1"
-literal|"\0\0\0\2\0\0\0\4\0\0\0\11\0\0\0\16\0\0\0\23\0\0\0\31\0\0\0\35\0\0\0!"
-literal|"\0\0\0#\0\0\0$\0\0\0%\0\0\0$\0\0\0&\0\0\0%\0\0\0\36\0\0\0\27\0\0\0\20"
-literal|"\0\0\0\10\0\0\0\3\0\0\0\1\204\0\0\0\0\5\0\0\0\1\0\0\0\2\0\0\0\4\0\0\0"
-literal|"\5\0\0\0\6\202\0\0\0\10\11\0\0\0\11\0\0\0\7\0\0\0\10\0\0\0\5\0\0\0\7"
-literal|"\0\0\0\6\0\0\0\5\0\0\0\3\0\0\0\1\202\0\0\0\0"
-block|}
-decl_stmt|;
-end_decl_stmt
 
 begin_comment
 comment|/* Defines */
@@ -394,18 +262,9 @@ block|{
 name|SHOOT_WINDOW
 block|,
 comment|/* root window  */
-ifdef|#
-directive|ifdef
-name|PLATFORM_OSX
-name|FALSE
-block|,
-else|#
-directive|else
 name|TRUE
 block|,
 comment|/* include WM decorations */
-endif|#
-directive|endif
 literal|0
 block|,
 comment|/* window ID    */
@@ -422,6 +281,7 @@ block|,
 literal|0
 block|,
 name|FALSE
+comment|/* show cursor */
 block|}
 decl_stmt|;
 end_decl_stmt
@@ -751,6 +611,13 @@ name|capabilities
 operator|=
 name|screenshot_osx_get_capabilities
 argument_list|()
+expr_stmt|;
+comment|/* on OS X, this just means shoot the shadow, default to nope */
+name|shootvals
+operator|.
+name|decorate
+operator|=
+name|FALSE
 expr_stmt|;
 block|}
 endif|#
