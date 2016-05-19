07fea49f4907965d5468def096ab5f3dff3df394
Michael Natterer <mitch@gimp.org>
2016-05-19 19:22:09 +0200

    app: include gimpdrawable-floating-selection.h in gimplayer-floating-sel.c
    Former-commit-id: 2b170e3a80f3977748ba89fb4e10c8c2871a7772

--- app/core/gimplayer-floating-sel.c
+++ app/core/gimplayer-floating-sel.c
@@ -48,6 +48,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|"gimpdrawable-floating-selection.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|"gimperror.h"
 end_include
 
