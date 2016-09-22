6e40bf06e1834f44ec13a3ff03ef99f4c9d22d33
Michael Natterer <mitch@gimp.org>
2016-09-22 18:52:46 +0200

    app: add gimp_prefs_box_set_page_resettable()
    which adds a "Reset" button to a prefs page.
    
    
    Former-commit-id: cc8b5cd37ecf8036a4d4adadf68d5e8edb0c80e0

--- app/widgets/gimpprefsbox.h
+++ app/widgets/gimpprefsbox.h
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:C;cregit-version:0.0.1
 begin_comment
-comment|/* GIMP - The GNU Image Manipulation Program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * gimpprefsbox.h  * Copyright (C) 2013 Michael Natterer<mitch@gimp.org>  *  * This program is free software: you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program.  If not, see<http://www.gnu.org/licenses/>.  */
+comment|/* GIMP - The GNU Image Manipulation Program  * Copyright (C) 1995 Spencer Kimball and Peter Mattis  *  * gimpprefsbox.h  * Copyright (C) 2013-2016 Michael Natterer<mitch@gimp.org>  *  * This program is free software: you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program.  If not, see<http://www.gnu.org/licenses/>.  */
 end_comment
 
 begin_ifndef
@@ -188,6 +188,27 @@ name|page
 parameter_list|,
 name|gboolean
 name|scrollable
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|GtkWidget
+modifier|*
+name|gimp_prefs_box_set_page_resettable
+parameter_list|(
+name|GimpPrefsBox
+modifier|*
+name|box
+parameter_list|,
+name|GtkWidget
+modifier|*
+name|page
+parameter_list|,
+specifier|const
+name|gchar
+modifier|*
+name|label
 parameter_list|)
 function_decl|;
 end_function_decl
