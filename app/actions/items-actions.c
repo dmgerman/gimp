c341560acf595097ef4da12e901f52af59610083
Michael Natterer <mitch@gimp.org>
2016-10-30 18:05:57 +0100

    app: color manage the new color tag color areas
    Former-commit-id: 9f9307edcb609f06b7e81f74c7e93deb9a627f9e

--- app/actions/items-actions.c
+++ app/actions/items-actions.c
@@ -36,6 +36,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|"core/gimp.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|"core/gimpitem.h"
 end_include
 
@@ -151,6 +157,20 @@ block|{
 name|GimpRGB
 name|color
 decl_stmt|;
+name|gimp_action_group_set_action_context
+argument_list|(
+name|group
+argument_list|,
+name|action
+argument_list|,
+name|gimp_get_user_context
+argument_list|(
+name|group
+operator|->
+name|gimp
+argument_list|)
+argument_list|)
+expr_stmt|;
 name|gimp_get_color_tag_color
 argument_list|(
 name|value
