08fc0e78c4ffc1b6013a22e41b84b60d948c2fa1
Øyvind Kolås <pippin@gimp.org>
2017-01-11 22:13:14 +0100

    app: add gimp_gegl_mode_is_linear
    Former-commit-id: 377f4687b1d0ad513fec861c0348d5ba41862d1a

--- app/gegl/gimp-gegl-nodes.h
+++ app/gegl/gimp-gegl-nodes.h
@@ -135,6 +135,16 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|gboolean
+name|gimp_gegl_mode_is_linear
+parameter_list|(
+name|GimpLayerMode
+name|mode
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_endif
 endif|#
 directive|endif
