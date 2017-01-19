302c2c2fafd1dfccc14f7412e0a685884f355887
Jehan <jehan@girinstud.io>
2017-01-19 15:01:54 +0100

    libgimpbase: update gimp_unit_get_digits() description.
    Digit accuracy being "approximately the same as an inch with 2 digits"
    is only true for built-in units, not user-defined ones.
    
    
    Former-commit-id: 7a330dcf6a05675b4c83b0ff6c5b9947b5fffc60

--- libgimpbase/gimpunit.c
+++ libgimpbase/gimpunit.c
@@ -555,7 +555,7 @@ block|}
 end_function
 
 begin_comment
-comment|/**  * gimp_unit_get_digits:  * @unit: The unit you want to know the digits.  *  * Returns the number of digits an entry field should provide to get  * approximately the same accuracy as an inch input field with two digits.  *  * Returns 0 for @unit == GIMP_UNIT_PIXEL.  *  * Returns: The suggested number of digits.  **/
+comment|/**  * gimp_unit_get_digits:  * @unit: The unit you want to know the digits.  *  * Returns the number of digits set for @unit.  * Built-in units' accuracy is approximately the same as an inch with  * two digits. User-defined units can suggest a different accuracy.  *  * Returns 0 for @unit == GIMP_UNIT_PIXEL.  *  * Returns: The suggested number of digits.  **/
 end_comment
 
 begin_function
