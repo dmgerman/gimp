dmgerman/gimp

commit df579165c412417f4b2c8b14cbe469f74a74a607

libgimpwidgets: make GimpDialog's help_id settable after construction
Former-commit-id: a94030052521e481cfe996044947c13b4e5c0276

libgimpwidgets/gimpdialog.c
@@ -39,7 +39,7 @@ end_comment
 
 begin_enum
 enum|enum
-DECL|enum|__anon2b89795f0103
+DECL|enum|__anon29528fed0103
 block|{
 DECL|enumerator|PROP_0
 name|PROP_0
@@ -402,8 +402,6 @@ argument_list|,
 name|NULL
 argument_list|,
 name|GIMP_PARAM_READWRITE
-operator||
-name|G_PARAM_CONSTRUCT_ONLY
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -803,6 +801,13 @@ break|break;
 case|case
 name|PROP_HELP_ID
 case|:
+name|g_free
+argument_list|(
+name|private
+operator|->
+name|help_id
+argument_list|)
+expr_stmt|;
 name|private
 operator|->
 name|help_id
@@ -810,6 +815,20 @@ operator|=
 name|g_value_dup_string
 argument_list|(
 name|value
+argument_list|)
+expr_stmt|;
+name|gimp_help_set_help_data
+argument_list|(
+name|GTK_WIDGET
+argument_list|(
+name|object
+argument_list|)
+argument_list|,
+name|NULL
+argument_list|,
+name|private
+operator|->
+name|help_id
 argument_list|)
 expr_stmt|;
 break|break;
@@ -1738,7 +1757,7 @@ end_function
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2b89795f0208
+DECL|struct|__anon29528fed0208
 block|{
 DECL|member|dialog
 name|GtkDialog

commit ee3f251e515d870f193dcff8659e92add0e01e49

libgimpwidgets: show help buttons in dialogs again
GimpDialog's "help-id" property didn't have G_PARAM_CONSTRUCT.

Broken since commit df579165c412417f4b2c8b14cbe469f74a74a607 [formerly a94030052521e481cfe996044947c13b4e5c0276].


Former-commit-id: b119fc995a00e9e029ded11bbf19389477983659

libgimpwidgets/gimpdialog.c
@@ -45,7 +45,7 @@ end_comment
 
 begin_enum
 enum|enum
-DECL|enum|__anon28f77c490103
+DECL|enum|__anon27b6d3df0103
 block|{
 DECL|enumerator|PROP_0
 name|PROP_0
@@ -408,6 +408,8 @@ argument_list|,
 name|NULL
 argument_list|,
 name|GIMP_PARAM_READWRITE
+operator||
+name|G_PARAM_CONSTRUCT
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -1766,7 +1768,7 @@ end_function
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon28f77c490208
+DECL|struct|__anon27b6d3df0208
 block|{
 DECL|member|dialog
 name|GtkDialog