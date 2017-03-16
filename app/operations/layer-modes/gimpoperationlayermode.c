dmgerman/gimp

commit ee348f200d592044eefcbe12e6f2220ef2a23ebd

app: improve performance of HSV [sic] color mode
No need to do full back and forth RGB/HSL conversions.


Former-commit-id: 45516a0ca9ddbda3ff982e3d240f03e1910b0c6f

app/operations/layer-modes/gimpoperationlayermode.c
@@ -95,7 +95,7 @@ end_define
 
 begin_enum
 enum|enum
-DECL|enum|__anon2aa8bbd50103
+DECL|enum|__anon27926a5c0103
 block|{
 DECL|enumerator|PROP_0
 name|PROP_0
@@ -7364,121 +7364,317 @@ operator|!=
 literal|0.0f
 condition|)
 block|{
-name|GimpRGB
-name|dest_rgb
-init|=
-block|{
-name|dest
-index|[
-literal|0
-index|]
-block|,
-name|dest
-index|[
-literal|1
-index|]
-block|,
-name|dest
-index|[
-literal|2
-index|]
-block|}
-decl_stmt|;
-name|GimpRGB
-name|src_rgb
-init|=
-block|{
-name|src
-index|[
-literal|0
-index|]
-block|,
-name|src
-index|[
-literal|1
-index|]
-block|,
-name|src
-index|[
-literal|2
-index|]
-block|}
-decl_stmt|;
-name|GimpHSL
-name|src_hsl
+name|gfloat
+name|dest_min
 decl_stmt|,
-name|dest_hsl
+name|dest_max
+decl_stmt|,
+name|dest_l
 decl_stmt|;
-name|gimp_rgb_to_hsl
-argument_list|(
-operator|&
-name|dest_rgb
-argument_list|,
-operator|&
-name|dest_hsl
-argument_list|)
-expr_stmt|;
-name|gimp_rgb_to_hsl
-argument_list|(
-operator|&
-name|src_rgb
-argument_list|,
-operator|&
-name|src_hsl
-argument_list|)
-expr_stmt|;
-name|dest_hsl
-operator|.
-name|h
+name|gfloat
+name|src_min
+decl_stmt|,
+name|src_max
+decl_stmt|,
+name|src_l
+decl_stmt|;
+name|dest_min
 operator|=
-name|src_hsl
-operator|.
-name|h
-expr_stmt|;
-name|dest_hsl
-operator|.
-name|s
-operator|=
-name|src_hsl
-operator|.
-name|s
-expr_stmt|;
-name|gimp_hsl_to_rgb
+name|MIN
 argument_list|(
-operator|&
-name|dest_hsl
+name|dest
+index|[
+literal|0
+index|]
 argument_list|,
-operator|&
-name|dest_rgb
+name|dest
+index|[
+literal|1
+index|]
 argument_list|)
 expr_stmt|;
+name|dest_min
+operator|=
+name|MIN
+argument_list|(
+name|dest_min
+argument_list|,
+name|dest
+index|[
+literal|2
+index|]
+argument_list|)
+expr_stmt|;
+name|dest_max
+operator|=
+name|MAX
+argument_list|(
+name|dest
+index|[
+literal|0
+index|]
+argument_list|,
+name|dest
+index|[
+literal|1
+index|]
+argument_list|)
+expr_stmt|;
+name|dest_max
+operator|=
+name|MAX
+argument_list|(
+name|dest_max
+argument_list|,
+name|dest
+index|[
+literal|2
+index|]
+argument_list|)
+expr_stmt|;
+name|dest_l
+operator|=
+operator|(
+name|dest_min
+operator|+
+name|dest_max
+operator|)
+operator|/
+literal|2.0f
+expr_stmt|;
+name|src_min
+operator|=
+name|MIN
+argument_list|(
+name|src
+index|[
+literal|0
+index|]
+argument_list|,
+name|src
+index|[
+literal|1
+index|]
+argument_list|)
+expr_stmt|;
+name|src_min
+operator|=
+name|MIN
+argument_list|(
+name|src_min
+argument_list|,
+name|src
+index|[
+literal|2
+index|]
+argument_list|)
+expr_stmt|;
+name|src_max
+operator|=
+name|MAX
+argument_list|(
+name|src
+index|[
+literal|0
+index|]
+argument_list|,
+name|src
+index|[
+literal|1
+index|]
+argument_list|)
+expr_stmt|;
+name|src_max
+operator|=
+name|MAX
+argument_list|(
+name|src_max
+argument_list|,
+name|src
+index|[
+literal|2
+index|]
+argument_list|)
+expr_stmt|;
+name|src_l
+operator|=
+operator|(
+name|src_min
+operator|+
+name|src_max
+operator|)
+operator|/
+literal|2.0f
+expr_stmt|;
+if|if
+condition|(
+name|src_l
+operator|!=
+literal|0.0f
+operator|&&
+name|src_l
+operator|!=
+literal|1.0f
+condition|)
+block|{
+name|gfloat
+name|factor
+decl_stmt|;
+name|gfloat
+name|offset
+decl_stmt|;
+name|gint
+name|c
+decl_stmt|;
+if|if
+condition|(
+name|src_l
+operator|<
+literal|0.5f
+condition|)
+block|{
+if|if
+condition|(
+name|dest_l
+operator|<
+literal|0.5f
+condition|)
+block|{
+name|factor
+operator|=
+name|dest_l
+operator|/
+name|src_l
+expr_stmt|;
+name|offset
+operator|=
+literal|0.0f
+expr_stmt|;
+block|}
+else|else
+block|{
+name|factor
+operator|=
+literal|1.0f
+operator|-
+name|dest_l
+operator|/
+name|src_l
+expr_stmt|;
+name|offset
+operator|=
+literal|2.0f
+operator|*
+name|dest_l
+operator|-
+literal|1.0f
+expr_stmt|;
+block|}
+block|}
+else|else
+block|{
+if|if
+condition|(
+name|dest_l
+operator|<
+literal|0.5f
+condition|)
+block|{
+name|factor
+operator|=
+name|dest_l
+operator|/
+operator|(
+literal|1.0f
+operator|-
+name|src_l
+operator|)
+expr_stmt|;
+name|offset
+operator|=
+literal|2.0f
+operator|*
+name|dest_l
+operator|-
+name|factor
+expr_stmt|;
+block|}
+else|else
+block|{
+name|factor
+operator|=
+operator|(
+literal|1.0f
+operator|-
+name|dest_l
+operator|)
+operator|/
+operator|(
+literal|1.0f
+operator|-
+name|src_l
+operator|)
+expr_stmt|;
+name|offset
+operator|=
+literal|1.0f
+operator|-
+name|factor
+expr_stmt|;
+block|}
+block|}
+for|for
+control|(
+name|c
+operator|=
+literal|0
+init|;
+name|c
+operator|<
+literal|3
+condition|;
+name|c
+operator|++
+control|)
+name|out
+index|[
+name|c
+index|]
+operator|=
+name|src
+index|[
+name|c
+index|]
+operator|*
+name|factor
+operator|+
+name|offset
+expr_stmt|;
+block|}
+else|else
+block|{
 name|out
 index|[
 name|RED
 index|]
 operator|=
-name|dest_rgb
-operator|.
-name|r
+name|dest_l
 expr_stmt|;
 name|out
 index|[
 name|GREEN
 index|]
 operator|=
-name|dest_rgb
-operator|.
-name|g
+name|dest_l
 expr_stmt|;
 name|out
 index|[
 name|BLUE
 index|]
 operator|=
-name|dest_rgb
-operator|.
-name|b
+name|dest_l
 expr_stmt|;
+block|}
 block|}
 name|out
 index|[

commit 19744eea0771b86a695f3a5e40ab1ea62d8b8cfc

app: fix and streamline HSL color layer mode
Fixes an error introduced by commit ee348f200d592044eefcbe12e6f2220ef2a23ebd [formerly 45516a0ca9ddbda3ff982e3d240f03e1910b0c6f].

Also, make the code more consistent across the different HS[VL] modes.


Former-commit-id: 8055f14e6b43bf613aee9dcc8a738da1684fd2a2

app/operations/layer-modes/gimpoperationlayermode.c
@@ -95,7 +95,7 @@ end_define
 
 begin_enum
 enum|enum
-DECL|enum|__anon294b48e70103
+DECL|enum|__anon28c50d600103
 block|{
 DECL|enumerator|PROP_0
 name|PROP_0
@@ -7517,8 +7517,14 @@ operator|!=
 literal|1.0f
 condition|)
 block|{
+name|gboolean
+name|dest_high
+decl_stmt|;
+name|gboolean
+name|src_high
+decl_stmt|;
 name|gfloat
-name|factor
+name|ratio
 decl_stmt|;
 name|gfloat
 name|offset
@@ -7526,21 +7532,41 @@ decl_stmt|;
 name|gint
 name|c
 decl_stmt|;
-if|if
-condition|(
-name|src_l
-operator|<
-literal|0.5f
-condition|)
-block|{
-if|if
-condition|(
+name|dest_high
+operator|=
 name|dest_l
-operator|<
+operator|>
 literal|0.5f
-condition|)
-block|{
-name|factor
+expr_stmt|;
+name|src_high
+operator|=
+name|src_l
+operator|>
+literal|0.5f
+expr_stmt|;
+name|dest_l
+operator|=
+name|MIN
+argument_list|(
+name|dest_l
+argument_list|,
+literal|1.0f
+operator|-
+name|dest_l
+argument_list|)
+expr_stmt|;
+name|src_l
+operator|=
+name|MIN
+argument_list|(
+name|src_l
+argument_list|,
+literal|1.0f
+operator|-
+name|src_l
+argument_list|)
+expr_stmt|;
+name|ratio
 operator|=
 name|dest_l
 operator|/
@@ -7550,79 +7576,30 @@ name|offset
 operator|=
 literal|0.0f
 expr_stmt|;
-block|}
-else|else
-block|{
-name|factor
-operator|=
-literal|1.0f
-operator|-
-name|dest_l
-operator|/
-name|src_l
-expr_stmt|;
-name|offset
-operator|=
-literal|2.0f
-operator|*
-name|dest_l
-operator|-
-literal|1.0f
-expr_stmt|;
-block|}
-block|}
-else|else
-block|{
 if|if
 condition|(
-name|dest_l
-operator|<
-literal|0.5f
+name|dest_high
 condition|)
-block|{
-name|factor
-operator|=
-name|dest_l
-operator|/
-operator|(
+name|offset
+operator|+=
 literal|1.0f
 operator|-
-name|src_l
-operator|)
+literal|2.0f
+operator|*
+name|dest_l
 expr_stmt|;
+if|if
+condition|(
+name|src_high
+condition|)
 name|offset
-operator|=
+operator|+=
 literal|2.0f
 operator|*
 name|dest_l
 operator|-
-name|factor
+name|ratio
 expr_stmt|;
-block|}
-else|else
-block|{
-name|factor
-operator|=
-operator|(
-literal|1.0f
-operator|-
-name|dest_l
-operator|)
-operator|/
-operator|(
-literal|1.0f
-operator|-
-name|src_l
-operator|)
-expr_stmt|;
-name|offset
-operator|=
-literal|1.0f
-operator|-
-name|factor
-expr_stmt|;
-block|}
-block|}
 for|for
 control|(
 name|c
@@ -7646,7 +7623,7 @@ index|[
 name|c
 index|]
 operator|*
-name|factor
+name|ratio
 operator|+
 name|offset
 expr_stmt|;
@@ -7766,9 +7743,6 @@ name|dest_delta
 decl_stmt|,
 name|dest_s
 decl_stmt|;
-name|gint
-name|c
-decl_stmt|;
 name|src_min
 operator|=
 name|MIN
@@ -7841,6 +7815,9 @@ name|ratio
 decl_stmt|;
 name|gfloat
 name|offset
+decl_stmt|;
+name|gint
+name|c
 decl_stmt|;
 name|dest_min
 operator|=
@@ -7958,6 +7935,9 @@ expr_stmt|;
 block|}
 else|else
 block|{
+name|gint
+name|c
+decl_stmt|;
 for|for
 control|(
 name|c
@@ -8143,6 +8123,9 @@ block|{
 name|gfloat
 name|ratio
 decl_stmt|;
+name|gfloat
+name|offset
+decl_stmt|;
 name|gint
 name|c
 decl_stmt|;
@@ -8224,6 +8207,16 @@ name|dest_max
 operator|/
 name|dest_delta
 expr_stmt|;
+name|offset
+operator|=
+operator|(
+literal|1.0f
+operator|-
+name|ratio
+operator|)
+operator|*
+name|dest_max
+expr_stmt|;
 for|for
 control|(
 name|c
@@ -8242,18 +8235,14 @@ index|[
 name|c
 index|]
 operator|=
-operator|(
 name|dest
 index|[
 name|c
 index|]
-operator|-
-name|dest_max
-operator|)
 operator|*
 name|ratio
 operator|+
-name|dest_max
+name|offset
 expr_stmt|;
 block|}
 else|else