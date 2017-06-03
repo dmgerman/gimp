cc8f20e65760eebba7585ced79450eb3a76e6ab2
Michael Natterer <mitch@gimp.org>
2017-06-03 18:12:50 +0200

    pdb: remove a  misplaced " from fileops.pdb
    Former-commit-id: c0c1225e9354fb157a0f7e497c97ee1e8e2a3b0b

--- app/pdb/fileops-cmds.c
+++ app/pdb/fileops-cmds.c
@@ -2675,7 +2675,7 @@ literal|"gimp-file-load"
 argument_list|,
 literal|"Loads an image file by invoking the right load handler."
 argument_list|,
-literal|"This procedure invokes the correct file load handler using magic if possible, and falling back on the file's extension and/or prefix if not. The name of the file to load is typically a full pathname, and the name entered is what the user actually typed before prepending a directory path. The reason for this is that if the user types http://www.xcf/~gimp/ he wants to fetch a URL, and the full pathname will not look like a URL.\""
+literal|"This procedure invokes the correct file load handler using magic if possible, and falling back on the file's extension and/or prefix if not. The name of the file to load is typically a full pathname, and the name entered is what the user actually typed before prepending a directory path. The reason for this is that if the user types http://www.xcf/~gimp/ he wants to fetch a URL, and the full pathname will not look like a URL."
 argument_list|,
 literal|"Josh MacDonald"
 argument_list|,
