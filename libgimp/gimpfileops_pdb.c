58c480d8e8af622f281ec0d9f147316f3fbb574e
Michael Natterer <mitch@gimp.org>
2017-06-03 18:15:23 +0200

    libgimp: forgot to push the generated libgimpfile two commits earlier
    Former-commit-id: e67e22cf0c8d01dcb1cac4043c41dc95a8e79c92

--- libgimp/gimpfileops_pdb.c
+++ libgimp/gimpfileops_pdb.c
@@ -30,7 +30,7 @@ comment|/**  * SECTION: gimpfileops  * @title: gimpfileops  * @short_description
 end_comment
 
 begin_comment
-comment|/**  * gimp_file_load:  * @run_mode: The run mode.  * @filename: The name of the file to load.  * @raw_filename: The name as entered by the user.  *  * Loads an image file by invoking the right load handler.  *  * This procedure invokes the correct file load handler using magic if  * possible, and falling back on the file's extension and/or prefix if  * not. The name of the file to load is typically a full pathname, and  * the name entered is what the user actually typed before prepending a  * directory path. The reason for this is that if the user types  * http://www.xcf/~gimp/ he wants to fetch a URL, and the full pathname  * will not look like a URL.\"  *  * Returns: The output image.  **/
+comment|/**  * gimp_file_load:  * @run_mode: The run mode.  * @filename: The name of the file to load.  * @raw_filename: The name as entered by the user.  *  * Loads an image file by invoking the right load handler.  *  * This procedure invokes the correct file load handler using magic if  * possible, and falling back on the file's extension and/or prefix if  * not. The name of the file to load is typically a full pathname, and  * the name entered is what the user actually typed before prepending a  * directory path. The reason for this is that if the user types  * http://www.xcf/~gimp/ he wants to fetch a URL, and the full pathname  * will not look like a URL.  *  * Returns: The output image.  **/
 end_comment
 
 begin_function
