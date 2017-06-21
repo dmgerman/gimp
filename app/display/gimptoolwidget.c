fb9d0a78fe163441fa60d2c6d3d0c58059ffeb25
Michael Natterer <mitch@gimp.org>
2017-06-21 02:16:11 +0200

    app: forgot to #include "gimptoolpath.h" in gimptoolwidget.c
    Former-commit-id: 1aca558867c0e1099eae2979225f3594d60c13cd

--- app/display/gimptoolwidget.c
+++ app/display/gimptoolwidget.c
@@ -54,6 +54,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|"gimpcanvaspath.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|"gimpcanvastransformguides.h"
 end_include
 
@@ -71,7 +77,7 @@ end_include
 
 begin_enum
 enum|enum
-DECL|enum|__anon286deff10103
+DECL|enum|__anon28d0e96e0103
 block|{
 DECL|enumerator|PROP_0
 name|PROP_0
@@ -87,7 +93,7 @@ end_enum
 
 begin_enum
 enum|enum
-DECL|enum|__anon286deff10203
+DECL|enum|__anon28d0e96e0203
 block|{
 DECL|enumerator|CHANGED
 name|CHANGED
