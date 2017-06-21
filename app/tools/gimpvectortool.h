598ae04d4a8672f47fd8e37deb70ea7711d3d62a
Michael Natterer <mitch@gimp.org>
2017-06-21 23:28:07 +0200

    app: bye bye to 1534 lines of code in GimpVectorTool, use a GimpToolPath
    Former-commit-id: 8b36164a2a5d570a6dad5f06642302068974fac2

--- app/tools/gimpvectortool.h
+++ app/tools/gimpvectortool.h
@@ -22,68 +22,6 @@ directive|include
 file|"gimpdrawtool.h"
 end_include
 
-begin_comment
-comment|/*  possible vector functions  */
-end_comment
-
-begin_typedef
-typedef|typedef
-enum|enum
-DECL|enum|__anon2922b9120103
-block|{
-DECL|enumerator|VECTORS_SELECT_VECTOR
-name|VECTORS_SELECT_VECTOR
-block|,
-DECL|enumerator|VECTORS_CREATE_VECTOR
-name|VECTORS_CREATE_VECTOR
-block|,
-DECL|enumerator|VECTORS_CREATE_STROKE
-name|VECTORS_CREATE_STROKE
-block|,
-DECL|enumerator|VECTORS_ADD_ANCHOR
-name|VECTORS_ADD_ANCHOR
-block|,
-DECL|enumerator|VECTORS_MOVE_ANCHOR
-name|VECTORS_MOVE_ANCHOR
-block|,
-DECL|enumerator|VECTORS_MOVE_ANCHORSET
-name|VECTORS_MOVE_ANCHORSET
-block|,
-DECL|enumerator|VECTORS_MOVE_HANDLE
-name|VECTORS_MOVE_HANDLE
-block|,
-DECL|enumerator|VECTORS_MOVE_CURVE
-name|VECTORS_MOVE_CURVE
-block|,
-DECL|enumerator|VECTORS_MOVE_STROKE
-name|VECTORS_MOVE_STROKE
-block|,
-DECL|enumerator|VECTORS_MOVE_VECTORS
-name|VECTORS_MOVE_VECTORS
-block|,
-DECL|enumerator|VECTORS_INSERT_ANCHOR
-name|VECTORS_INSERT_ANCHOR
-block|,
-DECL|enumerator|VECTORS_DELETE_ANCHOR
-name|VECTORS_DELETE_ANCHOR
-block|,
-DECL|enumerator|VECTORS_CONNECT_STROKES
-name|VECTORS_CONNECT_STROKES
-block|,
-DECL|enumerator|VECTORS_DELETE_SEGMENT
-name|VECTORS_DELETE_SEGMENT
-block|,
-DECL|enumerator|VECTORS_CONVERT_EDGE
-name|VECTORS_CONVERT_EDGE
-block|,
-DECL|enumerator|VECTORS_FINISHED
-name|VECTORS_FINISHED
-DECL|typedef|GimpVectorFunction
-block|}
-name|GimpVectorFunction
-typedef|;
-end_typedef
-
 begin_define
 DECL|macro|GIMP_TYPE_VECTOR_TOOL
 define|#
@@ -185,106 +123,27 @@ DECL|member|parent_instance
 name|GimpDrawTool
 name|parent_instance
 decl_stmt|;
-DECL|member|function
-name|GimpVectorFunction
-name|function
-decl_stmt|;
-comment|/* function we're performing         */
-DECL|member|restriction
-name|GimpAnchorFeatureType
-name|restriction
-decl_stmt|;
-comment|/* movement restriction              */
-DECL|member|modifier_lock
-name|gboolean
-name|modifier_lock
-decl_stmt|;
-comment|/* can we toggle the Shift key?      */
-DECL|member|saved_state
-name|GdkModifierType
-name|saved_state
-decl_stmt|;
-comment|/* modifier state at button_press    */
-DECL|member|last_x
-name|gdouble
-name|last_x
-decl_stmt|;
-comment|/* last x coordinate                 */
-DECL|member|last_y
-name|gdouble
-name|last_y
-decl_stmt|;
-comment|/* last y coordinate                 */
-DECL|member|undo_motion
-name|gboolean
-name|undo_motion
-decl_stmt|;
-comment|/* we need a motion to have an undo  */
-DECL|member|have_undo
-name|gboolean
-name|have_undo
-decl_stmt|;
-comment|/* did we push an undo at            */
-comment|/* ..._button_press?                 */
-DECL|member|cur_anchor
-name|GimpAnchor
-modifier|*
-name|cur_anchor
-decl_stmt|;
-comment|/* the current Anchor                */
-DECL|member|cur_anchor2
-name|GimpAnchor
-modifier|*
-name|cur_anchor2
-decl_stmt|;
-comment|/* secondary Anchor (end on_curve)   */
-DECL|member|cur_stroke
-name|GimpStroke
-modifier|*
-name|cur_stroke
-decl_stmt|;
-comment|/* the current Stroke                */
-DECL|member|cur_position
-name|gdouble
-name|cur_position
-decl_stmt|;
-comment|/* the current Position on a segment */
-DECL|member|cur_vectors
-name|GimpVectors
-modifier|*
-name|cur_vectors
-decl_stmt|;
-comment|/* the vectors the tool is hovering  */
-comment|/* over (if different from ->vectors */
 DECL|member|vectors
 name|GimpVectors
 modifier|*
 name|vectors
 decl_stmt|;
 comment|/* the current Vector data           */
-DECL|member|sel_count
-name|gint
-name|sel_count
-decl_stmt|;
-comment|/* number of selected anchors        */
-DECL|member|sel_anchor
-name|GimpAnchor
-modifier|*
-name|sel_anchor
-decl_stmt|;
-comment|/* currently selected anchor, NULL   */
-comment|/* if multiple anchors are selected  */
-DECL|member|sel_stroke
-name|GimpStroke
-modifier|*
-name|sel_stroke
-decl_stmt|;
-comment|/* selected stroke                   */
 DECL|member|saved_mode
 name|GimpVectorMode
 name|saved_mode
 decl_stmt|;
 comment|/* used by modifier_key()            */
+DECL|member|path
+name|GimpToolWidget
+modifier|*
+name|path
+decl_stmt|;
+DECL|member|grab_widget
+name|GimpToolWidget
+modifier|*
+name|grab_widget
+decl_stmt|;
 block|}
 struct|;
 end_struct
