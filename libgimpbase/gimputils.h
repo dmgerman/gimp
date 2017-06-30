a9cab910db153ee56a09dc8a9ac40130497e3be3
Massimo Valentini <mvalentini@src.gnome.org>
2017-06-30 14:52:44 +0200

    Bug 784305 - Deprecated enum constants are not registered in python-fu.
    Former-commit-id: 7c32451dfbf1b22d2cec191062762e362b8ab261

--- libgimpbase/gimputils.h
+++ libgimpbase/gimputils.h
@@ -364,6 +364,15 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|void
+name|gimp_base_compat_enum_init
+parameter_list|(
+name|void
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_macro
 name|G_END_DECLS
 end_macro
