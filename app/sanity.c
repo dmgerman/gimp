99f01657b9780f694fc8ed05ae734472116d47d8
Michael Natterer <mitch@gimp.org>
2017-08-28 11:34:48 +0200

    configure.ac: require Babl >= 0.1.31 and GEGL >= 0.3.21
    Former-commit-id: 2a9bacb528d373e111a265ed4a4723ebf27b32f4

--- app/sanity.c
+++ app/sanity.c
@@ -1535,7 +1535,7 @@ DECL|macro|BABL_REQUIRED_MICRO
 define|#
 directive|define
 name|BABL_REQUIRED_MICRO
-value|30
+value|31
 name|babl_get_version
 argument_list|(
 operator|&
@@ -1645,7 +1645,7 @@ DECL|macro|GEGL_REQUIRED_MICRO
 define|#
 directive|define
 name|GEGL_REQUIRED_MICRO
-value|20
+value|21
 name|gegl_get_version
 argument_list|(
 operator|&
