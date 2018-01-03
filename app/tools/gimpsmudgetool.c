f94a63de8e5d9ec7959aeaebb984fbbdea9a2a16
Michael Natterer <mitch@gimp.org>
2018-01-03 02:29:33 +0100

    Bug 785001 - Enhanced smudge tool: Smudge with painting
    Enable color picking with Ctrl-Click in the smudge tool because it now
    uses the foreground color.
    
    
    Former-commit-id: 119f648a76fa97b05606a2e3c6b76e9d09e8cb63

--- app/tools/gimpsmudgetool.c
+++ app/tools/gimpsmudgetool.c
@@ -208,6 +208,16 @@ argument_list|,
 name|GIMP_TOOL_CURSOR_SMUDGE
 argument_list|)
 expr_stmt|;
+name|gimp_paint_tool_enable_color_picker
+argument_list|(
+name|GIMP_PAINT_TOOL
+argument_list|(
+name|smudge
+argument_list|)
+argument_list|,
+name|GIMP_COLOR_PICK_MODE_FOREGROUND
+argument_list|)
+expr_stmt|;
 name|paint_tool
 operator|->
 name|status
