1fc61c2f71e2b68230764a71bc018b9ae9153718
Michael Natterer <mitch@gimp.org>
2018-01-29 10:50:24 +0100

    app: adapt include guards of gimp-version.h to new filename
    Former-commit-id: 31d55bc6057d9a6b85d3566fc83541c75cfdcb23

--- app/gimp-version.h
+++ app/gimp-version.h
@@ -6,14 +6,14 @@ end_comment
 begin_ifndef
 ifndef|#
 directive|ifndef
-name|__VERSION_H__
+name|__APP_GIMP_VERSION_H__
 end_ifndef
 
 begin_define
-DECL|macro|__VERSION_H__
+DECL|macro|__APP_GIMP_VERSION_H__
 define|#
 directive|define
-name|__VERSION_H__
+name|__APP_GIMP_VERSION_H__
 end_define
 
 begin_function_decl
@@ -46,7 +46,7 @@ directive|endif
 end_endif
 
 begin_comment
-comment|/* __VERSION_H__ */
+comment|/* __APP_GIMP_VERSION_H__ */
 end_comment
 
 end_unit
