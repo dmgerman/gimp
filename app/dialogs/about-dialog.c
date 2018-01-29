17f15044416fc65ce0ec1e3cd521121ef475ddc5
Jehan <jehan@girinstud.io>
2018-01-29 01:08:54 +0100

    app: remove duplicate include.
    Former-commit-id: 986b3fd70a173d980f8877024c3564deb8cf445a

--- app/dialogs/about-dialog.c
+++ app/dialogs/about-dialog.c
@@ -96,12 +96,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|"git-version.h"
-end_include
-
-begin_include
-include|#
-directive|include
 file|"gimp-intl.h"
 end_include
 
@@ -124,7 +118,7 @@ end_define
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon28d721790108
+DECL|struct|__anon2bb090430108
 block|{
 DECL|member|dialog
 name|GtkWidget
