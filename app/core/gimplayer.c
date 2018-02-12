dmgerman/gimp

commit 81f44661df8cea73ae50214e2ca955233e61385e

app: keep ancestor set in gimp_layer_start_move(), for use in end_move()
In gimp_layer_start_move(), keep the set of ancestors for which for
which we suspended mask cropping, so that we can resume mask
cropping for the same groups in gimp_layer_end_move().  This is
necessary, since gimp_image_remove_layer() calls gimp_item
start_move() before removing the layer from the layer tree, and
gimp_item_end_move() after removing the layer from the layer tree,
at which point the layer has no ancestors.


Former-commit-id: 10c125c6273270ba731b455a533abe226af025b9

app/core/gimplayer.c
@@ -203,7 +203,7 @@ end_include
 
 begin_enum
 enum|enum
-DECL|enum|__anon2c2f8b930103
+DECL|enum|__anon2ae2f6840103
 block|{
 DECL|enumerator|OPACITY_CHANGED
 name|OPACITY_CHANGED
@@ -249,7 +249,7 @@ end_enum
 
 begin_enum
 enum|enum
-DECL|enum|__anon2c2f8b930203
+DECL|enum|__anon2ae2f6840203
 block|{
 DECL|enumerator|PROP_0
 name|PROP_0
@@ -4624,26 +4624,66 @@ argument_list|(
 name|item
 argument_list|)
 decl_stmt|;
+name|GimpLayer
+modifier|*
+name|ancestor
+init|=
+name|layer
+decl_stmt|;
+name|GSList
+modifier|*
+name|ancestors
+init|=
+name|NULL
+decl_stmt|;
 comment|/* suspend mask cropping for all of the layer's ancestors */
 while|while
 condition|(
 operator|(
-name|layer
+name|ancestor
 operator|=
 name|gimp_layer_get_parent
 argument_list|(
-name|layer
+name|ancestor
 argument_list|)
 operator|)
 condition|)
+block|{
 name|gimp_group_layer_suspend_mask
 argument_list|(
 name|GIMP_GROUP_LAYER
 argument_list|(
-name|layer
+name|ancestor
 argument_list|)
 argument_list|,
 name|push_undo
+argument_list|)
+expr_stmt|;
+name|ancestors
+operator|=
+name|g_slist_prepend
+argument_list|(
+name|ancestors
+argument_list|,
+name|g_object_ref
+argument_list|(
+name|ancestor
+argument_list|)
+argument_list|)
+expr_stmt|;
+block|}
+comment|/* we keep the ancestor list around, so that we can resume mask cropping for    * the same set of groups in gimp_layer_end_move().  note that    * gimp_image_remove_layer() calls start_move() before removing the layer,    * while it's still part of the layer tree, and end_move() afterwards, when    * it's no longer part of the layer tree, and hence we can't use get_parent()    * in end_move() to get the same set of ancestors.    */
+name|layer
+operator|->
+name|move_stack
+operator|=
+name|g_slist_prepend
+argument_list|(
+name|layer
+operator|->
+name|move_stack
+argument_list|,
+name|ancestors
 argument_list|)
 expr_stmt|;
 if|if
@@ -4693,6 +4733,14 @@ argument_list|(
 name|item
 argument_list|)
 decl_stmt|;
+name|GSList
+modifier|*
+name|ancestors
+decl_stmt|;
+name|GSList
+modifier|*
+name|iter
+decl_stmt|;
 if|if
 condition|(
 name|GIMP_ITEM_CLASS
@@ -4714,26 +4762,68 @@ argument_list|,
 name|push_undo
 argument_list|)
 expr_stmt|;
-comment|/* resume mask cropping for all of the layer's ancestors */
-while|while
-condition|(
-operator|(
-name|layer
+name|ancestors
 operator|=
-name|gimp_layer_get_parent
+name|layer
+operator|->
+name|move_stack
+operator|->
+name|data
+expr_stmt|;
+name|layer
+operator|->
+name|move_stack
+operator|=
+name|g_slist_remove
 argument_list|(
 name|layer
+operator|->
+name|move_stack
+argument_list|,
+name|ancestors
 argument_list|)
-operator|)
-condition|)
+expr_stmt|;
+comment|/* resume mask cropping for all of the layer's ancestors */
+for|for
+control|(
+name|iter
+operator|=
+name|ancestors
+init|;
+name|iter
+condition|;
+name|iter
+operator|=
+name|g_slist_next
+argument_list|(
+name|iter
+argument_list|)
+control|)
+block|{
+name|GimpGroupLayer
+modifier|*
+name|ancestor
+init|=
+name|iter
+operator|->
+name|data
+decl_stmt|;
 name|gimp_group_layer_resume_mask
 argument_list|(
-name|GIMP_GROUP_LAYER
-argument_list|(
-name|layer
-argument_list|)
+name|ancestor
 argument_list|,
 name|push_undo
+argument_list|)
+expr_stmt|;
+name|g_object_unref
+argument_list|(
+name|ancestor
+argument_list|)
+expr_stmt|;
+block|}
+name|g_slist_free
+argument_list|(
+name|ancestors
 argument_list|)
 expr_stmt|;
 block|}

app/core/gimplayer.h
@@ -179,6 +179,12 @@ name|gboolean
 name|show_mask
 decl_stmt|;
 comment|/*  show mask or layer?               */
+DECL|member|move_stack
+name|GSList
+modifier|*
+name|move_stack
+decl_stmt|;
+comment|/*  ancestors affected by move        */
 DECL|member|layer_offset_node
 name|GeglNode
 modifier|*
@@ -191,7 +197,7 @@ name|mask_offset_node
 decl_stmt|;
 comment|/*  Floating selections  */
 struct|struct
-DECL|struct|__anon2a948f0b0108
+DECL|struct|__anon2acdc3200108
 block|{
 DECL|member|drawable
 name|GimpDrawable

commit 4dcebef0892aa8a04fe03ad2f1fd3d76404c282c

Bug 793373 - Crash when ctrl-alt-clicking, dragging then releasing...
... a selection.
The regression appeared with commit 81f44661df [formerly 10c125c627].
gimp_layer_end_move() may sometimes run even while a symmetric
gimp_layer_start_move() had not run. For instance this happens when
releasing the mouse button after dragging a ctrl-alt-click created
floating layer.
Therefore let's check that layer->move_stack is not NULL before
dereferencing it.


Former-commit-id: d9987ea7f13b22237e745b661713d6b47a477abf

app/core/gimplayer.c
@@ -203,7 +203,7 @@ end_include
 
 begin_enum
 enum|enum
-DECL|enum|__anon2af0ee070103
+DECL|enum|__anon2be6b25e0103
 block|{
 DECL|enumerator|OPACITY_CHANGED
 name|OPACITY_CHANGED
@@ -249,7 +249,7 @@ end_enum
 
 begin_enum
 enum|enum
-DECL|enum|__anon2af0ee070203
+DECL|enum|__anon2be6b25e0203
 block|{
 DECL|enumerator|PROP_0
 name|PROP_0
@@ -4733,14 +4733,6 @@ argument_list|(
 name|item
 argument_list|)
 decl_stmt|;
-name|GSList
-modifier|*
-name|ancestors
-decl_stmt|;
-name|GSList
-modifier|*
-name|iter
-decl_stmt|;
 if|if
 condition|(
 name|GIMP_ITEM_CLASS
@@ -4762,6 +4754,21 @@ argument_list|,
 name|push_undo
 argument_list|)
 expr_stmt|;
+if|if
+condition|(
+name|layer
+operator|->
+name|move_stack
+condition|)
+block|{
+name|GSList
+modifier|*
+name|ancestors
+decl_stmt|;
+name|GSList
+modifier|*
+name|iter
+decl_stmt|;
 name|ancestors
 operator|=
 name|layer
@@ -4826,6 +4833,7 @@ argument_list|(
 name|ancestors
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 end_function
 