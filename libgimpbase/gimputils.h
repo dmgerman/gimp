6c077ed00be9f50d0e6c11d072357bb9687e3702
Jehan <jehan@girinstud.io>
2018-02-22 05:05:24 +0100

    Bug 793514 - Adding version check for gdb.
    It seems that older GDB (under version 7) are not handling very well
    some common debug information format, in particular DWARF > 3. Such
    version of GDB is usually not a problem since it is quite old (more than
    10 years old, it would seem) so you don't see it anymore on any modern
    GNU/Linux distribution. On FreeBSD on the other hand, it is still
    available (probably for license reasons) and even installed by default!
    As a consequence, it makes debugging fail, even though LLDB is also
    installed by default.
    
    That is even more of a problem because it would seem that GIMP is killed
    (most likely by FreeBSD kernel according to the reporter tests) as a
    side-effect of GDB failing, which is seriously bad, in particular since
    we also use the debug dialog for non-fatal errors (which could therefore
    end up killing GIMP as side effect of a bad GDB!).
    
    So I add some GDB version check. I implement this without any dynamic
    memory management, as usual, since this needs to happen also during
    crash handling where the state is unstable and prone to memory
    allocation failure.
    I also add gimp_utils_backtrace_available() public API which can be used
    by the Preferences.
    
    
    Former-commit-id: 6975188d6f8fcb1e84e4e7a2632bc3ebf620ea55

--- libgimpbase/gimputils.h
+++ libgimpbase/gimputils.h
@@ -430,6 +430,16 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
+begin_function_decl
+name|gboolean
+name|gimp_utils_backtrace_available
+parameter_list|(
+name|gboolean
+name|optimal
+parameter_list|)
+function_decl|;
+end_function_decl
+
 begin_macro
 name|G_END_DECLS
 end_macro
