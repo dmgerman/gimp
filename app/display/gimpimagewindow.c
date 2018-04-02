e3a8d00b4909b295068704eff0d1f6a80fde8c17
Michael Natterer <mitch@gimp.org>
2018-04-02 22:21:41 +0200

    Bug 794356 - Rulers always shown for subsequent image views
    gimp_image_window_constructed(): connect_after to the notebook's
    "switch-page" signal so gimp_display_shell_appearance_update() is
    called after gimp_display_shell_canvas_realize(). Just another hack
    to fix the hack...
    
    
    Former-commit-id: ea48b9f31ca3460611530b6a8297386d9acdaf92

--- app/display/gimpimagewindow.c
+++ app/display/gimpimagewindow.c
@@ -383,7 +383,7 @@ end_define
 
 begin_enum
 enum|enum
-DECL|enum|__anon27e79cf20103
+DECL|enum|__anon2b8bb13d0103
 block|{
 DECL|enumerator|PROP_0
 name|PROP_0
@@ -516,7 +516,7 @@ end_struct
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon27e79cf20208
+DECL|struct|__anon2b8bb13d0208
 block|{
 DECL|member|canvas_x
 name|gint
@@ -2218,7 +2218,8 @@ argument_list|,
 name|TRUE
 argument_list|)
 expr_stmt|;
-name|g_signal_connect
+comment|/*  HACK: remove with GTK+ 3.x: there is no reason to    *  connect_after() here except the HACK at the end of    *  gimp_display_shell_canvas_realize().    *    *  We need to make sure gimp_display_shell_appearance_update() is    *  called after realize() so the ill-shown rulers are properly    *  hidden again    */
+name|g_signal_connect_after
 argument_list|(
 name|private
 operator|->
