955dac7def48d4544e8c631d71723376e3e6a4b3
Ell <ell_se@yahoo.com>
2018-04-05 13:41:36 -0400

    app: add include guards to gimppatincore-loops.h
    Former-commit-id: f7ea9abc732f925fdf1604d275d659c790ce304b

--- app/paint/gimppaintcore-loops.h
+++ app/paint/gimppaintcore-loops.h
@@ -3,6 +3,19 @@ begin_comment
 comment|/* GIMP - The GNU Image Manipulation Program  * Copyright (C) 2013 Daniel Sabo  *  * This program is free software: you can redistribute it and/or modify  * it under the terms of the GNU General Public License as published by  * the Free Software Foundation; either version 3 of the License, or  * (at your option) any later version.  *  * This program is distributed in the hope that it will be useful,  * but WITHOUT ANY WARRANTY; without even the implied warranty of  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the  * GNU General Public License for more details.  *  * You should have received a copy of the GNU General Public License  * along with this program.  If not, see<http://www.gnu.org/licenses/>.  */
 end_comment
 
+begin_ifndef
+ifndef|#
+directive|ifndef
+name|__GIMP_PAINT_CORE_LOOPS_H__
+end_ifndef
+
+begin_define
+DECL|macro|__GIMP_PAINT_CORE_LOOPS_H__
+define|#
+directive|define
+name|__GIMP_PAINT_CORE_LOOPS_H__
+end_define
+
 begin_function_decl
 name|void
 name|combine_paint_mask_to_canvas_mask
@@ -152,6 +165,15 @@ name|linear_mode
 parameter_list|)
 function_decl|;
 end_function_decl
+
+begin_endif
+endif|#
+directive|endif
+end_endif
+
+begin_comment
+comment|/* __GIMP_PAINT_CORE_LOOPS_H__ */
+end_comment
 
 end_unit
 
