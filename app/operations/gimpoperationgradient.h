4d2c2e5eb23d3a566cf3494222c123e34459c1f0
Ell <ell_se@yahoo.com>
2018-04-25 13:05:29 -0400

    app: in GimpOperationGradient, move cache generation back to process()
    Undo the part of commit f529b37caea268d26acce46a0138a1e532f27700 [formerly fa9a4108c3d4dc52ab73a2af49762832a4ccabc5]
    that moved cache generation from process() to prepare().  prepare()
    is called after each property change, in order to calculate the
    op's bounding box for invalidation.  Since we only need the cache
    for actual processing, generating it in process() avoids that
    overhead.
    
    
    Former-commit-id: 38ba45cf31dde26b31809084b15f2e975dc142f7

--- app/operations/gimpoperationgradient.h
+++ app/operations/gimpoperationgradient.h
@@ -180,6 +180,10 @@ DECL|member|gradient_cache_size
 name|gint
 name|gradient_cache_size
 decl_stmt|;
+DECL|member|gradient_cache_mutex
+name|GMutex
+name|gradient_cache_mutex
+decl_stmt|;
 block|}
 struct|;
 end_struct
