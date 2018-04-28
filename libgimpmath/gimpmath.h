c670887c10f5dc5ff68b6d9ef74e28205b48a641
Michael Natterer <mitch@gimp.org>
2018-04-28 21:22:00 +0200

    libgimpmath: remove all deprecated cruft
    Former-commit-id: 74d78e7009eb04de115a9f617a6242818b2abffd

--- libgimpmath/gimpmath.h
+++ libgimpmath/gimpmath.h
@@ -78,12 +78,6 @@ end_include
 begin_include
 include|#
 directive|include
-file|<libgimpmath/gimpmd5.h>
-end_include
-
-begin_include
-include|#
-directive|include
 file|<libgimpmath/gimpvector.h>
 end_include
 
