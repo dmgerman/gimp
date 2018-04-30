06ed28e3cddd89491ac553d9c522999cf0615c84
Tobias Ellinghaus <me@houz.org>
2018-04-30 12:18:58 +0200

    Fix compile
    Former-commit-id: c1e73b1cd4105bf65723ca97019bd50a0a99a936

--- app/core/gimp-utils.c
+++ app/core/gimp-utils.c
@@ -3132,8 +3132,6 @@ argument_list|,
 literal|1.0
 argument_list|,
 name|NULL
-argument_list|,
-literal|0
 argument_list|)
 expr_stmt|;
 comment|/* unref the image unconditionally, even when no display was created */
