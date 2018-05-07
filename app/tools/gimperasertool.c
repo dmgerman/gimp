9a7a985621ce240402488cf6dc2e9f3a40a94ae4
Ell <ell_se@yahoo.com>
2018-05-07 17:12:10 -0400

    app: #include "core/gimpdrawable.h" in gimperasertool.c
    ... for gimp_drawable_has_alpha().
    
    
    Former-commit-id: b7754375617f1234f31eb12809115ec07bc2f427

--- app/tools/gimperasertool.c
+++ app/tools/gimperasertool.c
@@ -36,6 +36,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|"core/gimpdrawable.h"
+end_include
+
+begin_include
+include|#
+directive|include
 file|"paint/gimperaseroptions.h"
 end_include
 
