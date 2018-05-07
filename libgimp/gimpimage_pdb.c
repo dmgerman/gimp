ed1c3ca4aad8a3dd62aa8d3a947ea06989cc725e
Michael Natterer <mitch@gimp.org>
2018-05-07 10:16:52 +0200

    pdb: add "Since: 2.10" to the newly added freeze/thaw procedures
    Former-commit-id: b15f38d21fb51fd06c510dc649007fba4a343e90

--- libgimp/gimpimage_pdb.c
+++ libgimp/gimpimage_pdb.c
@@ -2162,7 +2162,7 @@ block|}
 end_function
 
 begin_comment
-comment|/**  * gimp_image_freeze_layers:  * @image_ID: The image.  *  * Freeze the image's layer list.  *  * This procedure freezes the layer list of the image, supressing any  * updates to the Layers dialog in response to changes to the image's  * layers. This can significantly improve performance while applying  * changes affecting the layer list.  *  * Each call to gimp_image_freeze_layers() should be matched by a  * corresponding call to gimp_image_thaw_layers(), undoing its effects.  *  * Returns: TRUE on success.  **/
+comment|/**  * gimp_image_freeze_layers:  * @image_ID: The image.  *  * Freeze the image's layer list.  *  * This procedure freezes the layer list of the image, supressing any  * updates to the Layers dialog in response to changes to the image's  * layers. This can significantly improve performance while applying  * changes affecting the layer list.  *  * Each call to gimp_image_freeze_layers() should be matched by a  * corresponding call to gimp_image_thaw_layers(), undoing its effects.  *  * Returns: TRUE on success.  *  * Since: 2.10  **/
 end_comment
 
 begin_function
@@ -2229,7 +2229,7 @@ block|}
 end_function
 
 begin_comment
-comment|/**  * gimp_image_thaw_layers:  * @image_ID: The image.  *  * Thaw the image's layer list.  *  * This procedure thaws the layer list of the image, re-enabling  * updates to the Layers dialog.  *  * This procedure should match a corresponding call to  * gimp_image_freeze_layers().  *  * Returns: TRUE on success.  **/
+comment|/**  * gimp_image_thaw_layers:  * @image_ID: The image.  *  * Thaw the image's layer list.  *  * This procedure thaws the layer list of the image, re-enabling  * updates to the Layers dialog.  *  * This procedure should match a corresponding call to  * gimp_image_freeze_layers().  *  * Returns: TRUE on success.  *  * Since: 2.10  **/
 end_comment
 
 begin_function
@@ -2539,7 +2539,7 @@ block|}
 end_function
 
 begin_comment
-comment|/**  * gimp_image_freeze_channels:  * @image_ID: The image.  *  * Freeze the image's channel list.  *  * This procedure freezes the channel list of the image, supressing any  * updates to the Channels dialog in response to changes to the image's  * channels. This can significantly improve performance while applying  * changes affecting the channel list.  *  * Each call to gimp_image_freeze_channels() should be matched by a  * corresponding call to gimp_image_thaw_channels(), undoing its  * effects.  *  * Returns: TRUE on success.  **/
+comment|/**  * gimp_image_freeze_channels:  * @image_ID: The image.  *  * Freeze the image's channel list.  *  * This procedure freezes the channel list of the image, supressing any  * updates to the Channels dialog in response to changes to the image's  * channels. This can significantly improve performance while applying  * changes affecting the channel list.  *  * Each call to gimp_image_freeze_channels() should be matched by a  * corresponding call to gimp_image_thaw_channels(), undoing its  * effects.  *  * Returns: TRUE on success.  *  * Since: 2.10  **/
 end_comment
 
 begin_function
@@ -2606,7 +2606,7 @@ block|}
 end_function
 
 begin_comment
-comment|/**  * gimp_image_thaw_channels:  * @image_ID: The image.  *  * Thaw the image's channel list.  *  * This procedure thaws the channel list of the image, re-enabling  * updates to the Channels dialog.  *  * This procedure should match a corresponing call to  * gimp_image_freeze_channels().  *  * Returns: TRUE on success.  **/
+comment|/**  * gimp_image_thaw_channels:  * @image_ID: The image.  *  * Thaw the image's channel list.  *  * This procedure thaws the channel list of the image, re-enabling  * updates to the Channels dialog.  *  * This procedure should match a corresponing call to  * gimp_image_freeze_channels().  *  * Returns: TRUE on success.  *  * Since: 2.10  **/
 end_comment
 
 begin_function
@@ -2916,7 +2916,7 @@ block|}
 end_function
 
 begin_comment
-comment|/**  * gimp_image_freeze_vectors:  * @image_ID: The image.  *  * Freeze the image's vectors list.  *  * This procedure freezes the vectors list of the image, supressing any  * updates to the Paths dialog in response to changes to the image's  * vectors. This can significantly improve performance while applying  * changes affecting the vectors list.  *  * Each call to gimp_image_freeze_vectors() should be matched by a  * corresponding call to gimp_image_thaw_vectors(), undoing its  * effects.  *  * Returns: TRUE on success.  **/
+comment|/**  * gimp_image_freeze_vectors:  * @image_ID: The image.  *  * Freeze the image's vectors list.  *  * This procedure freezes the vectors list of the image, supressing any  * updates to the Paths dialog in response to changes to the image's  * vectors. This can significantly improve performance while applying  * changes affecting the vectors list.  *  * Each call to gimp_image_freeze_vectors() should be matched by a  * corresponding call to gimp_image_thaw_vectors(), undoing its  * effects.  *  * Returns: TRUE on success.  *  * Since: 2.10  **/
 end_comment
 
 begin_function
@@ -2983,7 +2983,7 @@ block|}
 end_function
 
 begin_comment
-comment|/**  * gimp_image_thaw_vectors:  * @image_ID: The image.  *  * Thaw the image's vectors list.  *  * This procedure thaws the vectors list of the image, re-enabling  * updates to the Paths dialog.  *  * This procedure should match a corresponding call to  * gimp_image_freeze_vectors().  *  * Returns: TRUE on success.  **/
+comment|/**  * gimp_image_thaw_vectors:  * @image_ID: The image.  *  * Thaw the image's vectors list.  *  * This procedure thaws the vectors list of the image, re-enabling  * updates to the Paths dialog.  *  * This procedure should match a corresponding call to  * gimp_image_freeze_vectors().  *  * Returns: TRUE on success.  *  * Since: 2.10  **/
 end_comment
 
 begin_function
