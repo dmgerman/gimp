eb96408ede8515742fe0df88e74a662ebd03f56b
Jehan <jehan@girinstud.io>
2018-05-14 03:09:37 +0200

    app: GTK+3 has more GdkAxisUse values.
    This fixes a crash when we were looping until GDK_AXIS_LAST, but our
    existing code was expecting less values.
    
    Also I note that GTK+3 has an additional GDK_AXIS_ROTATION since version
    3.22, even though we were told that some pens with rotation support
    would report rotation values with same valuator as for the wheel (at
    least, the Wacom Art Pen would apparently use the same 6th valuator).
    For proper support, it would be really worth testing such pens.
    
    
    Former-commit-id: e0b3e893d273fc3008d9b3920e81660f426264fb

--- app/widgets/gimpdeviceinfoeditor.c
+++ app/widgets/gimpdeviceinfoeditor.c
@@ -81,7 +81,7 @@ end_define
 
 begin_enum
 enum|enum
-DECL|enum|__anon293699800103
+DECL|enum|__anon28ef08a00103
 block|{
 DECL|enumerator|PROP_0
 name|PROP_0
@@ -94,7 +94,7 @@ end_enum
 
 begin_enum
 enum|enum
-DECL|enum|__anon293699800203
+DECL|enum|__anon28ef08a00203
 block|{
 DECL|enumerator|AXIS_COLUMN_INDEX
 name|AXIS_COLUMN_INDEX
@@ -113,7 +113,7 @@ end_enum
 
 begin_enum
 enum|enum
-DECL|enum|__anon293699800303
+DECL|enum|__anon28ef08a00303
 block|{
 DECL|enumerator|INPUT_COLUMN_INDEX
 name|INPUT_COLUMN_INDEX
@@ -129,7 +129,7 @@ end_enum
 
 begin_enum
 enum|enum
-DECL|enum|__anon293699800403
+DECL|enum|__anon28ef08a00403
 block|{
 DECL|enumerator|KEY_COLUMN_INDEX
 name|KEY_COLUMN_INDEX
@@ -463,10 +463,25 @@ argument_list|(
 literal|"Y tilt"
 argument_list|)
 block|,
-comment|/* Wheel as in mouse or input device wheel.    * Some pens use the same axis for their rotation feature.    * See bug 791455.    */
+comment|/* Wheel as in mouse or input device wheel.    * Some pens would use the same axis for their rotation feature.    * See bug 791455.    * Yet GTK+ has a different axis since v. 3.22.    * TODO: this should be actually tested with a device having such    * feature.    */
 name|N_
 argument_list|(
-literal|"Wheel/Rotation"
+literal|"Wheel"
+argument_list|)
+block|,
+name|N_
+argument_list|(
+literal|"Distance"
+argument_list|)
+block|,
+name|N_
+argument_list|(
+literal|"Rotation"
+argument_list|)
+block|,
+name|N_
+argument_list|(
+literal|"Slider"
 argument_list|)
 block|}
 decl_stmt|;
