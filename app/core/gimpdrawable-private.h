26949ca0de6fce4139af0664455f0ed092840177
Michael Natterer <mitch@gimp.org>
2018-05-16 02:06:03 +0200

    Bug 796090 - (wrong) true-color preview of GEGL filter ops, ...
    ...like gaussian blur in indexed mode
    
    In GimpDrawable's source node, after the filter stack, insert a node
    that converts the pixels back to the drawable's format if the drawable
    is indexed.
    
    
    Former-commit-id: d6e0ca505440596e81593cf9ec0fac4f0d9e2ba8

--- app/core/gimpdrawable-private.h
+++ app/core/gimpdrawable-private.h
@@ -48,6 +48,11 @@ name|GimpContainer
 modifier|*
 name|filter_stack
 decl_stmt|;
+DECL|member|convert_format
+name|GeglNode
+modifier|*
+name|convert_format
+decl_stmt|;
 DECL|member|floating_selection
 name|GimpLayer
 modifier|*
