fb7eb82223d4581ae09e6e64c161c26c3a8dc4b2
Øyvind Kolås <pippin@gimp.org>
2018-07-06 14:50:08 +0200

    app: gimp:flood propagate babl space
    Former-commit-id: ae2c30ad0e759cef3effdcbbc0b6eb4ffade0eb5

--- app/operations/gimpoperationflood.c
+++ app/operations/gimpoperationflood.c
@@ -706,15 +706,29 @@ modifier|*
 name|operation
 parameter_list|)
 block|{
+specifier|const
+name|Babl
+modifier|*
+name|space
+init|=
+name|gegl_operation_get_source_space
+argument_list|(
+name|operation
+argument_list|,
+literal|"input"
+argument_list|)
+decl_stmt|;
 name|gegl_operation_set_format
 argument_list|(
 name|operation
 argument_list|,
 literal|"input"
 argument_list|,
-name|babl_format
+name|babl_format_with_space
 argument_list|(
 literal|"Y float"
+argument_list|,
+name|space
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -724,9 +738,11 @@ name|operation
 argument_list|,
 literal|"output"
 argument_list|,
-name|babl_format
+name|babl_format_with_space
 argument_list|(
 literal|"Y float"
+argument_list|,
+name|space
 argument_list|)
 argument_list|)
 expr_stmt|;
@@ -3205,9 +3221,11 @@ name|Babl
 modifier|*
 name|input_format
 init|=
-name|babl_format
+name|gegl_operation_get_format
 argument_list|(
-literal|"Y float"
+name|operation
+argument_list|,
+literal|"input"
 argument_list|)
 decl_stmt|;
 specifier|const
@@ -3215,9 +3233,11 @@ name|Babl
 modifier|*
 name|output_format
 init|=
-name|babl_format
+name|gegl_operation_get_format
 argument_list|(
-literal|"Y float"
+name|operation
+argument_list|,
+literal|"output"
 argument_list|)
 decl_stmt|;
 name|GeglColor
