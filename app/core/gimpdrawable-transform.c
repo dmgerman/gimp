dmgerman/gimp

commit 5c9c9587969244a2f0b25c36b3cd3d4830229b90

app: add gimp_drawable_transform_get_effective_clip()
Which returns the actual clip mode that will be used by the
drawable for a particular transformation.


Former-commit-id: 2ae823ba2b852c8f6d36a462db6b71662ffe61f0

app/core/gimpdrawable-transform.c
@@ -253,6 +253,126 @@ comment|/*  public functions  */
 end_comment
 
 begin_function
+name|GimpTransformResize
+DECL|function|gimp_drawable_transform_get_effective_clip (GimpDrawable * drawable,GeglBuffer * orig_buffer,GimpTransformResize clip_result)
+name|gimp_drawable_transform_get_effective_clip
+parameter_list|(
+name|GimpDrawable
+modifier|*
+name|drawable
+parameter_list|,
+name|GeglBuffer
+modifier|*
+name|orig_buffer
+parameter_list|,
+name|GimpTransformResize
+name|clip_result
+parameter_list|)
+block|{
+name|g_return_val_if_fail
+argument_list|(
+name|GIMP_IS_DRAWABLE
+argument_list|(
+name|drawable
+argument_list|)
+argument_list|,
+name|clip_result
+argument_list|)
+expr_stmt|;
+name|g_return_val_if_fail
+argument_list|(
+name|gimp_item_is_attached
+argument_list|(
+name|GIMP_ITEM
+argument_list|(
+name|drawable
+argument_list|)
+argument_list|)
+argument_list|,
+name|clip_result
+argument_list|)
+expr_stmt|;
+name|g_return_val_if_fail
+argument_list|(
+name|orig_buffer
+operator|==
+name|NULL
+operator|||
+name|GEGL_IS_BUFFER
+argument_list|(
+name|orig_buffer
+argument_list|)
+argument_list|,
+name|clip_result
+argument_list|)
+expr_stmt|;
+comment|/*  Always clip unfloated buffers since they must keep their size  */
+if|if
+condition|(
+name|GIMP_IS_CHANNEL
+argument_list|(
+name|drawable
+argument_list|)
+condition|)
+block|{
+if|if
+condition|(
+name|orig_buffer
+condition|)
+block|{
+if|if
+condition|(
+operator|!
+name|babl_format_has_alpha
+argument_list|(
+name|gegl_buffer_get_format
+argument_list|(
+name|orig_buffer
+argument_list|)
+argument_list|)
+condition|)
+name|clip_result
+operator|=
+name|GIMP_TRANSFORM_RESIZE_CLIP
+expr_stmt|;
+block|}
+else|else
+block|{
+name|GimpImage
+modifier|*
+name|image
+init|=
+name|gimp_item_get_image
+argument_list|(
+name|GIMP_ITEM
+argument_list|(
+name|drawable
+argument_list|)
+argument_list|)
+decl_stmt|;
+if|if
+condition|(
+name|gimp_channel_is_empty
+argument_list|(
+name|gimp_image_get_mask
+argument_list|(
+name|image
+argument_list|)
+argument_list|)
+condition|)
+name|clip_result
+operator|=
+name|GIMP_TRANSFORM_RESIZE_CLIP
+expr_stmt|;
+block|}
+block|}
+return|return
+name|clip_result
+return|;
+block|}
+end_function
+
+begin_function
 name|GeglBuffer
 modifier|*
 DECL|function|gimp_drawable_transform_buffer_affine (GimpDrawable * drawable,GimpContext * context,GeglBuffer * orig_buffer,gint orig_offset_x,gint orig_offset_y,const GimpMatrix3 * matrix,GimpTransformDirection direction,GimpInterpolationType interpolation_type,GimpTransformResize clip_result,GimpColorProfile ** buffer_profile,gint * new_offset_x,gint * new_offset_y,GimpProgress * progress)
@@ -488,28 +608,16 @@ argument_list|(
 name|orig_buffer
 argument_list|)
 expr_stmt|;
-comment|/*  Always clip unfloated buffers since they must keep their size  */
-if|if
-condition|(
-name|G_TYPE_FROM_INSTANCE
-argument_list|(
-name|drawable
-argument_list|)
-operator|==
-name|GIMP_TYPE_CHANNEL
-operator|&&
-operator|!
-name|babl_format_has_alpha
-argument_list|(
-name|gegl_buffer_get_format
-argument_list|(
-name|orig_buffer
-argument_list|)
-argument_list|)
-condition|)
 name|clip_result
 operator|=
-name|GIMP_TRANSFORM_RESIZE_CLIP
+name|gimp_drawable_transform_get_effective_clip
+argument_list|(
+name|drawable
+argument_list|,
+name|orig_buffer
+argument_list|,
+name|clip_result
+argument_list|)
 expr_stmt|;
 comment|/*  Find the bounding coordinates of target */
 name|gimp_transform_resize_boundary
@@ -3356,26 +3464,16 @@ name|GimpColorProfile
 modifier|*
 name|profile
 decl_stmt|;
-comment|/*  always clip unfloated buffers so they keep their size  */
-if|if
-condition|(
-name|GIMP_IS_CHANNEL
-argument_list|(
-name|drawable
-argument_list|)
-operator|&&
-operator|!
-name|babl_format_has_alpha
-argument_list|(
-name|gegl_buffer_get_format
-argument_list|(
-name|orig_buffer
-argument_list|)
-argument_list|)
-condition|)
 name|clip_result
 operator|=
-name|GIMP_TRANSFORM_RESIZE_CLIP
+name|gimp_drawable_transform_get_effective_clip
+argument_list|(
+name|drawable
+argument_list|,
+name|orig_buffer
+argument_list|,
+name|clip_result
+argument_list|)
 expr_stmt|;
 comment|/*  also transform the mask if we are transforming an entire layer  */
 if|if

app/core/gimpdrawable-transform.h
@@ -17,6 +17,24 @@ name|__GIMP_DRAWABLE_TRANSFORM_H__
 end_define
 
 begin_function_decl
+name|GimpTransformResize
+name|gimp_drawable_transform_get_effective_clip
+parameter_list|(
+name|GimpDrawable
+modifier|*
+name|drawable
+parameter_list|,
+name|GeglBuffer
+modifier|*
+name|orig_buffer
+parameter_list|,
+name|GimpTransformResize
+name|clip_result
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
 name|GeglBuffer
 modifier|*
 name|gimp_drawable_transform_buffer_affine

commit 49b96cb0074901c3a2510bf928134630e2fd120b

Issue #2473 - Transforming a layer doesn't properly transform its mask
In gimp_drawable_transform_buffer_affine(), avoid modifying the
clipping mode when transforming layer masks, since this function is
used (among other things) to transform layer masks together with
their layer, in which case they should use the same clipping mode
as the layer.

This fixes a regression introduced by commit
5c9c9587969244a2f0b25c36b3cd3d4830229b90 [formerly 2ae823ba2b852c8f6d36a462db6b71662ffe61f0], causing layer masks to be
transformed with a mismatched clipping mode during layer
transforms, leading to discrepencies between the transformed layer
and the transformed mask.

This commit merely reverts the necessary part of above commit,
fixing the regression, though note that this code is really up for
some serious refactoring: the logic for determining which clipping
mode to use when is spread all over the place.


Former-commit-id: 45fc30caa7f9ed6e0483dfee221e719650dae0b4

app/core/gimpdrawable-transform.c
@@ -608,6 +608,17 @@ argument_list|(
 name|orig_buffer
 argument_list|)
 expr_stmt|;
+comment|/*  Don't modify the clipping mode of layer masks here, so that,    *  when transformed together with their layer, they match the    *  layer's clipping mode.    */
+if|if
+condition|(
+name|G_TYPE_FROM_INSTANCE
+argument_list|(
+name|drawable
+argument_list|)
+operator|==
+name|GIMP_TYPE_CHANNEL
+condition|)
+block|{
 name|clip_result
 operator|=
 name|gimp_drawable_transform_get_effective_clip
@@ -619,6 +630,7 @@ argument_list|,
 name|clip_result
 argument_list|)
 expr_stmt|;
+block|}
 comment|/*  Find the bounding coordinates of target */
 name|gimp_transform_resize_boundary
 argument_list|(