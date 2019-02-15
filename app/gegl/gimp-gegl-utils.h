775ea545b641165bb7ea4ea87530d52d141d5ae5
Ell <ell_se@yahoo.com>
2019-02-15 12:27:55 -0500

    app: add gimp_gegl_node_is_point_operation()
    ... which takes a GeglNode, and determines if the associated
    operation is a point operation.
    
    Use in GimpFilterTool, instead of performing the same check
    manually.
    
    
    Former-commit-id: 918f2e75bda4e8c93ceb76a78fdbd81a21181759

--- app/gegl/gimp-gegl-utils.h
+++ app/gegl/gimp-gegl-utils.h
@@ -72,6 +72,17 @@ function_decl|;
 end_function_decl
 
 begin_function_decl
+name|gboolean
+name|gimp_gegl_node_is_point_operation
+parameter_list|(
+name|GeglNode
+modifier|*
+name|node
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
 specifier|const
 name|Babl
 modifier|*
