dmgerman/gimp

commit d5f862a0bc8531b46b17324dccf03bdef9a254c7

app: don't dereference NULL image pointer
windows_menu_display_query_tooltip(): bail out if "image" is
NULL. Can't happen currently but did happen temporarily while hacking
on related code. Better safe than sorry.


Former-commit-id: 3bf2a3c16603f434521cf94c1e2fd7d745a18794

app/menus/windows-menu.c
@@ -1730,6 +1730,12 @@ decl_stmt|;
 name|gint
 name|height
 decl_stmt|;
+if|if
+condition|(
+operator|!
+name|image
+condition|)
+return|return;
 name|text
 operator|=
 name|gtk_widget_get_tooltip_text

commit ed1126ccf4947f1a1495e5c6d847d7134a894e99

Issue #835: warnings building with Clang.
Fix one warning and one error (introduced in commit d5f862a0bc8 [formerly 3bf2a3c1660]).


Former-commit-id: 90bc9e10f14644a2bf6c0071c0762d5f34f0957d

app/actions/dockable-commands.c
@@ -680,7 +680,10 @@ argument_list|(
 name|dockbook
 argument_list|)
 argument_list|,
+name|GTK_WIDGET
+argument_list|(
 name|dockable
+argument_list|)
 argument_list|)
 expr_stmt|;
 name|gtk_notebook_set_current_page

app/menus/windows-menu.c
@@ -1735,7 +1735,9 @@ condition|(
 operator|!
 name|image
 condition|)
-return|return;
+return|return
+name|FALSE
+return|;
 name|text
 operator|=
 name|gtk_widget_get_tooltip_text