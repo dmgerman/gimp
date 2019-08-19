047c5e29189503282e09730a64cd0bdbeb1ee203
Michael Natterer <mitch@gimp.org>
2019-08-19 14:02:34 +0200

    libgimp: add GimpThumbnailProceudre
    which handles the standard thmbnail procedure args and return values.
    
    
    Former-commit-id: c4a973c30af1433ead72f28469b6a711f754dff1

--- libgimp/gimp.h
+++ libgimp/gimp.h
@@ -206,6 +206,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<libgimp/gimpthumbnailprocedure.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<libgimp/gimp_pdb_headers.h>
 end_include
 
