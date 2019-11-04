99333b4143924f301e087202ad9b754c944837f3
Ell <ell_se@yahoo.com>
2019-11-04 12:51:09 +0200

    app: in GimpLineArt, ref input pickable
    ... in particular, so that the GimpLineArt object can take
    ownership over newly-created GimpImageProxy inputs.
    
    
    Former-commit-id: c8b5c81a418ab8e4997dc5bbffaa5a671d479e20

--- app/core/gimplineart.c
+++ app/core/gimplineart.c
@@ -141,7 +141,7 @@ end_include
 
 begin_enum
 enum|enum
-DECL|enum|__anon2c71e1dc0103
+DECL|enum|__anon2b27302c0103
 block|{
 DECL|enumerator|COMPUTING_START
 name|COMPUTING_START
@@ -157,7 +157,7 @@ end_enum
 
 begin_enum
 enum|enum
-DECL|enum|__anon2c71e1dc0203
+DECL|enum|__anon2b27302c0203
 block|{
 DECL|enumerator|PROP_0
 name|PROP_0
@@ -259,7 +259,7 @@ end_struct
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2c71e1dc0308
+DECL|struct|__anon2b27302c0308
 block|{
 DECL|member|buffer
 name|GeglBuffer
@@ -291,7 +291,7 @@ end_typedef
 begin_typedef
 typedef|typedef
 struct|struct
-DECL|struct|__anon2c71e1dc0408
+DECL|struct|__anon2b27302c0408
 block|{
 DECL|member|closed
 name|GeglBuffer
@@ -2213,13 +2213,17 @@ argument_list|,
 name|line_art
 argument_list|)
 expr_stmt|;
+name|g_set_object
+argument_list|(
+operator|&
 name|line_art
 operator|->
 name|priv
 operator|->
 name|input
-operator|=
+argument_list|,
 name|pickable
+argument_list|)
 expr_stmt|;
 name|gimp_line_art_compute
 argument_list|(
