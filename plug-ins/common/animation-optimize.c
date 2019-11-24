dmgerman/gimp

commit 1f0adcf34cee79f129aa7e52ce31797a26435f71

plug-ins: port animation-optimize to GEGL, stupid 8-bit port only
Former-commit-id: 9d33a9a9ca82ddc78d881a409a2ccf34823b129e

plug-ins/common/animation-optimize.c
@@ -74,7 +74,7 @@ end_define
 begin_typedef
 typedef|typedef
 enum|enum
-DECL|enum|__anon276826ef0103
+DECL|enum|__anon296771a30103
 block|{
 DECL|enumerator|DISPOSE_UNDEFINED
 name|DISPOSE_UNDEFINED
@@ -99,7 +99,7 @@ end_typedef
 begin_typedef
 typedef|typedef
 enum|enum
-DECL|enum|__anon276826ef0203
+DECL|enum|__anon296771a30203
 block|{
 DECL|enumerator|OPOPTIMIZE
 name|OPOPTIMIZE
@@ -378,15 +378,6 @@ specifier|static
 name|gint32
 modifier|*
 name|layers
-decl_stmt|;
-end_decl_stmt
-
-begin_decl_stmt
-DECL|variable|drawable
-specifier|static
-name|GimpDrawable
-modifier|*
-name|drawable
 decl_stmt|;
 end_decl_stmt
 
@@ -835,6 +826,13 @@ expr_stmt|;
 name|INIT_I18N
 argument_list|()
 expr_stmt|;
+name|gegl_init
+argument_list|(
+name|NULL
+argument_list|,
+name|NULL
+argument_list|)
+expr_stmt|;
 if|if
 condition|(
 name|run_mode
@@ -1061,8 +1059,88 @@ end_function
 
 begin_function
 specifier|static
+specifier|const
+name|Babl
+modifier|*
+DECL|function|get_format (gint32 drawable_ID)
+name|get_format
+parameter_list|(
+name|gint32
+name|drawable_ID
+parameter_list|)
+block|{
+if|if
+condition|(
+name|gimp_drawable_is_rgb
+argument_list|(
+name|drawable_ID
+argument_list|)
+condition|)
+block|{
+if|if
+condition|(
+name|gimp_drawable_has_alpha
+argument_list|(
+name|drawable_ID
+argument_list|)
+condition|)
+return|return
+name|babl_format
+argument_list|(
+literal|"R'G'B'A u8"
+argument_list|)
+return|;
+else|else
+return|return
+name|babl_format
+argument_list|(
+literal|"R'G'B' u8"
+argument_list|)
+return|;
+block|}
+elseif|else
+if|if
+condition|(
+name|gimp_drawable_is_gray
+argument_list|(
+name|drawable_ID
+argument_list|)
+condition|)
+block|{
+if|if
+condition|(
+name|gimp_drawable_has_alpha
+argument_list|(
+name|drawable_ID
+argument_list|)
+condition|)
+return|return
+name|babl_format
+argument_list|(
+literal|"Y'A u8"
+argument_list|)
+return|;
+else|else
+return|return
+name|babl_format
+argument_list|(
+literal|"Y' u8"
+argument_list|)
+return|;
+block|}
+return|return
+name|gimp_drawable_get_format
+argument_list|(
+name|drawable_ID
+argument_list|)
+return|;
+block|}
+end_function
+
+begin_function
+specifier|static
 name|void
-DECL|function|compose_row (gint frame_num,DisposeType dispose,gint row_num,guchar * dest,gint dest_width,GimpDrawable * drawable,gboolean cleanup)
+DECL|function|compose_row (gint frame_num,DisposeType dispose,gint row_num,guchar * dest,gint dest_width,gint32 drawable_ID,gboolean cleanup)
 name|compose_row
 parameter_list|(
 name|gint
@@ -1081,9 +1159,8 @@ parameter_list|,
 name|gint
 name|dest_width
 parameter_list|,
-name|GimpDrawable
-modifier|*
-name|drawable
+name|gint32
+name|drawable_ID
 parameter_list|,
 name|gboolean
 name|cleanup
@@ -1096,12 +1173,18 @@ name|line_buf
 init|=
 name|NULL
 decl_stmt|;
+name|GeglBuffer
+modifier|*
+name|src_buffer
+decl_stmt|;
+specifier|const
+name|Babl
+modifier|*
+name|format
+decl_stmt|;
 name|guchar
 modifier|*
 name|srcptr
-decl_stmt|;
-name|GimpPixelRgn
-name|pixel_rgn
 decl_stmt|;
 name|gint
 name|rawx
@@ -1161,9 +1244,7 @@ expr_stmt|;
 block|}
 name|gimp_drawable_offsets
 argument_list|(
-name|drawable
-operator|->
-name|drawable_id
+name|drawable_ID
 argument_list|,
 operator|&
 name|rawx
@@ -1172,13 +1253,18 @@ operator|&
 name|rawy
 argument_list|)
 expr_stmt|;
+name|rawwidth
+operator|=
+name|gimp_drawable_width
+argument_list|(
+name|drawable_ID
+argument_list|)
+expr_stmt|;
 name|rawheight
 operator|=
 name|gimp_drawable_height
 argument_list|(
-name|drawable
-operator|->
-name|drawable_id
+name|drawable_ID
 argument_list|)
 expr_stmt|;
 comment|/* this frame has nothing to give us for this row; return */
@@ -1195,31 +1281,25 @@ operator|<
 name|rawy
 condition|)
 return|return;
-name|rawbpp
+name|format
 operator|=
-name|gimp_drawable_bpp
+name|get_format
 argument_list|(
-name|drawable
-operator|->
-name|drawable_id
-argument_list|)
-expr_stmt|;
-name|rawwidth
-operator|=
-name|gimp_drawable_width
-argument_list|(
-name|drawable
-operator|->
-name|drawable_id
+name|drawable_ID
 argument_list|)
 expr_stmt|;
 name|has_alpha
 operator|=
 name|gimp_drawable_has_alpha
 argument_list|(
-name|drawable
-operator|->
-name|drawable_id
+name|drawable_ID
+argument_list|)
+expr_stmt|;
+name|rawbpp
+operator|=
+name|babl_format_get_bytes_per_pixel
+argument_list|(
+name|format
 argument_list|)
 expr_stmt|;
 if|if
@@ -1247,13 +1327,19 @@ name|rawbpp
 argument_list|)
 expr_stmt|;
 comment|/* Initialise and fetch the raw new frame row */
-name|gimp_pixel_rgn_init
+name|src_buffer
+operator|=
+name|gimp_drawable_get_buffer
 argument_list|(
-operator|&
-name|pixel_rgn
+name|drawable_ID
+argument_list|)
+expr_stmt|;
+name|gegl_buffer_get
+argument_list|(
+name|src_buffer
 argument_list|,
-name|drawable
-argument_list|,
+name|GEGL_RECTANGLE
+argument_list|(
 literal|0
 argument_list|,
 name|row_num
@@ -1263,28 +1349,22 @@ argument_list|,
 name|rawwidth
 argument_list|,
 literal|1
-argument_list|,
-name|FALSE
-argument_list|,
-name|FALSE
 argument_list|)
-expr_stmt|;
-name|gimp_pixel_rgn_get_rect
-argument_list|(
-operator|&
-name|pixel_rgn
+argument_list|,
+literal|1.0
+argument_list|,
+name|format
 argument_list|,
 name|line_buf
 argument_list|,
-literal|0
+name|GEGL_AUTO_ROWSTRIDE
 argument_list|,
-name|row_num
-operator|-
-name|rawy
-argument_list|,
-name|rawwidth
-argument_list|,
-literal|1
+name|GEGL_ABYSS_NONE
+argument_list|)
+expr_stmt|;
+name|g_object_unref
+argument_list|(
+name|src_buffer
 argument_list|)
 expr_stmt|;
 comment|/* render... */
@@ -1414,9 +1494,6 @@ name|gboolean
 name|diff_only
 parameter_list|)
 block|{
-name|GimpPixelRgn
-name|pixel_rgn
-decl_stmt|;
 specifier|static
 name|guchar
 modifier|*
@@ -1614,7 +1691,6 @@ literal|4
 else|:
 literal|2
 expr_stmt|;
-comment|/*  gimp_tile_cache_ntiles(total_frames * (width / gimp_tile_width() + 1) );*/
 name|drawabletype_alpha
 operator|=
 operator|(
@@ -1973,10 +2049,9 @@ name|this_frame_num
 operator|++
 control|)
 block|{
-name|drawable
-operator|=
-name|gimp_drawable_get
-argument_list|(
+name|gint32
+name|drawable_ID
+init|=
 name|layers
 index|[
 name|total_frames
@@ -1987,8 +2062,7 @@ operator|+
 literal|1
 operator|)
 index|]
-argument_list|)
-expr_stmt|;
+decl_stmt|;
 name|dispose
 operator|=
 name|get_frame_disposal
@@ -2011,14 +2085,9 @@ index|]
 argument_list|,
 name|width
 argument_list|,
-name|drawable
+name|drawable_ID
 argument_list|,
 name|FALSE
-argument_list|)
-expr_stmt|;
-name|gimp_drawable_detach
-argument_list|(
-name|drawable
 argument_list|)
 expr_stmt|;
 block|}
@@ -2624,6 +2693,15 @@ operator|==
 name|OPBACKGROUND
 condition|)
 block|{
+name|GeglBuffer
+modifier|*
+name|buffer
+decl_stmt|;
+specifier|const
+name|Babl
+modifier|*
+name|format
+decl_stmt|;
 name|new_layer_id
 operator|=
 name|gimp_layer_new
@@ -2658,20 +2736,26 @@ argument_list|,
 literal|0
 argument_list|)
 expr_stmt|;
-name|drawable
+name|buffer
 operator|=
-name|gimp_drawable_get
+name|gimp_drawable_get_buffer
 argument_list|(
 name|new_layer_id
 argument_list|)
 expr_stmt|;
-name|gimp_pixel_rgn_init
+name|format
+operator|=
+name|get_format
 argument_list|(
-operator|&
-name|pixel_rgn
+name|new_layer_id
+argument_list|)
+expr_stmt|;
+name|gegl_buffer_set
+argument_list|(
+name|buffer
 argument_list|,
-name|drawable
-argument_list|,
+name|GEGL_RECTANGLE
+argument_list|(
 literal|0
 argument_list|,
 literal|0
@@ -2679,36 +2763,20 @@ argument_list|,
 name|width
 argument_list|,
 name|height
-argument_list|,
-name|TRUE
-argument_list|,
-name|FALSE
 argument_list|)
-expr_stmt|;
-name|gimp_pixel_rgn_set_rect
-argument_list|(
-operator|&
-name|pixel_rgn
+argument_list|,
+literal|0
+argument_list|,
+name|format
 argument_list|,
 name|back_frame
 argument_list|,
-literal|0
-argument_list|,
-literal|0
-argument_list|,
-name|width
-argument_list|,
-name|height
+name|GEGL_ABYSS_NONE
 argument_list|)
 expr_stmt|;
-name|gimp_drawable_flush
+name|g_object_unref
 argument_list|(
-name|drawable
-argument_list|)
-expr_stmt|;
-name|gimp_drawable_detach
-argument_list|(
-name|drawable
+name|buffer
 argument_list|)
 expr_stmt|;
 block|}
@@ -2729,10 +2797,9 @@ operator|++
 control|)
 block|{
 comment|/*            * BUILD THIS FRAME into our 'this_frame' buffer.            */
-name|drawable
-operator|=
-name|gimp_drawable_get
-argument_list|(
+name|gint32
+name|drawable_ID
+init|=
 name|layers
 index|[
 name|total_frames
@@ -2743,17 +2810,14 @@ operator|+
 literal|1
 operator|)
 index|]
-argument_list|)
-expr_stmt|;
+decl_stmt|;
 comment|/* Image has been closed/etc since we got the layer list? */
 comment|/* FIXME - How do we tell if a gimp_drawable_get() fails? */
 if|if
 condition|(
 name|gimp_drawable_width
 argument_list|(
-name|drawable
-operator|->
-name|drawable_id
+name|drawable_ID
 argument_list|)
 operator|==
 literal|0
@@ -2811,18 +2875,12 @@ index|]
 argument_list|,
 name|width
 argument_list|,
-name|drawable
+name|drawable_ID
 argument_list|,
 name|FALSE
 argument_list|)
 expr_stmt|;
 block|}
-comment|/* clean up */
-name|gimp_drawable_detach
-argument_list|(
-name|drawable
-argument_list|)
-expr_stmt|;
 if|if
 condition|(
 name|opmode
@@ -4223,6 +4281,15 @@ expr_stmt|;
 block|}
 else|else
 block|{
+name|GeglBuffer
+modifier|*
+name|buffer
+decl_stmt|;
+specifier|const
+name|Babl
+modifier|*
+name|format
+decl_stmt|;
 name|cumulated_delay
 operator|=
 name|this_delay
@@ -4272,20 +4339,26 @@ argument_list|,
 literal|0
 argument_list|)
 expr_stmt|;
-name|drawable
+name|buffer
 operator|=
-name|gimp_drawable_get
+name|gimp_drawable_get_buffer
 argument_list|(
 name|new_layer_id
 argument_list|)
 expr_stmt|;
-name|gimp_pixel_rgn_init
+name|format
+operator|=
+name|get_format
 argument_list|(
-operator|&
-name|pixel_rgn
+name|new_layer_id
+argument_list|)
+expr_stmt|;
+name|gegl_buffer_set
+argument_list|(
+name|buffer
 argument_list|,
-name|drawable
-argument_list|,
+name|GEGL_RECTANGLE
+argument_list|(
 literal|0
 argument_list|,
 literal|0
@@ -4297,49 +4370,20 @@ argument_list|,
 name|bbox_bottom
 operator|-
 name|bbox_top
-argument_list|,
-name|TRUE
-argument_list|,
-name|FALSE
 argument_list|)
-expr_stmt|;
-name|gimp_pixel_rgn_set_rect
-argument_list|(
-operator|&
-name|pixel_rgn
+argument_list|,
+literal|0
+argument_list|,
+name|format
 argument_list|,
 name|opti_frame
 argument_list|,
-literal|0
-argument_list|,
-literal|0
-argument_list|,
-name|bbox_right
-operator|-
-name|bbox_left
-argument_list|,
-name|bbox_bottom
-operator|-
-name|bbox_top
+name|GEGL_AUTO_ROWSTRIDE
 argument_list|)
 expr_stmt|;
-name|gimp_drawable_flush
+name|g_object_unref
 argument_list|(
-name|drawable
-argument_list|)
-expr_stmt|;
-name|gimp_drawable_detach
-argument_list|(
-name|drawable
-argument_list|)
-expr_stmt|;
-name|gimp_item_transform_translate
-argument_list|(
-name|new_layer_id
-argument_list|,
-name|bbox_left
-argument_list|,
-name|bbox_top
+name|buffer
 argument_list|)
 expr_stmt|;
 block|}

plug-ins/common/Makefile.am
@@ -263,6 +263,7 @@ animation_optimize_LDADD = \
 	$(libgimpbase)		\
 	$(CAIRO_LIBS)		\
 	$(GDK_PIXBUF_LIBS)	\
+	$(GEGL_LIBS)		\
 	$(RT_LIBS)		\
 	$(INTLLIBS)		\
 	$(animation_optimize_RC)

plug-ins/common/plugin-defs.pl
@@ -1,6 +1,6 @@
 %plugins = (
     'align-layers' => { ui => 1 },
-    'animation-optimize' => {},
+    'animation-optimize' => { gegl => 1},
 # FIXME 'animation-play' => { ui => 1, gegl => 1 },
     'blinds' => { ui => 1, gegl => 1 },
     'border-average' => { ui => 1, gegl => 1 },

commit 8731d0392af980f9ff94f6aaa56f61fcddfc6154

Issue #4209: Animation optimize for GIF moves layers to top left corner.
Regression from commit 1f0adcf34ce [formerly 9d33a9a9ca8] as spotted by Massimo.


Former-commit-id: 9a4d1f0277fb2ed6a79f83401b97b17e36698d04

plug-ins/common/animation-optimize.c
@@ -74,7 +74,7 @@ end_define
 begin_typedef
 typedef|typedef
 enum|enum
-DECL|enum|__anon2bc2a0e60103
+DECL|enum|__anon27a8d8a40103
 block|{
 DECL|enumerator|DISPOSE_UNDEFINED
 name|DISPOSE_UNDEFINED
@@ -99,7 +99,7 @@ end_typedef
 begin_typedef
 typedef|typedef
 enum|enum
-DECL|enum|__anon2bc2a0e60203
+DECL|enum|__anon27a8d8a40203
 block|{
 DECL|enumerator|OPOPTIMIZE
 name|OPOPTIMIZE
@@ -4589,6 +4589,18 @@ expr_stmt|;
 name|g_object_unref
 argument_list|(
 name|buffer
+argument_list|)
+expr_stmt|;
+name|gimp_item_transform_translate
+argument_list|(
+name|GIMP_ITEM
+argument_list|(
+name|new_layer
+argument_list|)
+argument_list|,
+name|bbox_left
+argument_list|,
+name|bbox_top
 argument_list|)
 expr_stmt|;
 block|}